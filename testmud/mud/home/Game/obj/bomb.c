820bbf813bf88a6cf50a8c4be4c3be0096916021
Raymond Jennings <shentino@gmail.com>
2010-11-30 08:26:10 -0800

    Some fine tuning to the clone bomb stress tester

--- testmud/mud/home/Game/obj/bomb.c
+++ testmud/mud/home/Game/obj/bomb.c
@@ -3,22 +3,11 @@
 #include <kotaka/paths.h>
 #include <kotaka/log.h>
 
-mixed *alpha;
-
 static void create(int clone)
 {
 	if (clone) {
 		call_out("check", 0);
 	}
-}
-
-float hptime()
-{
-	mixed *timestamp;
-	
-	timestamp = millitime();
-	
-	return (float)timestamp[0] + timestamp[1];
 }
 
 static void check()
--- testmud/mud/home/Game/sys/testd.c
+++ testmud/mud/home/Game/sys/testd.c
@@ -1,16 +1,26 @@
 #include <kotaka/paths.h>
 #include <kotaka/log.h>
 
+#include <status.h>
+
 static void create()
 {
-	call_out("bomb", 0, 100000);
+}
+
+void ignite()
+{
+	call_out("bomb", 0, 1000000);
 }
 
 static void bomb(int quota)
 {
 	int max;
 	
-	max = 100000;
+	max = (int)sqrt((float)quota);
+	
+	if (quota % max != 0) {
+		max = quota % max;
+	}
 	
 	if (max > quota) {
 		max = quota;
@@ -18,11 +28,9 @@ static void bomb(int quota)
 	
 	for (; quota > 0 && max > 0; quota--, max--) {
 		clone_object("~/obj/bomb");
-
-		if (quota % 1000 == 0) {
-			LOGD->post_message("test", LOG_INFO, quota + " bombs left to clone.");
-		}
 	}
+	
+	LOGD->post_message("test", LOG_INFO, quota + " bombs left to clone.");
 	
 	if (quota > 0) {
 		call_out("bomb", 0, quota);
