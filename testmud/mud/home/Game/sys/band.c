406f0cd7f1c4cd3796592305ae9e338d2ea6cdf8
Raymond Jennings <shentino@gmail.com>
2011-06-22 01:49:53 -0700

    Allow ban manager to save state

--- testmud/mud/home/Game/sys/band.c
+++ testmud/mud/home/Game/sys/band.c
@@ -1,22 +1,32 @@
+#include <kotaka/privilege.h>
+
 /* keeps track of bans */
 
 mapping username_bans;
-mapping ip_bans;
+
+private void save();
+private void restore();
 
 static void create()
 {
 	username_bans = ([ ]);
+	restore();
 }
 
 void ban_username(string username)
 {
 	ACCESS_CHECK(GAME());
 
+	if (username == "admin") {
+		error("Cannot ban admin");
+	}
+
 	if (username_bans[username]) {
 		error("Username already banned");
 	}
 
 	username_bans[username] = 1;
+	save();
 }
 
 void unban_username(string username)
@@ -28,9 +38,20 @@ void unban_username(string username)
 	}
 
 	username_bans[username] = nil;
+	save();
 }
 
 int query_is_banned(string username)
 {
-	return !!username_bans(username);
+	return !!username_bans[username];
+}
+
+private void save()
+{
+	save_object("band.o");
+}
+
+private void restore()
+{
+	restore_object("band.o");
 }
