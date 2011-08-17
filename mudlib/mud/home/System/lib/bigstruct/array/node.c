71ccd8c22dff4236ee117b679622d99005e5fb6e
Raymond Jennings <shentino@gmail.com>
2011-08-17 06:19:11 -0700

    Make arrays optimized as arrays

--- mudlib/mud/home/System/lib/bigstruct/array/node.c
+++ mudlib/mud/home/System/lib/bigstruct/array/node.c
@@ -6,15 +6,15 @@
 inherit SECOND_AUTO;
 inherit "../base/node";
 
-mapping map;
+mixed *array;
 int level;
 
 static void create()
 {
 	::create();
-	
+
 	level = -1;
-	map = ([ ]);
+	array = allocate(256);
 }
 
 static void destruct()
@@ -23,9 +23,9 @@ static void destruct()
 		object *turkeys;
 		int sz;
 		int i;
-		
-		turkeys = map_values(map);
-		
+
+		turkeys = array - ({ nil });
+
 		sz = sizeof(turkeys);
 		for (i = 0; i < sz; i++) {
 			discard_node(turkeys[i]);
@@ -36,57 +36,22 @@ static void destruct()
 void set_level(int new_level)
 {
 	check_caller();
-	
+
 	ASSERT(level == -1);
-	
+
 	level = new_level;
 }
 
 int get_level()
 {
 	check_caller();
-	
+
 	return level;
 }
 
-mapping get_map()
+mixed *get_array()
 {
 	check_caller();
-	
-	return map;
-}
 
-void set_map(mapping new_map)
-{
-	check_caller();
-	
-	map = new_map;
-}
-
-void dump()
-{
-	ACCESS_CHECK(SYSTEM());
-	
-	LOGD->post_message("dump", LOG_INFO, "Level " + level + " node.");
-	
-	if (level > 1) {
-		int *indices;
-		object *subnodes;
-		int sz;
-		int i;
-		LOGD->post_message("dump", LOG_INFO, "({");
-		
-		indices = map_indices(map);
-		subnodes = map_values(map);
-		
-		sz = sizeof(indices);
-		LOGD->post_message("dump", LOG_INFO, sz + " subnodes");
-
-		for (i = 0; i < sz; i++) {
-			LOGD->post_message("dump", LOG_INFO, "index: " + indices[i]);
-			subnodes[i]->dump();
-		}
-		
-		LOGD->post_message("dump", LOG_INFO, "})");
-	}
+	return array;
 }
--- mudlib/mud/home/System/lib/bigstruct/array/root.c
+++ mudlib/mud/home/System/lib/bigstruct/array/root.c
@@ -25,7 +25,7 @@ atomic static void create()
 static void destruct()
 {
 	int i;
-	
+
 	for (i = 0; i < 4; i++) {
 		if (top[i]) {
 			discard_node(top[i]);
@@ -37,23 +37,22 @@ private int mask(int index, int level)
 {
 	int bits;
 	bits = level << 3;
-	return (index >> bits) << bits;
+	return (index >> bits) & 0xFF;
 }
 
 private mixed sub_get_element(object node, int index)
 {
 	int level;
 	int masked;
-	mapping map;
+	mixed *array;
 	object subnode;
-	
+
 	level = node->get_level();
-	map = node->get_map();
-	
+	array = node->get_array();
+	masked = mask(index, level);
+
 	if (level) {
-		masked = mask(index, level);
-		
-		subnode = map[masked];
+		subnode = array[masked];
 		
 		if (subnode) {
 			return sub_get_element(subnode, index);
@@ -61,7 +60,7 @@ private mixed sub_get_element(object node, int index)
 			return nil;
 		}
 	} else {
-		return map[index];
+		return array[masked];
 	}
 }
 
@@ -69,88 +68,63 @@ private void sub_set_element(object node, int index, mixed value)
 {
 	int level;
 	int masked;
-	mapping map;
+	mixed *array;
 	object subnode;
-	
+
 	level = node->get_level();
-	map = node->get_map();
-	
+	array = node->get_array();
+	masked = mask(index, level);
+
 	if (level) {
-		masked = mask(index, level);
-		
-		subnode = map[masked];
-		
+		subnode = array[masked];
+
 		if (subnode) {
 			sub_set_element(subnode, index, value);
-			
-			if (map_sizeof(subnode->get_map()) == 0) {
-				map[masked] = nil;
-				discard_node(subnode);
-			}
 		} else {
 			if (value != nil) {
 				subnode = new_node();
 				subnode->set_level(level - 1);
-				
-				map[masked] = subnode;
+
+				array[masked] = subnode;
 				sub_set_element(subnode, index, value);
 			}
 		}
 	} else {
-		map[index] = value;
+		array[masked] = value;
 	}
 }
 
 private void truncate_to(object node, int new_size)
 {
 	int level;
-	mapping map;
-	
-	level = node->get_level();
-	map = node->get_map();
-	
-	if (level) {
-		/* elevated level */
-		/* we need to properly dispose of any inferior nodes */
-		int masked;
-		int *indices;
-		object *nodes;
-		mapping tmap;
-		int sz;
-		
-		masked = mask(new_size, level);
-		tmap = map[masked ..];
-		
-		indices = map_indices(tmap);
-		nodes = map_values(tmap);
-		sz = sizeof(indices);
-		
-		if (sz) {
-			int i;
-			
-			i = 0;
-			
-			if (indices[0] == masked) {
-				/* filled node needs sub-level truncation */
-				i++;
-				
-				truncate_to(nodes[0], new_size);
+	int masked;
+	int oldmasked;
+	mixed *array;
 
-				if (!map_sizeof(nodes[0]->get_map())) {
-					discard_node(nodes[0]);
-					map[masked] = nil;
-				}
-			}
-			
-			for (; i < sz; i++) {
-				discard_node(nodes[i]);
-				map[indices[i]] = nil;
+	level = node->get_level();
+	array = node->get_array();
+	masked = mask(new_size - 1, level);
+	oldmasked = mask(size - 1, level);
+
+	if (level) {
+		int i;
+
+		for (i = masked + 1; i <= oldmasked; i++) {
+			if (array[i]) {
+				discard_node(array[i]);
+				array[i] = nil;
 			}
 		}
+
+		if (array[masked]) {
+			truncate_to(array[masked], level - 1);
+		}
 	} else {
-		/* bottom level, just erase */
-		map = map[.. new_size - 1];
-		node->set_map(map);
+		int i;
+
+		for (i = masked + 1; i <= oldmasked; i++) {
+			array[i] = nil;
+		}
 	}
 }
 
@@ -173,17 +147,17 @@ atomic void set_size(int new_size)
 	int cur_level;
 	int new_level;
 	object node;
-	
+
 	check_caller(WRITE_ACCESS);
-	
+
 	if (new_size < 0) {
 		error("Invalid argument");
 	}
-	
+
 	if (size == new_size) {
 		return;
 	}
-	
+
 	switch(size)
 	{
 	case 0x00000000 .. 0x000000FF: cur_level = 0; break;
@@ -191,7 +165,7 @@ atomic void set_size(int new_size)
 	case 0x00010000 .. 0x00FFFFFF: cur_level = 2; break;
 	case 0x01000000 .. 0x7FFFFFFF: cur_level = 3; break;
 	}
-	
+
 	switch(new_size)
 	{
 	case 0x00000000 .. 0x000000FF: new_level = 0; break;
@@ -199,19 +173,19 @@ atomic void set_size(int new_size)
 	case 0x00010000 .. 0x00FFFFFF: new_level = 2; break;
 	case 0x01000000 .. 0x7FFFFFFF: new_level = 3; break;
 	}
-	
+
 	if (new_size > size) {
 		while (cur_level < new_level) {
 			top[++cur_level] = new_node();
 			top[cur_level]->set_level(cur_level);
 		}
 	}
-	
+
 	if (new_size < size) {
 		while (cur_level > new_level) {
 			discard_node(top[cur_level--]);
 		}
-		
+
 		truncate_to(top[cur_level], new_size);
 	}
 
@@ -221,9 +195,9 @@ atomic void set_size(int new_size)
 atomic mixed get_element(int index)
 {
 	int level;
-	
+
 	check_caller(READ_ACCESS);
-	
+
 	if (index < 0) {
 		error("Subscript out of range: " + index + " vs " + 0);
 	}
@@ -231,7 +205,7 @@ atomic mixed get_element(int index)
 	if (index >= size) {
 		error("Subscript out of range: " + index + " vs " + size);
 	}
-	
+
 	switch(index)
 	{
 	case 0x00000000 .. 0x000000FF: level = 0; break;
@@ -239,7 +213,7 @@ atomic mixed get_element(int index)
 	case 0x00010000 .. 0x00FFFFFF: level = 2; break;
 	case 0x01000000 .. 0x7FFFFFFF: level = 3; break;
 	}
-	
+
 	return sub_get_element(top[level], index);
 }
 
@@ -271,11 +245,11 @@ atomic void set_element(int index, mixed value)
 atomic void clear()
 {
 	int old_size;
-	
+
 	check_caller(WRITE_ACCESS);
-	
+
 	old_size = size;
-	
+
 	set_size(0);
 	set_size(old_size);
 }
@@ -285,21 +259,23 @@ object slice(int lowindex, int highindex)
 	int count;
 	int i;
 	object slice;
-	
+
+	/* todo:  use array operations to handle this task in bulk */
+
 	check_caller(READ_ACCESS);
-	
+
 	if (lowindex < 0 || highindex >= size) {
 		error("Subscript out of range");
 	}
-	
+
 	slice = new_object(BIGSTRUCT_ARRAY_LWO);
 	slice->set_size(highindex - lowindex + 1);
-	
+
 	for (i = lowindex; i <= highindex; i++) {
 		slice->set_element(
 			i - lowindex, get_element(i)
 		);
 	}
-	
+
 	return slice;
 }
