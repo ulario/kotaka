9b3a16669b2750342fc931ea489d603d7ae650af
Raymond Jennings <shentino@gmail.com>
2012-04-18 20:41:17 -0700

    Remove debug dross

--- testmud/mud/home/Game/obj/bomb.c
+++ testmud/mud/home/Game/obj/bomb.c
@@ -1,30 +1,3 @@
-#include <status.h>
-
-#include <kotaka/paths.h>
-#include <kotaka/log.h>
-
-#include "~/test.h"
-
 static void create(int clone)
 {
-	if (clone) {
-		call_out("check", TEST_CO_INTERVAL);
-	}
-}
-
-static void check()
-{
-	object testd;
-
-	if (!(testd = find_object("~/sys/testd")) ) {
-		destruct_object(this_object());
-		return;
-	}
-
-	call_out("check", SUBD->rnd() * (float)TEST_CO_INTERVAL);
-}
-
-static void self_destruct()
-{
-	destruct_object(this_object());
 }
