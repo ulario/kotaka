a1998edeabadb031726bbafd2f1ef942474a7457
Raymond Jennings <shentino@gmail.com>
2012-04-19 23:05:36 -0700

    More daemons

--- testmud/mud/include/game/paths.h
+++ testmud/mud/include/game/paths.h
@@ -1,7 +1,8 @@
 #include <config.h>
 
 #define LIB_VERB		(USR_DIR + "/Game/lib/verb")
-#define LIB_SIGHANDLER		(USR_DIR + "/Game/lib/sighandler")
+#define LIB_TIME		(USR_DIR + "/Game/lib/time")
+#define GAME_LIB_USTATE		(USR_DIR + "/Game/lib/ustate")
 #define GAME_LIB_OBJECT		(USR_DIR + "/Game/lib/object")
 
 #define ALIASD			(USR_DIR + "/Game/sys/aliasd")
@@ -9,6 +10,9 @@
 #define ACCOUNTD		(USR_DIR + "/Game/sys/accountd")
 #define ROOT			(USR_DIR + "/Game/sys/root")
 #define SNOOPD			(USR_DIR + "/Game/sys/snoopd")
+#define TIMED			(USR_DIR + "/Game/sys/timed")
 #define GAME_HELPD		(USR_DIR + "/Game/sys/helpd")
 #define GAME_DRIVER		(USR_DIR + "/Game/sys/driver")
 #define GAME_USERD		(USR_DIR + "/Game/sys/userd")
+#define GAME_ROOT		(USR_DIR + "/Game/sys/root")
+#define GAME_TESTD		(USR_DIR + "/Game/sys/testd")
