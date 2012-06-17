62ba843db828049a21a0f0fe2d4941dbc77d0b1b
Raymond Jennings <shentino@gmail.com>
2012-06-17 14:55:05 -0700

    Added demonstration command for painter

---
+++ testmud/mud/home/Game/sys/bin/paint.c
@@ -0,0 +1,46 @@
+#include <kotaka/paths.h>
+#include <game/paths.h>
+#include <status.h>
+
+inherit LIB_BIN;
+
+void main(string args)
+{
+	int x, y;
+	object paint;
+	string buffer;
+
+	send_out("Canvas test:\n");
+
+	paint = new_object("~/lwo/paint/painter");
+	paint->start(80, 20);
+	paint->set_color(0xC);
+
+	send_out(status(ST_TICKS) + " ticks starting.\n");
+
+	for (x = 0; x < 200; x++) {
+		paint->move_pen(random(80), random(20));
+		paint->set_color(random(16));
+		paint->draw("+");
+	}
+
+	paint->set_color(0x2);
+	paint->move_pen(20, 0);
+	paint->draw("+-----------+");
+	paint->move_pen(20, 1);
+	paint->draw("|     /     |");
+	paint->move_pen(20, 2);
+	paint->draw("+-----------+");
+
+	buffer = "   " + random(250);
+	buffer = buffer[strlen(buffer) - 3 ..];
+	buffer += " / 250";
+
+	paint->set_color(0xB);
+	paint->move_pen(22, 1);
+	paint->draw(buffer);
+
+	send_out(status(ST_TICKS) + " ticks after painting remaining.\n");
+	send_out(paint->render_color());
+	send_out(status(ST_TICKS) + " ticks after rendering remaining.\n");
+}
