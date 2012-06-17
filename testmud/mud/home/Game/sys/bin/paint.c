6974a3af129cdcc960eab5c0eacc85135110e249
Raymond Jennings <shentino@gmail.com>
2012-06-17 15:04:59 -0700

    Add a pine tree to the demo

--- testmud/mud/home/Game/sys/bin/paint.c
+++ testmud/mud/home/Game/sys/bin/paint.c
@@ -40,6 +40,36 @@ void main(string args)
 	paint->move_pen(22, 1);
 	paint->draw(buffer);
 
+	paint->set_color(0x2A);
+	paint->move_pen(40, 10);
+	paint->draw("/");
+	paint->move_pen(39, 11);
+	paint->draw("//");
+	paint->move_pen(38, 12);
+	paint->draw("///");
+	paint->move_pen(37, 13);
+	paint->draw("////");
+	paint->move_pen(36, 14);
+	paint->draw("/////");
+
+	paint->set_color(0x20);
+	paint->move_pen(41, 11);
+	paint->draw("\\");
+	paint->move_pen(41, 12);
+	paint->draw("\\\\");
+	paint->move_pen(41, 13);
+	paint->draw("\\\\\\");
+	paint->move_pen(41, 14);
+	paint->draw("\\\\\\\\");
+
+	paint->set_color(0x30);
+	paint->move_pen(39, 15);
+	paint->draw("|||");
+	paint->move_pen(39, 16);
+	paint->draw("|||");
+	paint->move_pen(39, 17);
+	paint->draw("|||");
+
 	send_out(status(ST_TICKS) + " ticks after painting remaining.\n");
 	send_out(paint->render_color());
 	send_out(status(ST_TICKS) + " ticks after rendering remaining.\n");
