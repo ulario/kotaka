83cf9c9d6a93e0a651547662d556da840bf66b98
Raymond Jennings <shentino@gmail.com>
2012-06-18 02:35:39 -0700

    Add call_touch to game test

--- testmud/mud/home/Game/sys/testd.c
+++ testmud/mud/home/Game/sys/testd.c
@@ -13,21 +13,47 @@ void ignite(int count)
 	call_out("bomb", 0, count);
 }
 
+void touchall()
+{
+	int i;
+
+	LOGD->post_message("test", LOG_INFO, "Beginning touch");
+
+	call_out("do_touch", 0, status(ST_OTABSIZE) - 1);
+}
+
+static void do_touch(int quota)
+{
+	int limit;
+
+	limit = 1024;
+
+	if (quota % limit != 0) {
+		limit = quota % limit;
+	}
+
+	for (; quota >= 0 && limit > 0; quota--, limit--) {
+		object bomb;
+
+		if (bomb = find_object("~/obj/bomb" + quota)) {
+			call_touch(bomb);
+		}
+	}
+
+	LOGD->post_message("test", LOG_INFO, quota + " objects left to check for touches.");
+
+	if (quota > 0) {
+		call_out("do_touch", 0, quota);
+	}
+}
+
 static void bomb(int quota)
 {
-	int max;
+	int limit;
 
-	max = (int)sqrt((float)quota);
+	limit = 200;
 
-	if (quota % max != 0) {
-		max = quota % max;
-	}
-
-	if (max > quota) {
-		max = quota;
-	}
-
-	for (; quota > 0 && max > 0; quota--, max--) {
+	for (; quota > 0 && limit > 0; quota--, limit--) {
 		clone_object("~/obj/bomb");
 	}
 
