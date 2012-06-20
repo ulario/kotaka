184d0559e028f661065d3a9172f369879976a72b
Raymond Jennings <shentino@gmail.com>
2012-06-20 14:31:45 -0700

    Reenable bigstruct testing after a recent change

--- mudlib/mud/home/System/sys/testd.c
+++ mudlib/mud/home/System/sys/testd.c
@@ -28,12 +28,12 @@ private void test_bigstruct_array()
 
 	arr = new_object(BIGSTRUCT_ARRAY_LWO);
 	arr->set_size(0x40000000);
-	
+
 	for (i = 1; i < 0x40000000; i <<= 1) {
 		arr->set_element(i, i);
 		ASSERT(arr->get_element(i) == i);
 	}
-	
+
 	for (j = 0x40000000; j > 0; j /= 3, j <<= 1) {
 		arr->set_size(j);
 		arr->set_size(0x40000000);
@@ -48,22 +48,22 @@ private void test_bigstruct_array()
 	}
 
 	LOGD->post_message("test", LOG_DEBUG, "Testing array slicing");
-	
+
 	arr->set_element(49, 1337);
 	arr->set_element(50, 2337);
 	arr->set_element(150, 3337);
 	arr->set_element(151, 4337);
-	
+
 	arrslice = arr->slice(50, 150);
 	ASSERT(arrslice->get_size() == 101);
 	ASSERT(arrslice->get_element(0) == 2337);
 	ASSERT(arrslice->get_element(100) == 3337);
-	
+
 	arr->clear();
-	
+
 	/* leak test */
 	arr->set_size(0);
-	
+
 	i = status(ST_NOBJECTS);
 	arr->set_size(0x7FFFFFFF);
 	arr->set_element(49, 1337);
@@ -75,9 +75,9 @@ private void test_bigstruct_array()
 	arr->set_element(15000, 3337);
 	arr->set_element(15100, 4337);
 	arr->set_size(0);
-	
+
 	ASSERT(status(ST_NOBJECTS) == i);
-	
+
 	LOGD->post_message("test", LOG_DEBUG, "Array test passed");
 }
 
@@ -85,7 +85,7 @@ private void test_bigstruct_deque()
 {
 	int x, y;
 	object deque;
-	
+
 	deque = new_object(BIGSTRUCT_DEQUE_LWO);
 
 	for (x = 0; x < 3000; x++) {
@@ -97,10 +97,10 @@ private void test_bigstruct_deque()
 		ASSERT(x == deque->get_front());
 		deque->pop_front();
 	}
-	
+
 	ASSERT(deque->get_size() == 0);
 	ASSERT(deque->empty());
-	
+
 	for (x = 0; x < 3000; x++) {
 		deque->push_front(x);
 	}
@@ -111,10 +111,10 @@ private void test_bigstruct_deque()
 		ASSERT(x == deque->get_back());
 		deque->pop_back();
 	}
-	
+
 	ASSERT(deque->get_size() == 0);
 	ASSERT(deque->empty());
-	
+
 	for (x = 0; x < 3000; x++) {
 		deque->push_front(x);
 	}
@@ -125,10 +125,10 @@ private void test_bigstruct_deque()
 		ASSERT(x == deque->get_front());
 		deque->pop_front();
 	}
-	
+
 	ASSERT(deque->get_size() == 0);
 	ASSERT(deque->empty());
-	
+
 	for (x = 0; x < 3000; x++) {
 		deque->push_back(x);
 	}
@@ -139,10 +139,10 @@ private void test_bigstruct_deque()
 		ASSERT(x == deque->get_back());
 		deque->pop_back();
 	}
-	
+
 	ASSERT(deque->empty());
 	ASSERT(deque->get_size() == 0);
-	
+
 	LOGD->post_message("test", LOG_DEBUG, "Deque test passed");
 }
 
@@ -160,6 +160,7 @@ private void test_bigstruct_map()
 		map->set_element(key, i);
 		ASSERT(map->get_element(key) == i);
 	}
+
 	map->clear();
 	map = new_object(BIGSTRUCT_MAP_LWO);
 	map->set_type(T_INT);
@@ -173,18 +174,18 @@ private void test_bigstruct_map()
 
 	LOGD->post_message("test", LOG_DEBUG,
 		"Testing defrag");
-	
+
 	for (i = 0; i < 2500; i++) {
 		map->set_element(i, 0);
 	}
-	
+
 	for (i = 0; i < 50; i++) {
 		map->set_element(i * i, i);
 	}
 
 	LOGD->post_message("test", LOG_DEBUG,
 		"Lightweight defrag complete");
-	
+
 	map->rebalance();
 
 	for (i = 0; i < 50; i++) {
@@ -227,7 +228,7 @@ void test()
 
 	ACCESS_CHECK(SYSTEM());
 
-#if 0
+#if 1
 	LOGD->post_message("test", LOG_DEBUG, "TestD test battery beginning");
 
 #ifdef SYS_NETWORKING
