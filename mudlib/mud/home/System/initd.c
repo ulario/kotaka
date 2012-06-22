77d56845e6bcdcc7e457736abcb663d47245458e
Raymond Jennings <shentino@gmail.com>
2012-06-22 09:01:13 -0700

    Restore paranoid tick limit

--- mudlib/mud/home/System/initd.c
+++ mudlib/mud/home/System/initd.c
@@ -259,8 +259,6 @@ private void configure_klib()
 	for (index = 0; index < sizeof(wizards); index++) {
 		KERNELD->add_owner(wizards[index]);
 	}
-
-	KERNELD->set_rsrc("ticks", 5000000, 0, 0);
 }
 
 mapping read_init_file(string subsystem)
