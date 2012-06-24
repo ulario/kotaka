46375f595c99e35649dc371727ee56691d103b6f
Raymond Jennings <shentino@gmail.com>
2012-06-24 16:18:37 -0700

    Finish implementing clone tracking

--- mudlib/mud/home/System/lwo/object_info.c
+++ mudlib/mud/home/System/lwo/object_info.c
@@ -2,6 +2,9 @@
 
 #include <kotaka/privilege.h>
 #include <kotaka/paths.h>
+#include <kotaka/assert.h>
+
+#include <status.h>
 
 inherit SECOND_AUTO;
 
@@ -22,6 +25,10 @@ string destructor;		/* destructor */
 string *inherited_constructors;	/* constructors */
 string *inherited_destructors;	/* destructors */
 
+int nclones;			/* number of clones */
+int overflow;			/* if we overflowed the array */
+mapping clones;			/* clones */
+
 /****************/
 /* Declarations */
 /****************/
@@ -38,6 +45,9 @@ void set_destructor(string destructor);
 void set_inherited_constructors(string *constructors);
 void set_inherited_destructors(string *destructors);
 
+void add_clone(object clone);
+void remove_clone(object clone);
+
 string query_path();
 int *query_inherits();
 string *query_includes();
@@ -48,6 +58,9 @@ string query_destructor();
 
 string *query_inherited_constructors();
 string *query_inherited_destructors();
+
+object *query_clones();
+int query_clone_count();
 
 /***************/
 /* definitions */
@@ -113,6 +126,43 @@ void set_inherited_destructors(string *destructors)
 	inherited_destructors = destructors;
 }
 
+void add_clone(object clone)
+{
+	ACCESS_CHECK(SYSTEM());
+
+	nclones++;
+
+	if (overflow) {
+		return;
+	}
+
+	if (nclones > status(ST_ARRAYSIZE)) {
+		overflow = 1;
+		clones = nil;
+		return;
+	}
+
+	if (!clones) {
+		clones = ([ ]);
+	}
+
+	clones[clone] = 1;
+}
+
+void remove_clone(object clone)
+{
+	ACCESS_CHECK(SYSTEM());
+	ASSERT(nclones > 0);
+
+	nclones--;
+
+	if (overflow) {
+		return;
+	}
+
+	clones[clone] = nil;
+}
+
 string query_path()
 {
 	return path;
@@ -151,4 +201,18 @@ string *query_inherited_constructors()
 string *query_inherited_destructors()
 {
 	return inherited_destructors;
+}
+
+object *query_clones()
+{
+	if (overflow) {
+		return nil;
+	}
+
+	return clones ? map_indices(clones) : ({ });
+}
+
+int query_clone_count()
+{
+	return nclones;
 }
