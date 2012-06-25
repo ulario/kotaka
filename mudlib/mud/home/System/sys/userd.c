57d536a58524fed57fd9b2d30b64077ebfc2a180
Raymond Jennings <shentino@gmail.com>
2012-06-24 18:01:42 -0700

    rearranged userd

--- mudlib/mud/home/System/sys/userd.c
+++ mudlib/mud/home/System/sys/userd.c
@@ -71,7 +71,13 @@ void block_connections();
 void unblock_connections();
 int query_connection_count();
 
-/* basic */
+/* initd hooks */
+
+/***************/
+/* Definitions */
+/***************/
+
+/* internal */
 
 static void create()
 {
@@ -86,149 +92,6 @@ static void create()
 	intercepts = ([ ]);
 
 	reblocked = ([ ]);
-}
-
-/* admin */
-
-void enable()
-{
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	register_with_klib_userd();
-	enabled = 1;
-}
-
-void disable()
-{
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	enabled = 0;
-	unregister_with_klib_userd();
-}
-
-void block_connections()
-{
-	object *conns;
-	int index;
-
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	if (blocked) {
-		blocked++;
-		return;
-	}
-
-	LOGD->post_message("system", LOG_NOTICE, "PortD: Blocking connections");
-
-	blocked = 1;
-	reblocked = ([ ]);
-
-	conns = userd::query_connections();
-
-	for (index = 0; index < sizeof(conns); index++) {
-		object conn;
-
-		conn = conns[index];
-
-		/* don't interfere with the failsafe */
-		if (conn->query_user() <- "/kernel/obj/user") {
-			continue;
-		}
-
-		if (conn->query_mode() == MODE_BLOCK) {
-			reblocked[conn] = 1;
-		} else {
-			conn->set_mode(MODE_BLOCK);
-		}
-	}
-}
-
-void unblock_connections()
-{
-	object *conns;
-	int index;
-
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	if (blocked == 0) {
-		error("Connections not blocked");
-	}
-
-	if (blocked > 1) {
-		blocked--;
-		return;
-	}
-
-	LOGD->post_message("system", LOG_NOTICE, "PortD: Unblocking connections");
-
-	blocked = 0;
-	conns = userd::query_connections();
-
-	for (index = 0; index < sizeof(conns); index++) {
-		object conn;
-		conn = conns[index];
-
-		if (reblocked[conn]) {
-			reblocked[conn] = nil;
-		} else {
-			conn->set_mode(MODE_UNBLOCK);
-		}
-	}
-	
-	reblocked = nil;
-}
-
-void reboot()
-{
-	if (enabled) {
-		unregister_with_klib_userd();
-		register_with_klib_userd();
-	}
-}
-
-/* config */
-
-void set_binary_manager(int port, object LIB_USERD manager)
-{
-	ACCESS_CHECK(PRIVILEGED());
-
-	PERMISSION_CHECK(port != 0);
-
-	binary_managers[port] = manager;
-}
-
-void set_telnet_manager(int port, object LIB_USERD manager)
-{
-	ACCESS_CHECK(PRIVILEGED());
-
-	telnet_managers[port] = manager;
-}
-
-void set_fixed_manager(int port, object LIB_USERD manager)
-{
-	ACCESS_CHECK(PRIVILEGED());
-
-	fixed_managers[port] = manager;
-}
-
-/*********/
-/* Hooks */
-/*********/
-
-/* initd */
-
-void prepare_reboot()
-{
-	ACCESS_CHECK(SYSTEM());
-
-	unregister_with_klib_userd();
-}
-
-void bogus_reboot()
-{
-	ACCESS_CHECK(SYSTEM());
-
-	register_with_klib_userd();
 }
 
 private void analyze_connection(object LIB_CONN connection)
@@ -290,6 +153,268 @@ private object manager_of(object LIB_CONN connection)
 
 	return manager;
 }
+
+int login(string str)
+{
+	ACCESS_CHECK(previous_program() == LIB_CONN);
+
+	previous_object()->message("Internal error: connection manager fault\n");
+	return MODE_DISCONNECT;
+}
+
+int receive_message(string str)
+{
+	ACCESS_CHECK(previous_program() == LIB_CONN);
+
+	previous_object()->message("Internal error: connection manager fault\n");
+	return MODE_DISCONNECT;
+}
+
+private object query_select(string str, object conn)
+{
+	object base_conn;
+	int level;
+	object manager;
+	object user;
+
+	base_conn = conn;
+
+	while (base_conn && base_conn <- LIB_USER) {
+		base_conn = base_conn->query_conn();
+		level++;
+
+		if (level > MAX_CONN_DEPTH) {
+			error("Connection chain length overflow");
+		}
+	}
+
+	switch(level) {
+	case 0:
+		return clone_object("~/obj/filter/rlimits");
+	}
+
+	if (user = intercepts[base_conn]) {
+		intercepts[base_conn] = nil;
+		return user;
+	}
+
+	user = manager_of(base_conn);
+
+	if (user) {
+		user = user->select(str);
+	}
+
+	return user ? user : this_object();
+}
+
+private void register_with_klib_userd()
+{
+	mixed *status;
+	int index;
+	object this;
+	this = this_object();
+
+	status = status();
+
+	telnet_port_count = sizeof(status[ST_TELNETPORTS]);
+	binary_port_count = sizeof(status[ST_BINARYPORTS]);
+
+	USERD->set_telnet_manager(0, this);
+
+	for (index = 1; index < telnet_port_count; index++) {
+		USERD->set_telnet_manager(index, this);
+	}
+
+	for (index = 1; index < binary_port_count; index++) {
+		USERD->set_binary_manager(index, this);
+	}
+}
+
+private void unregister_with_klib_userd()
+{
+	mixed *status;
+	int index;
+
+	status = status();
+
+	USERD->set_telnet_manager(0, nil);
+
+	for (index = 0; index < telnet_port_count; index++) {
+		USERD->set_telnet_manager(index, nil);
+	}
+
+	for (index = 0; index < binary_port_count; index++) {
+		USERD->set_binary_manager(index, nil);
+	}
+
+	telnet_port_count = 0;
+	binary_port_count = 0;
+}
+
+/* external */
+
+int free_users()
+{
+	return status()[ST_UTABSIZE] - sizeof(userd::query_connections());
+}
+
+void enable()
+{
+	ACCESS_CHECK(SYSTEM() || KADMIN());
+
+	register_with_klib_userd();
+	enabled = 1;
+}
+
+void disable()
+{
+	ACCESS_CHECK(SYSTEM() || KADMIN());
+
+	enabled = 0;
+	unregister_with_klib_userd();
+}
+
+void block_connections()
+{
+	object *conns;
+	int index;
+
+	ACCESS_CHECK(SYSTEM() || KADMIN());
+
+	if (blocked) {
+		blocked++;
+		return;
+	}
+
+	LOGD->post_message("system", LOG_NOTICE, "PortD: Blocking connections");
+
+	blocked = 1;
+	reblocked = ([ ]);
+
+	conns = userd::query_connections();
+
+	for (index = 0; index < sizeof(conns); index++) {
+		object conn;
+
+		conn = conns[index];
+
+		/* don't interfere with the failsafe */
+		if (conn->query_user() <- "/kernel/obj/user") {
+			continue;
+		}
+
+		if (conn->query_mode() == MODE_BLOCK) {
+			reblocked[conn] = 1;
+		} else {
+			conn->set_mode(MODE_BLOCK);
+		}
+	}
+}
+
+object intercept(object LIB_CONN conn, object LIB_USER user)
+{
+	object base_conn;
+
+	ACCESS_CHECK(previous_program() == LIB_SYSTEM_USER);
+
+	base_conn = conn;
+
+	while (base_conn && base_conn <- LIB_USER) {
+		base_conn = base_conn->query_conn();
+	}
+
+	if (!base_conn) {
+		error("Bad redirect");
+	}
+
+	intercepts[base_conn] = user;
+
+	return query_select(nil, conn);
+}
+
+void unblock_connections()
+{
+	object *conns;
+	int index;
+
+	ACCESS_CHECK(SYSTEM() || KADMIN());
+
+	if (blocked == 0) {
+		error("Connections not blocked");
+	}
+
+	if (blocked > 1) {
+		blocked--;
+		return;
+	}
+
+	LOGD->post_message("system", LOG_NOTICE, "PortD: Unblocking connections");
+
+	blocked = 0;
+	conns = userd::query_connections();
+
+	for (index = 0; index < sizeof(conns); index++) {
+		object conn;
+		conn = conns[index];
+
+		if (reblocked[conn]) {
+			reblocked[conn] = nil;
+		} else {
+			conn->set_mode(MODE_UNBLOCK);
+		}
+	}
+
+	reblocked = nil;
+}
+
+void reboot()
+{
+	if (enabled) {
+		unregister_with_klib_userd();
+		register_with_klib_userd();
+	}
+}
+
+void set_binary_manager(int port, object LIB_USERD manager)
+{
+	ACCESS_CHECK(PRIVILEGED());
+
+	PERMISSION_CHECK(port != 0);
+
+	binary_managers[port] = manager;
+}
+
+void set_telnet_manager(int port, object LIB_USERD manager)
+{
+	ACCESS_CHECK(PRIVILEGED());
+
+	telnet_managers[port] = manager;
+}
+
+void set_fixed_manager(int port, object LIB_USERD manager)
+{
+	ACCESS_CHECK(PRIVILEGED());
+
+	fixed_managers[port] = manager;
+}
+
+/* initd hooks */
+
+void prepare_reboot()
+{
+	ACCESS_CHECK(SYSTEM());
+
+	unregister_with_klib_userd();
+}
+
+void bogus_reboot()
+{
+	ACCESS_CHECK(SYSTEM());
+
+	register_with_klib_userd();
+}
+
+/* userd hooks */
 
 string query_banner(object LIB_CONN connection)
 {
@@ -374,123 +499,4 @@ object select(string str)
 	ACCESS_CHECK(KERNEL() || SYSTEM());
 
 	return query_select(str, previous_object(1));
-}
-
-int login(string str)
-{
-	ACCESS_CHECK(previous_program() == LIB_CONN);
-
-	previous_object()->message("Internal error: connection manager fault\n");
-	return MODE_DISCONNECT;
-}
-
-object intercept(object LIB_CONN conn, object LIB_USER user)
-{
-	object base_conn;
-
-	ACCESS_CHECK(previous_program() == LIB_SYSTEM_USER);
-
-	base_conn = conn;
-
-	while (base_conn && base_conn <- LIB_USER) {
-		base_conn = base_conn->query_conn();
-	}
-
-	if (!base_conn) {
-		error("Bad redirect");
-	}
-
-	intercepts[base_conn] = user;
-
-	return query_select(nil, conn);
-}
-
-/********************/
-/* Helper Functions */
-/********************/
-
-private object query_select(string str, object conn)
-{
-	object base_conn;
-	int level;
-	object manager;
-	object user;
-
-	base_conn = conn;
-
-	while (base_conn && base_conn <- LIB_USER) {
-		base_conn = base_conn->query_conn();
-		level++;
-
-		if (level > MAX_CONN_DEPTH) {
-			error("Connection chain length overflow");
-		}
-	}
-
-	switch(level) {
-	case 0:
-		return clone_object("~/obj/filter/rlimits");
-	}
-
-	if (user = intercepts[base_conn]) {
-		intercepts[base_conn] = nil;
-		return user;
-	}
-
-	user = manager_of(base_conn);
-
-	if (user) {
-		user = user->select(str);
-	}
-
-	return user ? user : this_object();
-}
-
-private void register_with_klib_userd()
-{
-	mixed *status;
-	int index;
-	object this;
-	this = this_object();
-
-	status = status();
-
-	telnet_port_count = sizeof(status[ST_TELNETPORTS]);
-	binary_port_count = sizeof(status[ST_BINARYPORTS]);
-
-	USERD->set_telnet_manager(0, this);
-
-	for (index = 1; index < telnet_port_count; index++) {
-		USERD->set_telnet_manager(index, this);
-	}
-
-	for (index = 1; index < binary_port_count; index++) {
-		USERD->set_binary_manager(index, this);
-	}
-}
-
-private void unregister_with_klib_userd()
-{
-	mixed *status;
-	int index;
-
-	status = status();
-
-	USERD->set_telnet_manager(0, nil);
-
-	for (index = 0; index < telnet_port_count; index++) {
-		USERD->set_telnet_manager(index, nil);
-	}
-
-	for (index = 0; index < binary_port_count; index++) {
-		USERD->set_binary_manager(index, nil);
-	}
-
-	telnet_port_count = 0;
-	binary_port_count = 0;
-}
-
-int free_users()
-{
-	return status()[ST_UTABSIZE] - sizeof(userd::query_connections());
 }
