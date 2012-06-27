15fb64c77c30a25ecdcaad5c0b0205878cad2a12
Raymond Jennings <shentino@gmail.com>
2012-06-27 11:45:01 -0700

    Use shortcuts in verb library as well

--- testmud/mud/home/Game/lib/verb.c
+++ testmud/mud/home/Game/lib/verb.c
@@ -1,4 +1,21 @@
-/* for now, verbs are just being used in place of actions */
-/* the fancy parser will be in place, but this will remain as an option */
-/* just in case */
-void main(object usermob, string args);
+#include <kotaka/paths.h>
+
+static object query_ustate()
+{
+	return TLSD->query_tls_value("Game", "ustate");
+}
+
+static object query_user()
+{
+	return query_ustate()->query_user();
+}
+
+static void send_in(string str)
+{
+	query_ustate()->send_in(str);
+}
+
+static void send_out(string str)
+{
+	query_ustate()->send_out(str);
+}
