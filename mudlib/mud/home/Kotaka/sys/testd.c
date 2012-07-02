c970d8d8c6101a7e00fe91aef9a751d41c433569
Raymond Jennings <shentino@gmail.com>
2012-07-02 08:47:27 -0700

    Add regression test for qsort

--- mudlib/mud/home/Kotaka/sys/testd.c
+++ mudlib/mud/home/Kotaka/sys/testd.c
@@ -1,10 +1,30 @@
 #include <kotaka/paths.h>
 #include <kotaka/log.h>
+#include <kotaka/assert.h>
 
 static void create()
 {
 }
 
+private void test_qsort()
+{
+	int *arr, i;
+
+	arr = allocate(1000);
+
+	for (i = 0; i < 1000; i++) {
+		arr[i] = random(1000000);
+	}
+
+	SUBD->qsort(arr, 0, 1000);
+
+	for (i = 0; i < 999; i++) {
+		ASSERT(arr[i] <= arr[i + 1]);
+	}
+}
+
 void test()
 {
+	LOGD->post_message("test", LOG_DEBUG, "Testing qsort...");
+	test_qsort();
 }
