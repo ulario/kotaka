bd554e400c10d22f4f34773de75bc0e504e0e9fe
Raymond Jennings <shentino@gmail.com>
2012-07-05 15:12:01 -0700

    Comprehensive qsort test, also allow testing of dgd arrays

--- mudlib/mud/home/Kotaka/sys/testd.c
+++ mudlib/mud/home/Kotaka/sys/testd.c
@@ -2,29 +2,56 @@
 #include <kotaka/log.h>
 #include <kotaka/assert.h>
 
+#include <status.h>
+
+int *arr;
+
 static void create()
 {
+	int i, sz;
+
+	arr = allocate(sz = status(ST_ARRAYSIZE));
+
+	for (i = 0; i < sz; i++) {
+		arr[i] = random(1000000000);
+	}
 }
 
 private void test_qsort()
 {
-	int *arr, i;
+	int i, sz;
 
-	arr = allocate(1000);
+	sz = sizeof(arr);
 
-	for (i = 0; i < 1000; i++) {
-		arr[i] = random(1000000);
-	}
+	SUBD->qsort(arr, 0, sz);
 
-	SUBD->qsort(arr, 0, 1000);
-
-	for (i = 0; i < 999; i++) {
+	for (i = 0; i < sz - 1; i++) {
 		ASSERT(arr[i] <= arr[i + 1]);
+	}
+}
+
+private void scramble()
+{
+	int i, sz;
+
+	sz = sizeof(arr);
+
+	for (i = 0; i < sz - 1; i++) {
+		int j;
+		int tmp;
+
+		j = random(sz);
+
+		tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
 	}
 }
 
 void test()
 {
 	LOGD->post_message("test", LOG_DEBUG, "Testing qsort...");
+
+	scramble();
 	test_qsort();
 }
