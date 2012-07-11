cd4d5e8194581fa253b5ad89a3c1d1b0476d1ac8
Raymond Jennings <shentino@gmail.com>
2012-07-10 18:41:25 -0700

    Forbid insertion into objects that we are busy destructing

--- testmud/mud/home/Game/lib/object.c
+++ testmud/mud/home/Game/lib/object.c
@@ -4,6 +4,8 @@ inherit LIB_OBJECT;
 
 inherit "position";
 
+int destructing;
+
 /*****************/
 /* General stuff */
 /*****************/
@@ -26,12 +28,19 @@ static void create()
 	set_id_base(parts[sz - 1]);
 }
 
+int forbid_insert()
+{
+	return destructing;
+}
+
 static nomask void game_object_destruct()
 {
 	int sz;
 	int index;
 	object env;
 	object *children;
+
+	destructing = 1;
 
 	children = query_inventory();
 	env = query_environment();
