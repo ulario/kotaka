a4469d648cab27a4393ca17429b0e6fc4437484b
Raymond Jennings <shentino@gmail.com>
2012-07-26 18:18:20 -0700

    Don't cause a bottleneck, sequence isn't guaranteed anyway

--- mudlib/mud/home/System/sys/calloutd.c
+++ mudlib/mud/home/System/sys/calloutd.c
@@ -18,18 +18,14 @@
 inherit SECOND_AUTO;
 
 int suspend;			/* callouts suspended */
+int handle;			/* releaser handle */
 
 int lrc;			/* last reported callout count */
 int lrh;			/* last reported hole count */
 
-object releasers;
-
-int releases;		/* current number of release callouts */
-int max_releases;	/* maximum number of release callouts */
-int unordered;		/* allow current callouts to skip the suspension queue */
-
 object cmap;	/* ([ oindex : ([ handle : iterator ]) ]) */
 object cqueue;	/* ({ iterator : ({ obj, handle }) }) */
+
 int begin, end;
 int callouts, holes;
 
@@ -38,6 +34,31 @@ int empty();
 mixed *release();
 
 /* private */
+
+static void create()
+{
+	cmap = clone_object(BIGSTRUCT_ARRAY_OBJ);
+	cqueue = clone_object(BIGSTRUCT_ARRAY_OBJ);
+
+	cmap->set_size(0x7FFFFFFF);
+	cqueue->set_size(0x10000000);
+
+	begin = end = callouts = holes = handle = 0;
+
+	catch {
+		RSRCD->set_suspension_manager(this_object());
+	}
+}
+
+static void destrct()
+{
+	if (suspend) {
+		error("Cannot destruct while callouts are suspended");
+	}
+
+	destruct_object(cmap);
+	destruct_object(cqueue);
+}
 
 private int object_index(object obj)
 {
@@ -48,93 +69,6 @@ private int object_index(object obj)
 	}
 
 	return status(obj, O_INDEX);
-}
-
-static void create()
-{
-	cmap = clone_object(BIGSTRUCT_ARRAY_OBJ);
-	cqueue = clone_object(BIGSTRUCT_ARRAY_OBJ);
-	releasers = clone_object(BIGSTRUCT_DEQUE_OBJ);
-
-	cmap->set_size(0x7FFFFFFF);
-	cqueue->set_size(0x10000000);
-
-	begin = end = callouts = holes = 0;
-
-	max_releases = 2000000;
-
-	catch {
-		RSRCD->set_suspension_manager(this_object());
-	}
-}
-
-private void wipe_releasers()
-{
-	while (!releasers->empty()) {
-		object releaser;
-
-		releaser = releasers->get_front();
-		releasers->pop_front();
-
-		if (releaser) {
-			destruct_object(releaser);
-		}
-	}
-
-	releases = 0;
-}
-
-void set_unordered(int new_unordered)
-{
-	if (unordered == new_unordered) {
-		return;
-	}
-
-	unordered = new_unordered;
-}
-
-int query_unordered()
-{
-	return unordered;
-}
-
-private void charge_releasers()
-{
-	int needed;
-
-	needed = callouts + holes + 1;
-
-	if (needed > max_releases) {
-		needed = max_releases;
-	}
-
-	needed -= releases;
-
-	if (needed <= 0) {
-		return;
-	}
-
-	if (needed < 100) {
-		needed = 100;
-	}
-
-	while (needed > 0) {
-		int charges;
-		object releaser;
-
-		charges = needed;
-
-		if (charges > 500) {
-			charges = 500;
-		}
-
-		releaser = clone_object("~/obj/co_releaser");
-		releasers->push_back(releaser);
-
-		releaser->charge(charges);
-		releases += charges;
-		needed -= charges;
-	}
 }
 
 /* public */
@@ -174,17 +108,6 @@ atomic void hold_callouts(mixed delay)
 	call_out("release_callouts", delay);
 }
 
-void set_max_releases(int new_max)
-{
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	if (new_max < 1) {
-		error("Invalid argument");
-	}
-
-	max_releases = new_max;
-}
-
 /* rsrcd hooks */
 
 void suspend_callouts()
@@ -193,7 +116,11 @@ void suspend_callouts()
 		LOGD->post_message("system", LOG_INFO, "Suspending callouts");
 
 		suspend = -1;
-		wipe_releasers();
+
+		if (handle) {
+			remove_call_out(handle);
+			handle = 0;
+		}
 	} else {
 		RSRCD->suspend_callouts();
 	}
@@ -231,10 +158,6 @@ void suspend(object obj, int handle)
 	cqueue->set_element(end++, ({ obj, handle }) );
 	end &= 0x0FFFFFFF;
 	callouts++;
-
-	if (suspend == 1) {
-		charge_releasers();
-	}
 
 	statcheck();
 }
@@ -311,13 +234,19 @@ void release_callouts()
 	if (previous_program() == RSRCD) {
 		LOGD->post_message("system", LOG_INFO, "Releasing callouts");
 
+		/* release future callouts immediately */
+		RSRCD->release_callout(nil, 0);
+
 		if (empty()) {
 			suspend = 0;
-			RSRCD->release_callout(nil, 0);
 			LOGD->post_message("system", LOG_INFO, "Released callouts");
 		} else {
 			suspend = 1;
-			charge_releasers();
+
+			/* start draining backed up callouts */
+			if (!handle) {
+				handle = call_out("do_release", 0);
+			}
 		}
 	} else {
 		RSRCD->release_callouts();
@@ -335,23 +264,21 @@ void do_release()
 {
 	mixed *callout;
 	object obj;
-	int handle;
+	int ohandle;
 	int callouts;
 	int holes;
 
 	ACCESS_CHECK(SYSTEM());
 
-	releases--;
+	handle = 0;
 
 	switch(suspend) {
 	case -1: /* suspended! */
 		LOGD->post_message("system", LOG_INFO, "calloutd: Release while suspended");
-		wipe_releasers();
 		return;
 
 	case 0: /* idle! */
 		LOGD->post_message("system", LOG_INFO, "calloutd: Release while idle");
-		wipe_releasers();
 		return;
 
 	case 1: /* draining */
@@ -359,21 +286,21 @@ void do_release()
 
 		if (callout) {
 			obj = callout[CO_OBJ];
-			handle = callout[CO_HANDLE];
+			ohandle = callout[CO_HANDLE];
 
 			if (obj) {
 				catch {
-					RSRCD->release_callout(obj, handle);
+					RSRCD->release_callout(obj, ohandle);
 				}
 			}
 
 			statcheck();
+			handle = call_out("do_release", 0);
 		} else {
 			RSRCD->release_callout(nil, 0);
 			LOGD->post_message("system", LOG_INFO, "Released callouts");
 
 			suspend = 0;
-			wipe_releasers();
 
 			if (callouts != 0) {
 				error(callouts + " callouts unaccounted for");
@@ -394,20 +321,11 @@ private int bypass(object obj)
 		return 1;
 	}
 
-	if ((suspend == 1) && unordered) {
-		return 1;
-	}
-
-	if (obj <- "~/obj/co_releaser") {
+	if (suspend == 1) {
 		return 1;
 	}
 
 	return DRIVER->creator(object_name(obj)) == "System";
-}
-
-static void destruct()
-{
-	error("Cannot destruct callout manager");
 }
 
 int empty()
