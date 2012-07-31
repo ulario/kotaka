908ac50e689841c287905532fc51147a25747886
Raymond Jennings <shentino@gmail.com>
2012-07-31 09:22:30 -0700

    Add commands to regulate maze runner population, as well as purge all runners

---
+++ testmud/mud/home/Game/sys/bin/wiz/run.c
@@ -0,0 +1,16 @@
+#include <kotaka/paths.h>
+#include <game/paths.h>
+
+inherit LIB_WIZBIN;
+
+void main(string args)
+{
+	int count;
+
+	if (query_user()->query_class() < 3) {
+		send_out("You do not have sufficient access rights to tune the maze test.\n");
+		return;
+	}
+
+	"~/sys/mazed"->set_count_goal((int)args);
+}
