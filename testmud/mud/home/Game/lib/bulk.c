b77bdb6dd7fa68ae29bdc0defef8bd822f021e75
Raymond Jennings <shentino@gmail.com>
2012-08-09 06:00:45 -0700

    More notes on how to handle bulk.

--- testmud/mud/home/Game/lib/bulk.c
+++ testmud/mud/home/Game/lib/bulk.c
@@ -4,15 +4,13 @@
 inherit LIB_OBJECT;
 
 /* for all derived calculations, object is presumed to be a cube */
-/* -1.0 = autocalc */
-/* -2.0 = inherit from first archetype */
 
 /* two of three required */
 float density;		/* kg/m^3 */
 float mass;		/* kg */
 float volume;		/* m^3 (can be derived from area or length) */
 
-/* optional */
+/* optional, derived from volume, can derive volume */
 float length;		/* m */
 float area;		/* m^2 */
 
@@ -28,14 +26,37 @@ static void create()
 {
 	mass = -1.0;
 	density = -1.0;
-	length = -1.0;
-	area = -1.0;
-
 	volume = -1.0;
+	area = -1.0;
+	length = -1.0;
+}
+
+void set_base_density(float new_density)
+{
+	density = new_density;
+}
+
+void set_base_mass(float new_mass)
+{
+	mass = new_mass;
+}
+
+void set_base_volume(float new_volume)
+{
+	volume = new_volume;
+}
+
+void set_base_area(float new_area)
+{
+	area = new_area;
+}
+
+void set_base_length(float new_length)
+{
+	length = new_length;
 }
 
 /*
-
 Global derivation
 	mass = volume * density (if density defined and volume derivable)
 	density = mass / volume (if mass defined and volume derivable)
@@ -53,5 +74,4 @@ Area derivation
 Length derivation
 	volume / area (if volume and area defined)
 	volume ^ 1/3 (if volume defined)
-
 */
