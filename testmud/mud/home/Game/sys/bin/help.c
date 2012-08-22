2d39f0377f4ec37aff18f54f3057b8e2f556a300
Raymond Jennings <shentino@gmail.com>
2012-08-22 10:14:02 -0700

    Add help command and make it work at a low level by default until the proper searching cna be provided

--- testmud/mud/home/Game/sys/bin/help.c
+++ testmud/mud/home/Game/sys/bin/help.c
@@ -5,5 +5,42 @@ inherit LIB_BIN;
 
 void main(string args)
 {
-	query_ustate()->push_state(clone_object("~/obj/ustate/help"));
+	string topic;
+	object pager;
+	mixed *info;
+
+	if (args == "") {
+		switch(query_user()->query_class()) {
+		case 3:
+			topic = "admin/index";
+			break;
+		case 2:
+			topic = "wizard/index";
+			break;
+		case 1:
+			topic = "player/index";
+			break;
+		case 0:
+			topic = "guest/index";
+			break;
+		}
+	} else {
+		topic = args;
+	}
+
+	info = file_info("~Game/data/help/" + topic + ".hlp");
+
+	if (!info) {
+		send_out(topic + ": No such help topic.\n");
+		return;
+	}
+
+	if (info[0] < 0) {
+		send_out(topic + ": Is a category.\n");
+		return;
+	}
+
+	pager = clone_object("~Kotaka/obj/ustate/page");
+	pager->set_text(read_file("~Game/data/help/" + topic + ".hlp"));
+	query_ustate()->push_state(pager);
 }
