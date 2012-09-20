2cbd05699f4815fdd0edca36efcdfff96432d42c
Raymond Jennings <shentino@gmail.com>
2012-09-20 14:55:18 -0700

    Add help module to privilege checks

--- mudlib/mud/include/kotaka/privilege.h
+++ mudlib/mud/include/kotaka/privilege.h
@@ -2,6 +2,7 @@
 #include <kernel/kernel.h>
 #include <kernel/user.h>
 #define KOTAKA()	(sscanf(previous_program(), USR_DIR + "/Kotaka/%*s"))
+#define HELP()		(sscanf(previous_program(), USR_DIR + "/Help/%*s"))
 #define GAME()		(sscanf(previous_program(), USR_DIR + "/Game/%*s"))
 #define KADMIN()	(previous_program(1) == LIB_WIZTOOL && \
 			previous_program() == USR_DIR + "/admin/_code")
