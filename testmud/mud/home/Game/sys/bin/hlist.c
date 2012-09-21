da2ad79c577bb81ebc0f68f5eaba46ed73dfad60
Raymond Jennings <shentino@gmail.com>
2012-09-20 22:20:46 -0700

    Remove redundant functions from help list command

--- testmud/mud/home/Game/sys/bin/hlist.c
+++ testmud/mud/home/Game/sys/bin/hlist.c
@@ -24,72 +24,12 @@ string *topic_mask(string *topics, int *bits, int mask)
 
 int topic_compare(string lhs, string rhs)
 {
-	string *lparts, *rparts;
-	int lsz, rsz;
-	int csz, i;
-
-	ASSERT(lhs && rhs);
-
-	lparts = explode(lhs, "/");
-	rparts = explode(rhs, "/");
-
-	lsz = sizeof(lparts);
-	rsz = sizeof(rparts);
-
-	csz = lsz < rsz ? lsz : rsz;
-	csz--;
-
-	for (i = 0; i < csz; i++) {
-		if (lparts[i] != rparts[i]) {
-			return 0; /* forked */
-		}
-	}
-
-	ASSERT(lsz != rsz);
-
-	return lsz < rsz ? -1 : 1;
+	return "help"->topic_compare(lhs, rhs);
 }
 
 string *topic_filter(string *topics)
 {
-	int i;
-	int sz;
-
-	string *contenders;
-
-	contenders = ({ topics[0] });
-
-	sz = sizeof(topics);
-
-	for (i = 1; i < sz; i++) {
-		int j;
-		int sz2;
-		int survive;
-		survive = 1;
-
-		sz2 = sizeof(contenders);
-
-		for (j = 0; j < sz2; j++) {
-			switch(topic_compare(topics[i], contenders[j])) {
-			case -1: /* victory */
-				contenders[j] = nil;
-				/* fall through */
-			case 0: /* tie */
-				break;
-			case 1: /* defeat */
-				survive = 0;
-				continue;
-			}
-		}
-
-		contenders -= ({ nil });
-
-		if (survive) {
-			contenders += ({ topics[i] });
-		}
-	}
-
-	return contenders;
+	return "help"->topic_compare(topics);
 }
 
 void main(string args)
@@ -149,8 +89,8 @@ void main(string args)
 			text = "Contents of " + topic + ":\n\n";
 		}
 
-		categories = "~/sys/helpd"->query_categories(topic);
-		topics = "~/sys/helpd"->query_topics(topic);
+		categories = HELPD->query_categories(topic);
+		topics = HELPD->query_topics(topic);
 
 		if (sizeof(categories)) {
 			text += "Categories: " + implode(categories, ", ") + "\n\n";
