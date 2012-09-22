dfb5ca397c75c975735fbfd47fa881b3bb811f18
Raymond Jennings <shentino@gmail.com>
2012-09-22 00:23:50 -0700

    Remove now obsolete clone bomb testing

--- testmud/mud/home/Game/obj/bomb.c
+++
@@ -1,9 +0,0 @@
-#include <kotaka/paths.h>
-#include <limits.h>
-
-/* string kaboom; */
-
-static void create(int clone)
-{
-	/* kaboom = STRINGD->chars('\0', MAX_STRING_SIZE); */
-}
--- testmud/mud/home/Game/sys/testd.c
+++ testmud/mud/home/Game/sys/testd.c
@@ -10,65 +10,6 @@ static void create()
 {
 }
 
-/* bomb tests */
-
-void ignite(int count)
-{
-	call_out("bomb", 0, count);
-}
-
-void touchall()
-{
-	int i;
-
-	LOGD->post_message("test", LOG_INFO, "Beginning touch");
-
-	call_out("do_touch", 0, status(ST_OTABSIZE) - 1);
-}
-
-static void do_touch(int quota)
-{
-	int limit;
-
-	limit = 1024;
-
-	if (quota % limit != 0) {
-		limit = quota % limit;
-	}
-
-	for (; quota >= 0 && limit > 0; quota--, limit--) {
-		object bomb;
-
-		if (bomb = find_object("~/obj/bomb" + quota)) {
-			call_touch(bomb);
-		}
-	}
-
-	LOGD->post_message("test", LOG_INFO, quota + " objects left to check for touches.");
-
-	if (quota > 0) {
-		call_out("do_touch", 0, quota);
-	}
-}
-
-static void bomb(int quota)
-{
-	int limit;
-
-	limit = 100;
-	limit += quota % limit;
-
-	for (; quota > 0 && limit > 0; quota--, limit--) {
-		clone_object("~/obj/bomb");
-	}
-
-	LOGD->post_message("test", LOG_INFO, quota + " bombs left to clone.");
-
-	if (quota > 0) {
-		call_out("bomb", 0, quota);
-	}
-}
-
 void test()
 {
 	ACCESS_CHECK(GAME());
