fe10dc00f6c8842a8023466b38672fa24d3bd873
Raymond Jennings <shentino@gmail.com>
2012-09-22 02:35:14 -0700

    Remove obsolete game driver inheritable

--- mudlib/mud/home/Kotaka/lib/driver.c
+++
@@ -1,7 +0,0 @@
-#include <kotaka/paths.h>
-
-object make_lwo(string path);
-object make_clone(string path);
-object create_object(varargs object LIB_OBJECT *parents);
-atomic void destroy_object(object LIB_OBJECT turkey,
-	varargs int recursive);
