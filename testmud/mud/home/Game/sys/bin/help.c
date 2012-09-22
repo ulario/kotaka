ea073361022bebcef4a94d2d4cf4699862c54302
Raymond Jennings <shentino@gmail.com>
2012-09-22 03:12:33 -0700

    Streamlined help command a bit

--- testmud/mud/home/Game/sys/bin/help.c
+++ testmud/mud/home/Game/sys/bin/help.c
@@ -74,6 +74,33 @@ string *topic_filter(string *topics)
 	return contenders;
 }
 
+private string list_category(string category)
+{
+	string *chunks;
+	string *categories;
+	string *topics;
+	string text;
+
+	chunks = ({ });
+
+	categories = HELPD->query_categories(category);
+	topics = HELPD->query_topics(category);
+
+	if (sizeof(categories)) {
+		chunks += ({ "Categories:\n    " + implode(categories, "\n    ") });
+	}
+
+	if (sizeof(topics)) {
+		chunks += ({ "Topics:\n    " + implode(topics, "\n    ") });
+	}
+
+	if (!sizeof(categories) && !sizeof(topics)) {
+		chunks += ({ "...nothing" });
+	}
+
+	return implode(chunks, "\n\n");
+}
+
 void main(string args)
 {
 	mapping index;
@@ -81,90 +108,66 @@ void main(string args)
 	string *candidates;
 	string *survivors;
 	string topic;
+	string header;
+	string text;
 	int sz, i;
 
 	args = STRINGD->trim_whitespace(args);
 	args = STRINGD->replace(args, " ", "_");
 
 	if (args == "") {
-		args = "index";
-	}
+		/* list root category */
+		header = "(root contents)";
+		text = list_category(nil);
+	} else {
+		/* find topic, barf if ambiguous */
+		index = HELPD->query_index();
+		ASSERT(index);
 
-	index = HELPD->query_index();
-	ASSERT(index);
+		if (index[args] == nil) {
+			send_out("No such topic or category is available.\n");
+			return;
+		}
 
-	if (index[args] == nil) {
-		send_out("No such topic or category is available.\n");
-		return;
-	}
+		list = index[args];
+		candidates = map_indices(list);
+		sz = sizeof(candidates);
 
-	list = index[args];
-	candidates = map_indices(list);
-	sz = sizeof(candidates);
+		survivors = topic_filter(candidates);
+		sz = sizeof(survivors);
 
-	survivors = topic_filter(candidates);
-	sz = sizeof(survivors);
+		if (sz == 1) {
+			topic = survivors[0];
 
-	if (sz == 1) {
-		object paint;
-		object pager;
-		string text;
-		string header;
-
-		pager = clone_object("~Kotaka/obj/ustate/page");
-		paint = new_object("~Kotaka/lwo/painter");
-
-		topic = survivors[0];
-
-		if (list[topic] & 1) {
-			header = topic;
-			text = HELPD->query_content(topic);
-		} else {
-			if (HELPD->test_topic(topic + "/index")) {
-				header = topic + "/index";
-				text = HELPD->query_content(topic + "/index");
+			if (list[topic] & 1) {
+				header = topic;
+				text = HELPD->query_content(topic);
 			} else {
-				string *chunks;
-				string *categories;
-				string *topics;
-
-				header = topic + " (contents)";
-
-				chunks = ({ });
-
-				categories = HELPD->query_categories(topic);
-				topics = HELPD->query_topics(topic);
-
-				if (sizeof(categories)) {
-					chunks += ({ "Categories:\n    " + implode(categories, "\n    ") });
+				if (HELPD->test_topic(topic + "/index")) {
+					header = topic + "/index";
+					text = HELPD->query_content(topic + "/index");
+				} else {
+					header = topic + " (contents)";
+					text = list_category(topic);
 				}
+			}
 
-				if (sizeof(topics)) {
-					chunks += ({ "Topics:\n    " + implode(topics, "\n    ") });
-				}
+			if (sizeof(candidates - survivors)) {
+				text += "\nSee also: " + implode(candidates - survivors, ", ") + "\n";
+			}
+		} else {
+			send_out("That topic is ambiguous, please choose from:\n");
 
-				if (!sizeof(categories) && !sizeof(topics)) {
-					chunks += ({ "...nothing" });
-				}
-
-				text = implode(chunks, "\n\n");
+			for (i = 0; i < sz; i++) {
+				send_out(survivors[i] + "\n");
 			}
 		}
+	}
 
-		if (sizeof(candidates - survivors)) {
-			text += "\nSee also: " + implode(candidates - survivors, ", ") + "\n";
-		}
-
-		text = "[ " + header + " ]\n\n" + text;
-		pager->set_text(text);
-
+	if (header) {
+		object pager;
+		pager = clone_object("~Kotaka/obj/ustate/page");
+		pager->set_text("[ " + header + " ]\n\n" + text);
 		query_ustate()->push_state(pager);
-		return;
-	} else {
-		send_out("That topic is ambiguous, please choose from:\n");
-
-		for (i = 0; i < sz; i++) {
-			send_out(survivors[i] + "\n");
-		}
 	}
 }
