78e3ff3388928a863e875a17f38cecabfb9dad4b
Raymond Jennings <shentino@gmail.com>
2012-09-22 16:33:51 -0700

    Finally get off my lazy butt and apply AGPL3 copyright notices to all LPC source files

--- mudlib/mud/home/System/sys/proxyd.c
+++ mudlib/mud/home/System/sys/proxyd.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /** Grants proxies
 
 A proxy is used to grant user level permissions to objects, or to allow
