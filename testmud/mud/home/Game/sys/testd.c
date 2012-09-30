43fa621b652f36922ce24be287284610bc22eb8f
Raymond Jennings <shentino@gmail.com>
2012-09-30 00:44:24 -0700

    Remove cruft from tester

--- testmud/mud/home/Game/sys/testd.c
+++ testmud/mud/home/Game/sys/testd.c
@@ -17,14 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <kotaka/paths.h>
-#include <kotaka/log.h>
-#include <kotaka/assert.h>
-#include <kotaka/privilege.h>
-#include "~/test.h"
-
-#include <status.h>
-
 static void create()
 {
 }
