f4062d33bc7733a9b9ee75572c3478d7a2180ada
Raymond Jennings <shentino@gmail.com>
2012-09-30 08:16:00 -0700

    Stub the test subsystem

--- testmud/mud/home/Test/initd.c
+++ testmud/mud/home/Test/initd.c
@@ -17,6 +17,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <kotaka/log.h>
+#include <kotaka/paths.h>
+
 static void create()
 {
+	LOGD->post_message("test", LOG_DEBUG, "Test subsystem loaded");
 }
