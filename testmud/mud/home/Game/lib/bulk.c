967bedac7bc74082a99061c7b4dbd1a572e6b8c2
Raymond Jennings <shentino@gmail.com>
2012-10-12 05:44:37 -0700

    Simplify bulk system for now, use Skotos model.

--- testmud/mud/home/Game/lib/bulk.c
+++ testmud/mud/home/Game/lib/bulk.c
@@ -19,78 +19,55 @@
  */
 #include <kotaka/paths.h>
 #include <kotaka/privilege.h>
+#include <kotaka/checkarg.h>
 
 inherit LIB_OBJECT;
 
-/* for all derived calculations, object is presumed to be a cube */
-
-/* two of three required */
-float density;		/* kg/m^3 */
+/* keep it short and sweet and to the point for now */
+float density;		/* kg/l */
 float mass;		/* kg */
-float volume;		/* m^3 (can be derived from area or length) */
-
-/* optional, derived from volume, can derive volume */
-float length;		/* m */
-float area;		/* m^2 */
-
-int flexible;
-int container;
-
-float mass_capacity;	/* kg, optional, default = cvolume * density */
-float volume_capacity;	/* m^3, optional, default = 0.9 * volume */
-float length_capacity;	/* m, optional, default = 0.9 * length */
-float area_capacity;	/* m^2, optional, default = cvolume / clength */
 
 static void create()
 {
-	mass = -1.0;
-	density = -1.0;
-	volume = -1.0;
-	area = -1.0;
-	length = -1.0;
+	/* be sensible, one kilogram */
+	density = 1.0;
+	mass = 1.0;
 }
 
-void set_base_density(float new_density)
+/* kilograms per liter */
+void set_density(float new_density)
 {
+	if (new_density <= 0.0) {
+		error("Invalid density");
+	}
+
 	density = new_density;
 }
 
-void set_base_mass(float new_mass)
+/* kilograms */
+void set_mass(float new_mass)
 {
+	if (new_mass < 0.0) {
+		error("Negative mass");
+	}
+
 	mass = new_mass;
 }
 
-void set_base_volume(float new_volume)
+/* kilograms per liter */
+float query_density()
 {
-	volume = new_volume;
+	return density;
 }
 
-void set_base_area(float new_area)
+/* kilograms */
+float query_mass()
 {
-	area = new_area;
+	return mass;
 }
 
-void set_base_length(float new_length)
+/* return volume in cubic meters */
+float query_volume()
 {
-	length = new_length;
+	return 1000.0 * mass / density;
 }
-
-/*
-Global derivation
-	mass = volume * density (if density defined and volume derivable)
-	density = mass / volume (if mass defined and volume derivable)
-	volume = mass / density (if mass and density defined)
-
-Volume derivation
-	length * area (if area defined)
-	length ^ 3 (if length defined)
-	area ^ 3/2 (if area defined)
-
-Area derivation
-	volume / length (if volume defined)
-	length ^ 2 (if length defined)
-
-Length derivation
-	volume / area (if volume and area defined)
-	volume ^ 1/3 (if volume defined)
-*/
