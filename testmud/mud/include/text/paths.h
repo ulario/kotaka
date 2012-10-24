a5861b4754f90922c729590dd913de1a27c2cf27
Raymond Jennings <shentino@gmail.com>
2012-10-24 04:00:22 -0700

    Create text/paths.h, move text objects over

---
+++ testmud/mud/include/text/paths.h
@@ -0,0 +1,12 @@
+#include <config.h>
+
+#define LIB_BIN			(USR_DIR + "/Text/lib/bin")
+#define LIB_WIZBIN		(USR_DIR + "/Text/lib/wizbin")
+#define LIB_VERB		(USR_DIR + "/Text/lib/verb")
+#define TEXT_LIB_USTATE		(USR_DIR + "/Text/lib/ustate")
+
+#define ALIASD			(USR_DIR + "/Text/sys/aliasd")
+#define BIND			(USR_DIR + "/Text/sys/bind")
+#define VERBD			(USR_DIR + "/Text/sys/verbd")
+#define TEXT_SUBD		(USR_DIR + "/Text/sys/subd")
+#define TEXT_USERD		(USR_DIR + "/Text/sys/userd")
