f46ca2c7031bd80449380d078fa5936ae10fbc13
Raymond Jennings <shentino@gmail.com>
2012-11-04 09:44:22 -0800

    whoops, wrong include file

--- testmud/mud/home/Text/lib/ustate.c
+++ testmud/mud/home/Text/lib/ustate.c
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <kotaka/paths.h>
-#include <game/paths.h>
+#include <text/paths.h>
 
 /* overrides LIB_USTATE to provide auditing */
 
