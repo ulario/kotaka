27d0be5a450e5778443bb8137e66c61a59008f15
Raymond Jennings <shentino@gmail.com>
2012-11-06 06:53:02 -0800

    Allow text subsystem to be rebooted

--- testmud/mud/home/Text/initd.c
+++ testmud/mud/home/Text/initd.c
@@ -19,6 +19,8 @@
  */
 #include <kotaka/paths.h>
 #include <kotaka/log.h>
+#include <kotaka/privilege.h>
+#include <kotaka/bigstruct.h>
 
 inherit LIB_INITD;
 inherit UTILITY_COMPILE;
@@ -29,4 +31,42 @@ static void create()
 	load_dir("sys", 1);
 
 	KERNELD->set_global_access("Text", 1);
+}
+
+void full_reset()
+{
+	object turkeylist;
+	object cursor;
+	object first;
+	object this;
+
+	ACCESS_CHECK(PRIVILEGED() || INTERFACE());
+
+	turkeylist = new_object(BIGSTRUCT_DEQUE_LWO);
+
+	this = this_object();
+	cursor = KERNELD->first_link("Text");
+	first = cursor;
+
+	do {
+		turkeylist->push_back(cursor);
+		cursor = KERNELD->next_link(cursor);
+	} while (cursor != first);
+
+	while (!turkeylist->empty()) {
+		object turkey;
+
+		turkey = turkeylist->get_front();
+		turkeylist->pop_front();
+
+		if (!turkey || turkey == this) {
+			/* don't self destruct or double destruct */
+			continue;
+		}
+
+		destruct_object(turkey);
+	}
+
+	load_dir("obj", 1);
+	load_dir("sys", 1);
 }
