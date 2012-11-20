2bade127429df43a2dbb64cff67995a666af9ba3
Raymond Jennings <shentino@gmail.com>
2012-11-20 09:19:03 -0800

    Move clone discovery logic

--- mudlib/mud/home/System/sys/cloned.c
+++ mudlib/mud/home/System/sys/cloned.c
@@ -17,3 +17,57 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*
+void discover_clones()
+{
+	ACCESS_CHECK(PRIVILEGED());
+
+	rlimits (0; -1) {
+		object indices;
+		string *owners;
+		object first;
+		object selfqueue;
+		int sz, i;
+
+		indices = PROGRAMD->query_object_indices();
+		sz = indices->get_size();
+
+		for (i = 0; i < sz; i++) {
+			object pinfo;
+			int count;
+
+			pinfo = PROGRAMD->get_element(indices->get_element(i));
+			pinfo->reset_clones();
+		}
+
+		owners = KERNELD->query_owners();
+		sz = sizeof(owners);
+
+		for (i = 0; i < sz; i++) {
+			object current;
+			int nclones;
+
+			first = KERNELD->first_link(owners[i]);
+
+			current = first;
+
+			if (!current) {
+				continue;
+			}
+
+			do {
+				if (sscanf(object_name(current), "%*s#%*d")) {
+					rqueue->push_back( ({ 1, status(current, O_INDEX), current }) );
+				}
+				current = KERNELD->next_link(current);
+				nclones++;
+			} while (current != first);
+		}
+
+		if (!in_objectd) {
+			flush_rqueue();
+		}
+	}
+}
+*/
--- mudlib/mud/home/System/sys/objectd.c
+++ mudlib/mud/home/System/sys/objectd.c
@@ -325,59 +325,6 @@ void discover_objects()
 	}
 }
 
-/*
-void discover_clones()
-{
-	ACCESS_CHECK(PRIVILEGED());
-
-	rlimits (0; -1) {
-		object indices;
-		string *owners;
-		object first;
-		object selfqueue;
-		int sz, i;
-
-		indices = PROGRAMD->query_object_indices();
-		sz = indices->get_size();
-
-		for (i = 0; i < sz; i++) {
-			object pinfo;
-			int count;
-
-			pinfo = PROGRAMD->get_element(indices->get_element(i));
-			pinfo->reset_clones();
-		}
-
-		owners = KERNELD->query_owners();
-		sz = sizeof(owners);
-
-		for (i = 0; i < sz; i++) {
-			object current;
-			int nclones;
-
-			first = KERNELD->first_link(owners[i]);
-
-			current = first;
-
-			if (!current) {
-				continue;
-			}
-
-			do {
-				if (sscanf(object_name(current), "%*s#%*d")) {
-					rqueue->push_back( ({ 1, status(current, O_INDEX), current }) );
-				}
-				current = KERNELD->next_link(current);
-				nclones++;
-			} while (current != first);
-		}
-
-		if (!in_objectd) {
-			flush_rqueue();
-		}
-	}
-}
-
 void full_reset()
 {
 	ACCESS_CHECK(PRIVILEGED() || INTERFACE());
