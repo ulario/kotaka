600c6bcdbc1a385d50b30de069431454529a702e
Raymond Jennings <shentino@gmail.com>
2012-11-20 12:19:58 -0800

    Remove obsolete canary

--- mudlib/mud/home/System/lib/auto/second_auto.c
+++ mudlib/mud/home/System/lib/auto/second_auto.c
@@ -30,25 +30,9 @@ inherit "callout_guard";
 inherit "touch";
 inherit "object";
 
-object canary;
-
 /**********/
 /* status */
 /**********/
-
-void set_canary(object new_canary)
-{
-	ACCESS_CHECK(SYSTEM());
-
-	canary = new_canary;
-}
-
-object query_canary()
-{
-	ACCESS_CHECK(SYSTEM());
-
-	return canary;
-}
 
 nomask void _F_dummy()
 {
