4c0bea4ee87a5a4541a4ac01791cec70be936c46
Raymond Jennings <shentino@gmail.com>
2012-11-22 21:27:59 -0800

    Purge header files, we should rebuild it from scratch anyway

--- mudlib/mud/home/Kotaka/lib/object.c
+++ mudlib/mud/home/Kotaka/lib/object.c
@@ -17,15 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <type.h>
-
-#include <kotaka/privilege.h>
-#include <kotaka/paths.h>
-#include <kotaka/assert.h>
-#include <kotaka/checkarg.h>
-#include <kotaka/property.h>
-#include <kotaka/log.h>
-
 /*************/
 /* Variables */
 /*************/
