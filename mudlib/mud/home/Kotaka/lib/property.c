4c3bccad323f169240d00e06c0b3265dabc127eb
Raymond Jennings <shentino@gmail.com>
2012-11-22 21:44:48 -0800

    Added some header files to property section

--- mudlib/mud/home/Kotaka/lib/property.c
+++ mudlib/mud/home/Kotaka/lib/property.c
@@ -17,7 +17,11 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <kotaka/paths.h>
 #include <kotaka/privilege.h>
+#include <kotaka/property.h>
+
+#include <type.h>
 
 inherit "declaration";
 
