25c10642d8fd6b469361f1a29cdaca1d4fe3b1c8
Raymond Jennings <shentino@gmail.com>
2012-11-22 22:11:44 -0800

    Move inventory handling logic

--- mudlib/mud/home/Kotaka/lib/inventory.c
+++ mudlib/mud/home/Kotaka/lib/inventory.c
@@ -23,6 +23,10 @@
 private object environment;
 private object *inventory;
 
+/**********************/
+/* Inventory handling */
+/**********************/
+
 static void initialize_inventory()
 {
 	ACCESS_CHECK(previous_program() == LIB_OBJECT);
@@ -30,4 +34,182 @@ static void initialize_inventory()
 	if (!inventory) {
 		inventory = ({ });
 	}
+}
+
+int forbid_move(object new_env)
+{
+	return 0;
+}
+
+int forbid_insert(object obj)
+{
+	return 0;
+}
+
+int forbid_remove(object obj)
+{
+	return 0;
+}
+
+void move_notify(object old_env)
+{
+}
+
+void insert_notify(object obj)
+{
+}
+
+void remove_notify(object obj)
+{
+}
+
+nomask object _F_query_environment()
+{
+	ACCESS_CHECK(KOTAKA());
+
+	return environment;
+}
+
+nomask object *_F_query_inventory()
+{
+	ACCESS_CHECK(KOTAKA());
+
+	return inventory - ({ nil });
+}
+
+nomask void _F_add_inventory(object arriving)
+{
+	ACCESS_CHECK(KOTAKA());
+	ASSERT(arriving);
+	ASSERT(!sizeof( ({ arriving }) & inventory ));
+
+	inventory = ({ arriving }) + (inventory - ({ nil }));
+}
+
+nomask void _F_del_inventory(object departing)
+{
+	ACCESS_CHECK(KOTAKA());
+	ASSERT(departing);
+
+	inventory -= ({ departing });
+}
+
+nomask int _F_is_container_of(object test)
+{
+	object env;
+	object this;
+	int steps;
+
+	ACCESS_CHECK(KOTAKA());
+
+	this = this_object();
+	env = test->_F_query_environment();
+
+	while (env) {
+		if (env == this) {
+			return 1;
+		}
+
+		steps++;
+
+		if (steps > 100) {
+			error("Nested too deeply");
+		}
+
+		env = env->_F_query_environment();
+	}
+
+	return 0;
+}
+
+nomask int _F_depth()
+{
+	if (environment) {
+		return environment->_F_depth() + 1;
+	} else {
+		return 0;
+	}
+}
+
+atomic nomask void _F_move(object new_env)
+{
+	object this;
+	string base;
+	int new_number;
+
+	ACCESS_CHECK(KOTAKA());
+
+	CHECKARG(!new_env || new_env <- LIB_OBJECT, 1, "move");
+
+	this = this_object();
+
+	if (new_env) {
+		if (new_env == this) {
+			error("Self containment");
+		}
+
+		if (_F_is_container_of(new_env)) {
+			error("Recursive containment");
+		}
+
+		if (new_env->_F_depth() >= 50) {
+			error("Nested too deeply");
+		}
+	}
+
+	if (new_env == environment) {
+		return;
+	}
+
+	if (environment) {
+		environment->_F_del_inventory(this_object());
+	}
+	environment = new_env;
+	if (environment) {
+		environment->_F_add_inventory(this_object());
+	}
+}
+
+/* high */
+
+void move(object new_env)
+{
+	object old_env;
+	object this;
+
+	old_env = environment;
+
+	this = this_object();
+
+	PERMISSION_CHECK(!old_env || !old_env->forbid_remove(this));
+	PERMISSION_CHECK(!forbid_move(new_env));
+	PERMISSION_CHECK(!new_env || !new_env->forbid_insert(this));
+
+	_F_move(new_env);
+
+	if (old_env) {
+		old_env->remove_notify(this);
+	}
+	move_notify(old_env);
+	if (new_env) {
+		new_env->insert_notify(this);
+	}
+}
+
+int is_container_of(object test)
+{
+	CHECKARG(test, 1, "is_container_of");
+	CHECKARG(test <- LIB_OBJECT, 1, "is_container_of");
+
+	return _F_is_container_of(test);
+}
+
+object query_environment()
+{
+	return environment;
+}
+
+object *query_inventory()
+{
+	return _F_query_inventory();
 }
