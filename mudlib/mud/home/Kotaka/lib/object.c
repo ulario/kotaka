67dd4a8526e52421a51707d7225302cc5a5aa8ee
Raymond Jennings <shentino@gmail.com>
2012-11-22 21:53:12 -0800

    Readded headers to object section

--- mudlib/mud/home/Kotaka/lib/object.c
+++ mudlib/mud/home/Kotaka/lib/object.c
@@ -17,6 +17,11 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <kotaka/assert.h>
+#include <kotaka/checkarg.h>
+#include <kotaka/paths.h>
+#include <kotaka/privilege.h>
+
 inherit property "property";
 
 /*************/
