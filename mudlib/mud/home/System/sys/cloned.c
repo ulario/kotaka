3a639c09b491819c65f64fad562e451746f6b9ea
Raymond Jennings <shentino@gmail.com>
2012-11-23 20:46:25 -0800

    Back to drawing board.  Leave in place as stub for now

--- mudlib/mud/home/System/sys/cloned.c
+++ mudlib/mud/home/System/sys/cloned.c
@@ -27,144 +27,18 @@
 
 inherit SECOND_AUTO;
 
-object db;
-object queue;
-int depth;
-
-mapping kclones;	/* tracker for kernel clones */
-
-private void flush();
-private void discover_clones();
-
 static void create()
 {
-	depth++;
-
-	kclones = ([ ]);
-	queue = new_object(BIGSTRUCT_DEQUE_LWO);
-	db = clone_object(BIGSTRUCT_MAP_OBJ);
-	db->set_type(T_INT);
-
-	depth--;
-
-	discover_clones();
-	flush();
-}
-
-private void flush()
-{
-	while (!queue->empty()) {
-		object first;
-		object next;
-		int ad;
-		int index;
-		int kernel;
-		int count;
-		object obj;
-
-		({ ad, obj }) = queue->get_front();
-		queue->pop_front();
-
-		if (!obj) {
-			continue;
-		}
-
-		index = status(obj, O_INDEX);
-		first = db->get_element(index);
-
-		kernel = sscanf(object_name(obj), "/kernel/%*s");
-
-		depth++;
-
-		if (ad) {
-			if (kernel) {
-				if (!kclones[index]) {
-					kclones[index] = ([ ]);
-				}
-
-				kclones[index][obj] = 1;
-			} else {
-				if (first) {
-					next = first->query_next_clone();
-				} else {
-					db->set_element(status(obj, O_INDEX), obj);
-					first = obj;
-					next = obj;
-				}
-
-				next->set_prev_clone(obj);
-				first->set_next_clone(obj);
-				obj->set_prev_clone(first);
-				obj->set_next_clone(next);
-			}
-		} else {
-			if (kernel) {
-				kclones[index][obj] = nil;
-
-				if (!map_sizeof(kclones[index])) {
-					kclones[index] = nil;
-				}
-			} else {
-				next = first->query_next_clone();
-
-				if (first == next) {
-					db->set_element(index, nil);
-				} else {
-					object prev;
-
-					if (first == obj) {
-						first = next;
-					}
-
-					next = obj->query_next_clone();
-					prev = obj->query_prev_clone();
-
-					prev->set_next_clone(next);
-					next->set_prev_clone(prev);
-				}
-			}
-		}
-
-		depth--;
-	}
 }
 
 void add_clone(object obj)
 {
 	ACCESS_CHECK(previous_program() == OBJECTD);
-
-	if (!db) {
-		return;
-	}
-
-	depth++;
-
-	queue->push_back( ({ 1, obj }) );
-
-	depth--;
-
-	if (depth == 0) {
-		flush();
-	}
 }
 
 void remove_clone(object obj)
 {
 	ACCESS_CHECK(previous_program() == OBJECTD);
-
-	if (!db) {
-		return;
-	}
-
-	depth++;
-
-	queue->push_back( ({ 0, obj }) );
-
-	depth--;
-
-	if (depth == 0) {
-		flush();
-	}
 }
 
 private void discover_clones()
@@ -191,7 +65,7 @@ private void discover_clones()
 				name = object_name(obj);
 
 				if (sscanf(name, "%*s#%*d")) {
-					queue->push_back( ({ 1, obj }) );
+					// deal with clone here
 					count++;
 				}
 				obj = KERNELD->next_link(obj);
