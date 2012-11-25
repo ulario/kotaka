8a487679a317b364d0a348962b3c274f2b13cd5a
Raymond Jennings <shentino@gmail.com>
2012-11-25 15:45:03 -0800

    Remove redundant command function

--- testmud/mud/home/Text/lib/ustate.c
+++ testmud/mud/home/Text/lib/ustate.c
@@ -36,22 +36,3 @@ static void terminate_account_state()
 		pop_state();
 	}
 }
-
-int execute_command(string command, string args)
-{
-	if (BIND->execute_command("adm/" + command, args))
-		return 1;
-	if (BIND->execute_command("wiz/tool/" + command, args))
-		return 1;
-	if (BIND->execute_command("wiz/debug/" + command, args))
-		return 1;
-	if (BIND->execute_command("wiz/" + command, args))
-		return 1;
-	if (BIND->execute_command("test/" + command, args))
-		return 1;
-	if (BIND->execute_command("movie/" + command, args))
-		return 1;
-	if (BIND->execute_command(command, args))
-		return 1;
-	return 0;
-}
