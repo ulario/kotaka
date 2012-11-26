cc4fdac7e363b4ecf2f53b47062d4ebaffd8d97b
Raymond Jennings <shentino@gmail.com>
2012-11-25 16:08:07 -0800

    Alphabetize paths.h for text

--- testmud/mud/include/text/paths.h
+++ testmud/mud/include/text/paths.h
@@ -3,17 +3,17 @@
 /* lib */
 
 #define LIB_BIN			(USR_DIR + "/Text/lib/bin")
-#define LIB_WIZBIN		(USR_DIR + "/Text/lib/wizbin")
 #define LIB_VERB		(USR_DIR + "/Text/lib/verb")
+#define LIB_WIZBIN		(USR_DIR + "/Text/lib/wizbin")
 #define TEXT_LIB_USTATE		(USR_DIR + "/Text/lib/ustate")
 
 /* sys */
 
 #define ALIASD			(USR_DIR + "/Text/sys/aliasd")
 #define BIND			(USR_DIR + "/Text/sys/bind")
-#define VERBD			(USR_DIR + "/Text/sys/verbd")
 #define TEXT_SUBD		(USR_DIR + "/Text/sys/subd")
 #define TEXT_USERD		(USR_DIR + "/Text/sys/userd")
+#define VERBD			(USR_DIR + "/Text/sys/verbd")
 
 /* lwos */
 
