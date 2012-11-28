4a607bd0ed2399bc325f3bbe2f6cedade5a9df77
Raymond Jennings <shentino@gmail.com>
2012-11-27 16:32:42 -0800

    Grant global access to account

--- testmud/mud/home/Account/initd.c
+++ testmud/mud/home/Account/initd.c
@@ -25,6 +25,8 @@ inherit UTILITY_COMPILE;
 
 static void create()
 {
+	KERNELD->set_global_access("Account", 1);
+
 	load_dir("sys");
 
 	LOGD->post_message("account", LOG_DEBUG, "Hello world from the account subsystem");
