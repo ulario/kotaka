802c23110e1bb552619ab8a4e1a7e864fdb4b486
Raymond Jennings <shentino@gmail.com>
2012-12-04 23:44:19 -0800

    Add test of include and inherit database

--- mudlib/mud/home/System/sys/testd.c
+++ mudlib/mud/home/System/sys/testd.c
@@ -38,7 +38,7 @@ static void create()
 	tls::create();
 }
 
-static void tls_test(varargs mixed args...)
+static void tls_test()
 {
 	set_tlvar(0, "foobar");
 
@@ -47,9 +47,91 @@ static void tls_test(varargs mixed args...)
 	LOGD->post_message("test", LOG_DEBUG, "TLS on callout test passed");
 }
 
+private void dump_progdb()
+{
+	object ind;
+	int sz, i;
+
+	ind = PROGRAMD->query_program_indices();
+	sz = ind->get_size();
+
+	for (i = 0; i < sz; i++) {
+		int oindex;
+		object pinfo;
+		string buf;
+		int ssz, j;
+
+		mixed arr;
+
+		oindex = ind->get_element(i);
+		pinfo = PROGRAMD->query_program_info(oindex);
+
+		buf = "Program info for " + oindex + "\n";
+		buf += "Name: " + pinfo->query_path() + "\n";
+		buf += "Inherits:\n";
+
+		arr = pinfo->query_inherits();
+		ssz = sizeof(arr);
+
+		for (j = 0; j < ssz; j++) {
+			buf += "    " + PROGRAMD->query_program_info(arr[j])->query_path() + "\n";
+		}
+
+		buf += "Includes:\n";
+
+		arr = pinfo->query_includes();
+		ssz = sizeof(arr);
+
+		for (j = 0; j < ssz; j++) {
+			buf += "    " + arr[j] + "\n";
+		}
+
+		arr = PROGRAMD->query_inheriters(oindex);
+
+		if (arr) {
+			buf += "Inherited by:\n";
+
+			ssz = arr->get_size();;
+
+			for (j = 0; j < ssz; j++) {
+				buf += "    " + PROGRAMD->query_program_info(
+					arr->get_element(j)
+				)->query_path() + "\n";
+			}
+		}
+
+		LOGD->post_message("test", LOG_DEBUG, buf);
+	}
+
+	ind = PROGRAMD->query_includer_indices();
+	sz = ind->get_size();
+
+	for (i = 0; i < sz; i++) {
+		object arr;
+		string buf;
+		int j, ssz;
+
+		buf = "Include file " + ind->get_element(i) + "\n";
+		buf += "Included by:\n";
+
+		arr = PROGRAMD->query_includers(ind->get_element(i));
+		ssz = arr->get_size();
+
+		for (j = 0; j < ssz; j++) {
+			buf += "    " + PROGRAMD->query_program_info(arr->get_element(j))->query_path() + "\n";
+		}
+
+		LOGD->post_message("test", LOG_DEBUG, buf);
+	}
+}
+
 void test()
 {
 	string err;
 
 	ACCESS_CHECK(SYSTEM());
+
+	dump_progdb();
+
+	call_out("tls_test", 0);
 }
