6771a929000478b548d12891a221d31d6a7c9682
Raymond Jennings <shentino@gmail.com>
2013-03-14 16:21:40 -0700

    Remove sanity checking code from trusted functions

--- mudlib/mud/home/Kotaka/lib/archetype.c
+++ mudlib/mud/home/Kotaka/lib/archetype.c
@@ -67,30 +67,9 @@ nomask object *kotaka_query_archetypes()
 
 nomask void kotaka_set_archetypes(object *new_archs)
 {
-	int i;
-	int sz;
-
-	object *old_archs;
-	object *check;
-
 	ACCESS_CHECK(KOTAKA());
 
-	archetypes -= ({ nil });
-	new_archs = new_archs -= ({ nil });
-	check = new_archs - ({ archetypes });
-
-	sz = sizeof(check);
-
-	for (i = 0; i < sz; i++) {
-		if (!check[i] <- LIB_OBJECT) {
-			error("Bad argument 1 for function set_archetypes (found non LIB_OBJECT)");
-		}
-		if (kotaka_is_archetype_of(check[i])) {
-			error("Circular reference");
-		}
-	}
-
-	archetypes = new_archs;
+	archetypes = new_archs - ({ nil });
 }
 
 nomask void kotaka_clear_archetypes()
@@ -102,14 +81,7 @@ nomask void kotaka_clear_archetypes()
 
 nomask void kotaka_add_archetype(object new_arch)
 {
-	CHECKARG(new_arch, 1, "add_archetype");
-	CHECKARG(new_arch <- LIB_OBJECT, 1, "add_archetype");
-
 	ACCESS_CHECK(KOTAKA());
-
-	if (kotaka_is_archetype_of(new_arch)) {
-		error("Circular reference");
-	}
 
 	archetypes -= ({ nil });
 	archetypes += ({ new_arch });
@@ -117,12 +89,6 @@ nomask void kotaka_add_archetype(object new_arch)
 
 nomask void kotaka_add_archetype_at(object new_arch, int position)
 {
-	CHECKARG(new_arch, 1, "add_archetype_at");
-	CHECKARG(new_arch <- LIB_OBJECT, 1, "add_archetype_at");
-
-	CHECKARG(position >= -1, 2, "add_archetype_at");
-	CHECKARG(position <= sizeof(archetypes), 2, "add_archetype_at");
-
 	ACCESS_CHECK(KOTAKA());
 
 	archetypes -= ({ nil });
