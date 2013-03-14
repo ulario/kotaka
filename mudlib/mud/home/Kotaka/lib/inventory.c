cf565bcd22d3eac12debcc8ad11337894fa21f99
Raymond Jennings <shentino@gmail.com>
2013-03-14 16:23:35 -0700

    Add trusted inventory functions

--- mudlib/mud/home/Kotaka/lib/inventory.c
+++ mudlib/mud/home/Kotaka/lib/inventory.c
@@ -61,6 +61,137 @@ void remove_notify(object obj)
 {
 }
 
+/* trusted */
+
+nomask object kotaka_query_environment()
+{
+	return environment;
+}
+
+nomask object *kotaka_query_inventory()
+{
+	inventory -= ({ nil });
+
+	return inventory[..];
+}
+
+nomask void kotaka_add_inventory(object arriving)
+{
+	ASSERT(arriving);
+	ASSERT(!sizeof( ({ arriving }) & inventory ));
+
+	ACCESS_CHECK(KOTAKA());
+
+	inventory = ({ arriving }) + (inventory - ({ nil }));
+}
+
+nomask void kotaka_del_inventory(object departing)
+{
+	ASSERT(departing);
+
+	ACCESS_CHECK(KOTAKA());
+
+	inventory -= ({ departing });
+}
+
+nomask int kotaka_is_container_of(object test)
+{
+	object env;
+	object this;
+	int steps;
+
+	this = this_object();
+	env = test->query_environment();
+
+	while (env) {
+		if (env == this) {
+			return 1;
+		}
+
+		steps++;
+
+		if (steps > 100) {
+			error("Nested too deeply");
+		}
+
+		env = env->query_environment();
+	}
+
+	return 0;
+}
+
+nomask int kotaka_query_depth()
+{
+	object obj;
+	int depth;
+
+	obj = environment;
+
+	while (obj) {
+		depth++;
+
+		obj = obj->query_environment();
+	}
+
+	return depth;
+}
+
+nomask void kotaka_move(object new_env)
+{
+	object old_env;
+	object this;
+
+	CHECKARG(!new_env || new_env <- LIB_OBJECT, 1, "move");
+
+	old_env = environment;
+
+	this = this_object();
+
+	PERMISSION_CHECK(!old_env || !old_env->forbid_remove(this));
+	PERMISSION_CHECK(!forbid_move(new_env));
+	PERMISSION_CHECK(!new_env || !new_env->forbid_insert(this));
+
+	if (new_env == environment) {
+		return;
+	}
+
+	if (new_env) {
+		if (new_env == this) {
+			error("Self containment");
+		}
+
+		if (is_container_of(new_env)) {
+			error("Recursive containment");
+		}
+
+		if (new_env->query_depth() >= 50) {
+			error("Nested too deeply");
+		}
+	}
+
+	if (environment) {
+		environment->kotaka_del_inventory(this_object());
+	}
+
+	environment = new_env;
+
+	if (environment) {
+		environment->kotaka_add_inventory(this_object());
+	}
+
+	if (old_env) {
+		old_env->remove_notify(this);
+	}
+
+	move_notify(old_env);
+
+	if (new_env) {
+		new_env->insert_notify(this);
+	}
+}
+
+/* untrusted */
+
 nomask object query_environment()
 {
 	return environment;
@@ -73,7 +204,7 @@ nomask object *query_inventory()
 	return inventory[..];
 }
 
-nomask void add_inventory(object arriving)
+nomask void kotaka_add_inventory(object arriving)
 {
 	ASSERT(arriving);
 	ASSERT(!sizeof( ({ arriving }) & inventory ));
@@ -83,7 +214,7 @@ nomask void add_inventory(object arriving)
 	inventory = ({ arriving }) + (inventory - ({ nil }));
 }
 
-nomask void del_inventory(object departing)
+nomask void kotaka_del_inventory(object departing)
 {
 	ASSERT(departing);
 
@@ -168,17 +299,21 @@ void move(object new_env)
 	}
 
 	if (environment) {
-		environment->del_inventory(this_object());
+		environment->kotaka_del_inventory(this_object());
 	}
+
 	environment = new_env;
+
 	if (environment) {
-		environment->add_inventory(this_object());
+		environment->kotaka_add_inventory(this_object());
 	}
 
 	if (old_env) {
 		old_env->remove_notify(this);
 	}
+
 	move_notify(old_env);
+
 	if (new_env) {
 		new_env->insert_notify(this);
 	}
