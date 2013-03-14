d0decaac5588da6052c237f7e48e68b98d75a6d7
Raymond Jennings <shentino@gmail.com>
2013-03-13 18:20:22 -0700

    Remove flowers from grass

--- testmud/mud/home/Game/sys/handler/paint/grass.c
+++ testmud/mud/home/Game/sys/handler/paint/grass.c
@@ -37,38 +37,21 @@ void on_paint_text(object gc, object obj, object viewer)
 		for (mx = -8; mx <= 8; mx++) {
 			gc->move_pen(mx, my);
 
-			if (SUBD->rnd() < 0.1) {
-				/* flower */
-				switch(random(3)) {
-				case 0: /* white */
-					gc->set_color(0x2F);
-					break;
-				case 1: /* yellow */
-					gc->set_color(0x28 | GREEN | RED);
-					break;
-				case 2: /* magenta */
-					gc->set_color(0x28 | BLUE | RED);
-					break;
-				}
-				gc->draw("*");
-			} else {
-				/* grass */
-				switch(random(2)) {
-				case 0:
-					gc->set_color(0x20);
-					break;
-				case 1:
-					gc->set_color(0x2A);
-					break;
-				}
-				switch(random(2)) {
-				case 0:
-					gc->draw(",");
-					break;
-				case 1:
-					gc->draw("'");
-					break;
-				}
+			switch(random(2)) {
+			case 0:
+				gc->set_color(0x20);
+				break;
+			case 1:
+				gc->set_color(0x2A);
+				break;
+			}
+			switch(random(2)) {
+			case 0:
+				gc->draw(",");
+				break;
+			case 1:
+				gc->draw("'");
+				break;
 			}
 		}
 	}
