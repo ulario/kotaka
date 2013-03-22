5cb12fb3c7c1194de9a8bdf38c3e81eed77e2add
Raymond Jennings <shentino@gmail.com>
2013-03-22 15:16:46 -0700

    Forgot to add lib_initd

--- testmud/mud/home/Directory/initd.c
+++ testmud/mud/home/Directory/initd.c
@@ -17,3 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <kotaka/paths.h>
+
+inherit LIB_INITD;
