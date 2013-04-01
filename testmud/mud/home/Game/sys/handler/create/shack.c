2d16476201b773916941acb0afd8a6bdb2e280fc
Raymond Jennings <shentino@gmail.com>
2013-04-01 03:33:00 -0700

    Update copyright dates on files changed in 2013

--- testmud/mud/home/Game/sys/handler/create/shack.c
+++ testmud/mud/home/Game/sys/handler/create/shack.c
@@ -2,7 +2,7 @@
  * This file is part of Kotaka, a mud library for DGD
  * http://github.com/shentino/kotaka
  *
- * Copyright (C) 2012  Raymond Jennings
+ * Copyright (C) 2012-2013  Raymond Jennings
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
