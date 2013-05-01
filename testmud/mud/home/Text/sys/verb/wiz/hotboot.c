64e2b3725925c5ccd9d587f6810ab47232893cc1
Raymond Jennings <shentino@gmail.com>
2013-04-30 22:35:45 -0700

    Readd hotboot verb accidentally removed earlier

---
+++ testmud/mud/home/Text/sys/verb/wiz/hotboot.c
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths.h>
+
+#include <account/paths.h>
+#include <text/paths.h>
+#include <game/paths.h>
+
+inherit LIB_VERB;
+
+void main(object actor, string args)
+{
+	object proxy;
+
+	if (query_user()->query_class() < 3) {
+		send_out("Only an administrator can hotboot the mud.\n");
+		return;
+	}
+
+	proxy = PROXYD->get_proxy(query_user()->query_name());
+
+	proxy->dump_state();
+	proxy->shutdown(1);
+}
