8149a7fc0653c8637444828f75393abb0bf6d43d
Raymond Jennings <shentino@gmail.com>
2013-05-26 00:05:37 -0700

    Add convenient configuration tool for in game objects

---
+++ testmud/mud/home/Game/obj/ustate/objconf.c
@@ -0,0 +1,143 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <text/paths.h>
+#include <kotaka/paths.h>
+#include <kotaka/privilege.h>
+
+inherit TEXT_LIB_USTATE;
+
+int reading;
+int stopped;
+int dead;
+
+object obj;
+
+static void create(int clone)
+{
+	::create();
+}
+
+static void destruct(int clone)
+{
+	::destruct();
+}
+
+private void prompt()
+{
+	send_out("[\033[1;34mobject configuration\033[0m] ");
+}
+
+void set_object(object o)
+{
+	ACCESS_CHECK(GAME());
+
+	obj = o;
+}
+
+void begin()
+{
+	ACCESS_CHECK(previous_object() == query_user());
+}
+
+void stop()
+{
+	ACCESS_CHECK(previous_object() == query_user());
+
+	stopped = 1;
+}
+
+void go()
+{
+	ACCESS_CHECK(previous_object() == query_user());
+
+	stopped = 0;
+
+	if (!reading) {
+		prompt();
+	}
+}
+
+void pre_end()
+{
+	ACCESS_CHECK(previous_object() == query_user());
+
+	dead = 1;
+}
+
+void end()
+{
+	ACCESS_CHECK(previous_object() == query_user());
+
+	destruct_object(this_object());
+}
+
+private void do_pset(string input)
+{
+	string pname;
+	string pvalue;
+	mixed value;
+
+	if (input == nil) {
+		send_out("Property name?\n");
+		return;
+	}
+
+	if (!sscanf(input, "%s %s", pname, pvalue)) {
+		send_out("Property value?\n");
+		return;
+	}
+
+	obj->set_property(pname, PARSE_VALUE->parse(pvalue));
+}
+
+void receive_in(string input)
+{
+	string args;
+	string first;
+
+	ACCESS_CHECK(previous_object() == query_user());
+
+	if (!obj) {
+		send_out("The object you were configuring no longer exists.\n");
+		pop_state();
+		return;
+	}
+
+	reading = 1;
+
+	if (!sscanf(input, "%s %s", first, args)) {
+		first = input;
+	}
+
+	switch(first) {
+	case "quit":
+		pop_state();
+		return;
+	case "pset":
+		do_pset(args);
+		break;
+	}
+
+	reading = 0;
+
+	if (!stopped) {
+		prompt();
+	}
+}
---
+++ testmud/mud/home/Game/sys/verb/ooc/configure.c
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths.h>
+#include <text/paths.h>
+#include <type.h>
+
+inherit LIB_ENGVERB;
+
+/* ({ role, prepositions, raw }) */
+mixed **query_roles()
+{
+	return ({ });
+}
+
+void do_action(object actor, mapping roles, string evoke)
+{
+	object configure;
+	mixed dob;
+
+	if (!actor) {
+		send_out("You must be in character to use this command.\n");
+		return;
+	}
+
+	dob = roles["dob"];
+
+	if (!dob) {
+		send_out("Configure what?\n");
+		return;
+	}
+
+	if (typeof(dob) == T_STRING) {
+		send_out(dob + "\n");
+		return;
+	}
+
+	if (dob[0]) {
+		send_out("Your grammar stinks.\n");
+		return;
+	}
+
+	dob = dob[1];
+
+	send_out("You start to configure " + TEXT_SUBD->generate_brief_definite(dob) + ".\n");
+
+	configure = clone_object("~/obj/ustate/objconf");
+	configure->set_object(dob);
+	query_ustate()->push_state(configure);
+}
