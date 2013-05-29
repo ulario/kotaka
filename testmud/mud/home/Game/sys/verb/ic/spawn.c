10a0e92f0a0f86fb4464bff201e620184266ee6c
Raymond Jennings <shentino@gmail.com>
2013-05-28 20:59:53 -0700

    Add spawn verb

---
+++ testmud/mud/home/Game/sys/verb/ic/spawn.c
@@ -0,0 +1,49 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/assert.h>
+#include <kotaka/paths.h>
+#include <text/paths.h>
+#include <game/paths.h>
+
+inherit LIB_RAWVERB;
+
+void main(object actor, string args)
+{
+	object master;
+	object thing;
+
+	if (!actor) {
+		send_out("You must be in character to use this command.\n");
+		return;
+	}
+
+	master = CATALOGD->lookup_object(args);
+
+	if (!master) {
+		send_out("No such object: " + args + ".\n");
+		return;
+	}
+
+	send_out("You spawn " + TEXT_SUBD->generate_brief_indefinite(master) + ".\n");
+
+	thing = GAME_INITD->create_object();
+	thing->add_archetype(master);
+	thing->move(actor);
+}
