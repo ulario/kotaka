c4894af3675c05b279102e6b84c3adbd774dbf8e
Raymond Jennings <shentino@gmail.com>
2013-06-12 16:07:51 -0700

    Debug

--- mudlib/mud/home/System/sys/testd.c
+++ mudlib/mud/home/System/sys/testd.c
@@ -163,6 +163,8 @@ private void verify_objregd_owner(string owner)
 			break;
 		}
 	} while (!seen[obj]);
+
+	LOGD->post_message("system", LOG_DEBUG, "ObjRegD check passed for " + owner);
 }
 
 static void verify_objregd()
