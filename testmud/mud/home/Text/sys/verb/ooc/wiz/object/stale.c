fa65679028f117d75a8621265d49c44d74ffb834
Raymond Jennings <shentino@gmail.com>
2013-06-12 13:34:19 -0700

    Add stale object detector

---
+++ testmud/mud/home/Text/sys/verb/ooc/wiz/object/stale.c
@@ -0,0 +1,59 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths.h>
+#include <text/paths.h>
+#include <status.h>
+
+inherit LIB_RAWVERB;
+
+void main(object actor, string args)
+{
+	/* Report all objects inheriting a destructed inheritable */
+	object indices;
+	int i, sz;
+
+	if (query_user()->query_class() < 2) {
+		send_out("Only a wizard can do that.\n");
+		return;
+	}
+
+	indices = PROGRAMD->query_program_indices();
+	sz = indices->get_size();
+
+	for (i = 0; i < sz; i++) {
+		int pindex;
+		object pinfo;
+		int *libs;
+		int ssz, j;
+
+		pindex = indices->get_element(i);
+		pinfo = PROGRAMD->query_program_info(pindex)
+		libs = pinfo->get_inherits();
+
+		ssz = sizeof(libs);
+
+		for (j = 0; j < ssz; j++) {
+			if (PROGRAMD->query_program_info(libs[j])->is_destructed()) {
+				send_out(pinfo->query_path() + "\n");
+				continue;
+			}
+		}
+	}
+}
