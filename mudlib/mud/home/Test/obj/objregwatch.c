70a9c1c74993cb3513630e4f031939efc30741a6
Raymond Jennings <shentino@gmail.com>
2013-06-24 17:13:18 -0700

    Restore ObjRegD sanity tester

---
+++ mudlib/mud/home/Test/obj/objregwatch.c
@@ -0,0 +1,73 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kernel/kernel.h>
+#include <kotaka/paths.h>
+
+string owner;
+object cursor;
+object master;
+
+static void create(int clone)
+{
+	if (clone) {
+		call_out("check", 0);
+	}
+}
+
+void activate(object first)
+{
+	master = find_object(USR_DIR + "/Test/sys/objregwatchd");
+
+	if (previous_object() != master) {
+		error("Access denied");
+	}
+
+	cursor = first;
+
+	owner = first->query_owner();
+}
+
+static void check()
+{
+	if (!master || !owner) {
+		destruct_object(this_object());
+
+		return;
+	}
+
+	if (!cursor) {
+		cursor = KERNELD->first_link(owner);
+
+		if (!cursor) {
+			destruct_object(this_object());
+
+			return;
+		}
+	}
+
+	call_out("check", 0.1);
+
+	cursor = KERNELD->next_link(cursor);
+
+	if (!cursor) {
+		DRIVER->message("Fatal error: ObjRegD corruption for " + owner);
+		shutdown();
+	}
+}
---
+++ mudlib/mud/home/Test/sys/objregwatchd.c
@@ -0,0 +1,63 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths.h>
+
+mapping trackers; /* ([ owner : tracker ]) */
+
+static void create()
+{
+	trackers = ([ ]);
+
+	call_out("check", 0);
+}
+
+static void check()
+{
+	string *owners;
+	int sz, i;
+
+	owners = KERNELD->query_owners();
+
+	sz = sizeof(owners);
+
+	for (i = 0; i < sz; i++) {
+		string owner;
+		object first;
+		object tracker;
+
+		owner = owners[i];
+
+		if (trackers[owner]) {
+			continue;
+		}
+
+		first = KERNELD->first_link(owner);
+
+		if (!first) {
+			continue;
+		}
+
+		tracker = clone_object("~/obj/objregwatch");
+
+		trackers[owner] = tracker;
+
+		tracker->activate(first);
+	}
+}
