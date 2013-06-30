731bfc8170974cefda4394640c62829756262c2e
Raymond Jennings <shentino@gmail.com>
2013-06-30 14:52:46 -0700

    Add command to destroy objects, usable only by administrators for now

---
+++ mudlib/mud/home/Game/sys/verb/wiz/ic/destroy.c
@@ -0,0 +1,72 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths.h>
+#include <text/paths.h>
+#include <type.h>
+
+inherit LIB_ENGVERB;
+
+/* ({ role, prepositions, raw }) */
+mixed **query_roles()
+{
+	return ({ });
+}
+
+void do_action(object actor, mapping roles, string evoke)
+{
+	object configure;
+	mixed dob;
+
+	if (query_user()->query_class() < 3) {
+		send_out("You must be an administrator to use this command.\n");
+		return;
+	}
+
+	if (!actor) {
+		send_out("You must be in character to use this command.\n");
+		return;
+	}
+
+	dob = roles["dob"];
+
+	if (!dob) {
+		send_out("Destroy what?\n");
+		return;
+	}
+
+	if (typeof(dob) == T_STRING) {
+		send_out(dob + "\n");
+		return;
+	}
+
+	if (dob[0]) {
+		send_out("Your grammar stinks.\n");
+		return;
+	}
+
+	dob = dob[1];
+
+	if (sizeof(dob->query_inventory())) {
+		send_out(TEXT_SUBD->generate_brief_definite(dob) + " is not empty.\n");
+		return;
+	}
+
+	destruct_object(dob);
+}
