d43642d53c379ec40d8c89f1503b0b34a07269f4
Raymond Jennings <shentino@gmail.com>
2013-07-19 03:53:28 -0700

    Add general property command

---
+++ mudlib/mud/home/Text/sys/verb/wiz/ooc/property/pop.c
@@ -0,0 +1,106 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths.h>
+#include <account/paths.h>
+#include <game/paths.h>
+#include <text/paths.h>
+#include <thing/paths.h>
+
+inherit LIB_RAWVERB;
+
+void main(object actor, string args)
+{
+	string *users;
+	object user;
+	object obj;
+	string oname;
+	string pname;
+	string operatur;
+	mixed pvalue;
+	mixed *pinfo;
+
+	user = query_user();
+
+	if (user->query_class() < 2) {
+		send_out("You do not have sufficient access rights to set object properties.\n");
+		return;
+	}
+
+	if (sscanf(args, "%s %s %s %s", operatur, oname, pname, pvalue) != 4) {
+		send_out("Usage: pop <operator> <object> <property name> <value>\n");
+		return;
+	}
+
+	if (!sizeof( ({ operatur })
+		& ({ "add", "sub", "mul", "div", "radd", "and", "or", "xor" }) )
+	) {
+		send_out("Invalid operator.\nPlease use add, sub, mul, div, radd, and, or, or xor.\n");
+		return;
+	}
+
+	obj = find_object(oname);
+
+	if (!obj) {
+		obj = CATALOGD->lookup_object(oname);
+	}
+
+	if (!obj) {
+		send_out(oname + ": No such object.\n");
+		return;
+	}
+
+	pinfo = PROPERTYD->query_property(pname);
+
+	if (!pinfo) {
+		send_out(pname + ": No such property.\n");
+		return;
+	}
+
+	pvalue = PARSE_VALUE->parse(pvalue);
+
+	switch(operatur) {
+	case "add":
+		obj->set_property(pname, obj->query_property(pname) + pvalue);
+		break;
+	case "and":
+		obj->set_property(pname, obj->query_property(pname) & pvalue);
+		break;
+	case "div":
+		obj->set_property(pname, obj->query_property(pname) / pvalue);
+		break;
+	case "mul":
+		obj->set_property(pname, obj->query_property(pname) * pvalue);
+		break;
+	case "or":
+		obj->set_property(pname, obj->query_property(pname) | pvalue);
+		break;
+	case "radd":
+		obj->set_property(pname, pvalue + obj->query_property(pname));
+		break;
+	case "sub":
+		obj->set_property(pname, obj->query_property(pname) - pvalue);
+		break;
+	case "xor":
+		obj->set_property(pname, obj->query_property(pname) ^ pvalue);
+		break;
+	}
+
+	send_out("Done.\n");
+}
