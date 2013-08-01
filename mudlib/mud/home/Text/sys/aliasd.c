2ffcdc2129793a5bafd10c394f37ee286773f743
Raymond Jennings <shentino@gmail.com>
2013-07-31 17:35:42 -0700

    Add alias daemon

---
+++ mudlib/mud/home/Text/sys/aliasd.c
@@ -0,0 +1,44 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/privilege.h>
+
+mapping aliases;
+
+static void create()
+{
+	aliases = ([ ]);
+}
+
+string *query_aliases()
+{
+	return map_indices(aliases);
+}
+
+string query_alias(string name)
+{
+	return aliases[name];
+}
+
+void set_alias(string name, string value)
+{
+	ACCESS_CHECK(INTERFACE());
+
+	aliases[name] = value;
+}
