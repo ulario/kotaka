581e35603e0518c75f9dfaa5dcfc6661d5963692
Raymond Jennings <shentino@gmail.com>
2013-08-01 13:06:08 -0700

    Add more channel commands to post, add, and delete channels

---
+++ mudlib/mud/home/Text/sys/verb/ooc/chpost.c
@@ -0,0 +1,50 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <account/paths.h>
+#include <kotaka/paths.h>
+#include <text/paths.h>
+
+inherit LIB_RAWVERB;
+
+void main(object actor, string args)
+{
+	object user;
+	string channel;
+	string text;
+
+	user = query_user();
+
+	if (user->query_class() < 2) {
+		send_out("You do not have sufficient access rights to post to generic channels.\n");
+		return;
+	}
+
+	if (sscanf(args, "%s %s", channel, text) != 2) {
+		send_out("Usage: chpost <channel> <text\n");
+		return;
+	}
+
+	if (!CHANNELD->test_channel(channel)) {
+		send_out("No such channel.\n");
+		return;
+	}
+
+	CHANNELD->post_message(channel, user->query_name(), text);
+}
---
+++ mudlib/mud/home/Text/sys/verb/wiz/ooc/channel/chdel.c
@@ -0,0 +1,54 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <account/paths.h>
+#include <kotaka/paths.h>
+#include <text/paths.h>
+
+inherit LIB_RAWVERB;
+
+void main(object actor, string args)
+{
+	object user;
+	string name;
+	string *subscriptions;
+
+	user = query_user();
+	name = user->query_username();
+
+	if (user->query_class() < 2) {
+		send_out("You do not have sufficient access rights to delete channels.\n");
+		return;
+	}
+
+	if (args == "") {
+		send_out("Cat got your tongue?\n");
+		return;
+	}
+
+	if (!CHANNELD->test_channel(args)) {
+		send_out("That channel does not exist.\n");
+		return;
+	}
+
+	CHANNELD->del_channel(args);
+
+	send_out("Channel deleted.\n");
+	return;
+}
