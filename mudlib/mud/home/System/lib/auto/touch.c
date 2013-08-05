c1d84e62c8a916a2c94193fcc3cdfe3e35447207
Raymond Jennings <shentino@gmail.com>
2013-08-05 08:16:22 -0700

    Fixup touch

--- mudlib/mud/home/System/lib/auto/touch.c
+++ mudlib/mud/home/System/lib/auto/touch.c
@@ -17,10 +17,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <kernel/kernel.h>
 #include <kotaka/privilege.h>
-#include <kotaka/paths.h>
-#include <kotaka/log.h>
+#include <kotaka/paths/system.h>
 #include <status.h>
 
 inherit "call_guard";
