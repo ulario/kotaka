bb9391a76b90acb724172356cfd79b716c7c1926
Raymond Jennings <shentino@gmail.com>
2013-08-05 08:09:26 -0700

    include cleanup for second_auto

--- mudlib/mud/home/System/lib/auto/second_auto.c
+++ mudlib/mud/home/System/lib/auto/second_auto.c
@@ -18,8 +18,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <kotaka/privilege.h>
-#include <kotaka/paths.h>
-#include <kotaka/log.h>
 
 #include <trace.h>
 #include <type.h>
