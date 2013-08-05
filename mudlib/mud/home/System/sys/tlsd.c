7f91a1681af8323ff69f09620082f5283cc3c1d2
Raymond Jennings <shentino@gmail.com>
2013-08-05 16:22:33 -0700

    Remove some messy comments

--- mudlib/mud/home/System/sys/tlsd.c
+++ mudlib/mud/home/System/sys/tlsd.c
@@ -17,10 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/** TLS manager
-
-Enhances TLS by providing access controls and string labels
-*/
 #include <kernel/access.h>
 #include <kernel/kernel.h>
 #include <kernel/tls.h>
--- mudlib/mud/home/Thing/sys/propertyd.c
+++ mudlib/mud/home/Thing/sys/propertyd.c
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/** Property typing and inheritance */
 #include <kotaka/paths/thing.h>
 #include <kotaka/privilege.h>
 #include <kotaka/property.h>
