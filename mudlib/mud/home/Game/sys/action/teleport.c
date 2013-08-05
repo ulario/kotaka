951fdfbeb6e3de74f12c671e2e654289b3899c4b
Raymond Jennings <shentino@gmail.com>
2013-08-05 06:26:21 -0700

    Add teleport action

---
+++ mudlib/mud/home/Game/sys/action/teleport.c
@@ -0,0 +1,45 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <game/paths.h>
+#include <kotaka/paths.h>
+#include <text/paths.h>
+
+inherit LIB_USERIO;
+inherit LIB_EMIT;
+inherit LIB_ACTION;
+
+void action(mapping roles)
+{
+	object exit;
+	object target;
+	object actor;
+
+	actor = roles["actor"];
+	target = roles["dob"];
+
+	if (actor->query_property("is_immobile")) {
+		send_out("You're stuck like glue and can't move.\n");
+		return;
+	}
+
+	emit_from(actor, ({ "vanish", "vanishes" }), "into thin air");
+	actor->move(target);
+	emit_from(actor, ({ "appear", "appears" }), "out of thin air");
+}
