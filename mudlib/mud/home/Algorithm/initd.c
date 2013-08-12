b3169c1ba1e0cfa7f5ed813b9ff5bdcb74eb30ba
Raymond Jennings <shentino@gmail.com>
2013-08-12 03:58:27 -0700

    Proper subsystem initialization

--- mudlib/mud/home/Algorithm/initd.c
+++ mudlib/mud/home/Algorithm/initd.c
@@ -17,3 +17,14 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <kotaka/paths/system.h>
+
+inherit LIB_INITD;
+inherit UTILITY_COMPILE;
+
+static void create()
+{
+	KERNELD->set_global_access("Algorithm", 1);
+
+	load_dir("sys", 1);
+}
