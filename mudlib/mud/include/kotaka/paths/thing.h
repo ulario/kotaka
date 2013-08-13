a4b5684aec17fd77e87019c5effb8c998124ceca
Raymond Jennings <shentino@gmail.com>
2013-08-13 06:20:15 -0700

    Remove some deprecated paths

--- mudlib/mud/include/kotaka/paths/thing.h
+++ mudlib/mud/include/kotaka/paths/thing.h
@@ -2,9 +2,6 @@
 
 #define LIB_BULK		(USR_DIR + "/Thing/lib/bulk")
 
-#define OBJ_THING		(USR_DIR + "/Thing/obj/thing")
-
 #define BULKD			(USR_DIR + "/Thing/sys/bulkd")
 #define PROPERTYD		(USR_DIR + "/Thing/sys/propertyd")
-#define TIMED			(USR_DIR + "/Thing/sys/timed")
 #define THING_SUBD		(USR_DIR + "/Thing/sys/subd")
