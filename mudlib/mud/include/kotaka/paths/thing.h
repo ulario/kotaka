3ac755ed8e8fa4e2f76c5f9bcdedc75da1cdcd46
Raymond Jennings <shentino@gmail.com>
2013-08-13 06:22:18 -0700

    Add a define for the general thing library

--- mudlib/mud/include/kotaka/paths/thing.h
+++ mudlib/mud/include/kotaka/paths/thing.h
@@ -1,6 +1,7 @@
 #include <config.h>
 
 #define LIB_BULK		(USR_DIR + "/Thing/lib/bulk")
+#define LIB_THING		(USR_DIR + "/Thing/lib/thing")
 
 #define BULKD			(USR_DIR + "/Thing/sys/bulkd")
 #define PROPERTYD		(USR_DIR + "/Thing/sys/propertyd")
