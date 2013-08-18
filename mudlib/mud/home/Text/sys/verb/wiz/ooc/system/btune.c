af2092f1be7ab6af6bd67fcc59009adb2ee51470
Raymond Jennings <shentino@gmail.com>
2013-08-17 19:48:55 -0700

    Add command to tune bulkd

---
+++ mudlib/mud/home/Text/sys/verb/wiz/ooc/system/btune.c
@@ -0,0 +1,54 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2013  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths/kotaka.h>
+#include <kotaka/paths/string.h>
+#include <kotaka/paths/system.h>
+#include <kotaka/paths/text.h>
+#include <kotaka/paths/thing.h>
+
+inherit LIB_RAWVERB;
+
+void main(object actor, string args)
+{
+	object user;
+	float interval;
+
+	user = query_user();
+
+	if (user->query_class() < 2) {
+		send_out("You do not have sufficient access rights to tune the bulk sync system.\n");
+		return;
+	}
+
+	if (args == "") {
+		send_out("Current bulk sync interval: " + STRINGD->mixed_sprint(BULKD->query_interval()) + "\n");
+		return;
+	}
+
+	if (!sscanf(args, "%f", interval)) {
+		send_out("Floats only please.\n");
+		return;
+	}
+
+	BULKD->set_interval(interval);
+
+	send_out("BulkD tuned.\n");
+	return;
+}
