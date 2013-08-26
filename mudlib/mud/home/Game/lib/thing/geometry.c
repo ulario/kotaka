ee0618f84b03268b0ed77f9ff25ac4321fad979c
Raymond Jennings <shentino@gmail.com>
2013-08-25 23:50:53 -0700

    Forgot to add this

--- mudlib/mud/home/Game/lib/thing/geometry.c
+++ mudlib/mud/home/Game/lib/thing/geometry.c
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <game/paths.h>
 #include <kotaka/privilege.h>
 
 object *query_inventory();
