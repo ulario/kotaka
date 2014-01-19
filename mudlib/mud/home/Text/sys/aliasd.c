86caa786c098118d8490b19c9eb8bc80f8539ec4
Raymond Jennings <shentino@gmail.com>
2014-01-19 03:38:56 -0800

    Include proper headers for aliasd

--- mudlib/mud/home/Text/sys/aliasd.c
+++ mudlib/mud/home/Text/sys/aliasd.c
@@ -17,6 +17,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <kotaka/paths/string.h>
+#include <kotaka/paths/system.h>
 #include <kotaka/privilege.h>
 
 mapping aliases;
