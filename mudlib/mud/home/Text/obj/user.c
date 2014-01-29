3ac5d75355d55d2c2f87eff66b016d3b507e9f7b
Raymond Jennings <shentino@gmail.com>
2014-01-29 13:03:50 -0800

    oops, forgot about this

--- mudlib/mud/home/Text/obj/user.c
+++ mudlib/mud/home/Text/obj/user.c
@@ -164,6 +164,11 @@ void channel_message(string channel, string stamp, string sender, string message
 {
 	ACCESS_CHECK(previous_program() == CHANNELD);
 
-	send_out("[\033[1m" + channel + "\033[0m] \033[1;32m" + stamp +
-		"\033[0m \033[1;34m" + sender + "\033[0m: " + message + "\n");
+	if (sender) {
+		send_out("[\033[1m" + channel + "\033[0m] \033[1;32m" + stamp +
+			"\033[0m \033[1;34m" + sender + "\033[0m: " + message + "\n");
+	} else {
+		send_out("[\033[1m" + channel + "\033[0m] \033[1;32m" + stamp +
+			"\033[0m " + message + "\n");
+	}
 }
