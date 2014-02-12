d37134c0e40d07b49a3ad3b2be58a3a404a5cf30
Raymond Jennings <shentino@gmail.com>
2014-02-11 20:29:26 -0800

    Move binary search routines to sortd

--- mudlib/mud/home/Algorithm/sys/sortd.c
+++ mudlib/mud/home/Algorithm/sys/sortd.c
@@ -88,3 +88,83 @@ void bqsort(mixed arr, int begin, int end, varargs string compfunc)
 {
 	qsort(arr, begin, end, compfunc);
 }
+
+int binary_search_floor(mixed *arr, mixed value)
+{
+	int begin;
+	int end;
+
+	begin = 0;
+	end = sizeof(arr);
+
+	if (begin == end) {
+		/* zero size array */
+		return -1;
+	}
+
+	if (value < arr[begin]) {
+		/* completely out of range */
+		return -1;
+	}
+
+	if (value >= arr[end - 1]) {
+		/* it floats to the top floor */
+		return end - 1;
+	}
+
+	while (end - begin > 1) {
+		int midpoint;
+		mixed probe;
+
+		midpoint = (begin + end) >> 1;
+
+		probe = arr[midpoint];
+
+		if (value < probe) {
+			/* too low */
+			end = midpoint;
+		} else {
+			begin = midpoint;
+		}
+	}
+
+	return begin;
+}
+
+/* find the smallest value greater than or equal to the target */
+int binary_search_ceiling(mixed *arr, mixed value)
+{
+	int begin;
+	int end;
+
+	begin = 0;
+	end = sizeof(arr);
+
+	if (value <= arr[begin]) {
+		/* floats to the first floor */
+		return 0;
+	}
+
+	if (value > arr[end - 1]) {
+		/* goes into orbit */
+		return end;
+	}
+
+	while (end - begin > 1) {
+		int midpoint;
+		mixed probe;
+
+		midpoint = (begin + end) >> 1;
+
+		probe = arr[midpoint];
+
+		if (value <= probe) {
+			end = midpoint;
+		} else {
+			/* too high */
+			begin = midpoint;
+		}
+	}
+
+	return begin;
+}
