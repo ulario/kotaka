78e2c82b7a8c1e0b6ce3aa812aaaf41eab038d89
Raymond Jennings <shentino@gmail.com>
2014-03-31 00:28:36 -0700

    Make remove verb nicer

--- mudlib/mud/home/Verb/sys/verb/core/ic/remove.c
+++ mudlib/mud/home/Verb/sys/verb/core/ic/remove.c
@@ -106,7 +106,7 @@ void main(object actor, mapping roles)
 		layer = worn[i]->query_property("clothing_layer");
 
 		if (layer > dlayer) {
-			send_out("Try removing your " + worn[i]->query_property("brief") + " first.\n");
+			send_out("You have to remove your " + worn[i]->query_property("brief") + " first.\n");
 			return;
 		}
 	}
