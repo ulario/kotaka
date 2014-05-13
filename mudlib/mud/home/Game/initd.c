07c5e32e0f729246832b60b0f59535bc7a10c023
Raymond Jennings <shentino@gmail.com>
2014-05-12 20:27:20 -0700

    Remove useless property

--- mudlib/mud/home/Game/initd.c
+++ mudlib/mud/home/Game/initd.c
@@ -127,8 +127,6 @@ void configure_properties()
 	PROPERTYD->add_property("size_y", T_INT, PROP_MAGIC, ({ "query_y_size", "set_y_size" }) );
 	PROPERTYD->add_property("size_z", T_INT, PROP_MAGIC, ({ "query_z_size", "set_z_size" }) );
 
-	PROPERTYD->add_property("is_infinite", T_INT, PROP_INHERIT);
-
 	/* catch all */
 	PROPERTYD->add_property("data", T_MAPPING, PROP_SIMPLE, ([ ]) );
 
