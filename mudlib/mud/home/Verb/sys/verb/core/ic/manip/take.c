3f652bd38cd3fe6f8cab9af62bc5012fc5edad5c
Raymond Jennings <shentino@gmail.com>
2014-06-02 16:46:09 -0700

    Fix a couple of verbs

--- mudlib/mud/home/Verb/sys/verb/core/ic/manip/take.c
+++ mudlib/mud/home/Verb/sys/verb/core/ic/manip/take.c
@@ -139,6 +139,6 @@ void main(object actor, mapping roles)
 	if (from) {
 		emit_from(actor, actor, " ", ({ "take", "takes" }), dob, " from ", from, ".");
 	} else {
-		emit_from(actor, actor, ({ "take", "takes" }), " ", dob, ".");
+		emit_from(actor, actor, " ", ({ "take", "takes" }), " ", dob, ".");
 	}
 }
