8540ede2506a643c4c9f4277c98ac2fa0d4a16c7
Raymond Jennings <shentino@gmail.com>
2014-06-05 07:17:01 -0700

    Oops, forgot a header file

--- mudlib/mud/home/Test/sys/objregwatchd.c
+++ mudlib/mud/home/Test/sys/objregwatchd.c
@@ -18,6 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <kotaka/paths/system.h>
+#include <status.h>
 
 mapping trackers; /* ([ owner : tracker ]) */
 int handle;
