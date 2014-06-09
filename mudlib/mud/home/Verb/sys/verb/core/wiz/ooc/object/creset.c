6687386cf8f94e1d5756fdbb711f8110df15d1ed
Raymond Jennings <shentino@gmail.com>
2014-06-09 08:20:47 -0700

    Add command to separately reset the clone manager

---
+++ mudlib/mud/home/Verb/sys/verb/core/wiz/ooc/object/creset.c
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2012, 2013, 2014  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths/system.h>
+#include <kotaka/paths/verb.h>
+
+inherit LIB_VERB;
+
+string *query_parse_methods()
+{
+	return ({ "raw" });
+}
+
+void main(object actor, mapping roles)
+{
+	object user;
+
+	user = query_user();
+
+	if (user->query_class() < 3) {
+		send_out("You do not have sufficient access rights to reset the clone manager.\n");
+		return;
+	}
+
+	CLONED->discover_clones();
+}
