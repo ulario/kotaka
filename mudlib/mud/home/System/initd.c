34e8435a67eae899faf68868ca84770777920224
Raymond Jennings <shentino@gmail.com>
2014-06-09 07:14:23 -0700

    infinite rlimits is redundant here

--- mudlib/mud/home/System/initd.c
+++ mudlib/mud/home/System/initd.c
@@ -71,73 +71,72 @@ static void create()
 	check_config();
 	check_versions();
 
-	rlimits (0; -1) {
-		catch {
-			subsystems = ([ ]);
+	catch {
+		subsystems = ([ ]);
 
-			upgraded_v_0_34 = 1;
-			upgraded_v_0_35 = 1;
+		upgraded_v_0_34 = 1;
+		upgraded_v_0_35 = 1;
 
-			load_core();
+		load_core();
 
-			remove_file("/log/session.log");
+		remove_file("/log/session.log");
 
-			LOGD->post_message("boot", LOG_INFO, "Logging initialized");
-			LOGD->post_message("boot", LOG_INFO, "Welcome to Kotaka " + KOTAKA_VERSION);
+		LOGD->post_message("boot", LOG_INFO, "Logging initialized");
+		LOGD->post_message("boot", LOG_INFO, "Welcome to Kotaka " + KOTAKA_VERSION);
 
-			LOGD->post_message("boot", LOG_INFO, "Loading kernel manager");
-			load_object(KERNELD);
-			KERNELD->set_global_access("System", 1);
+		LOGD->post_message("boot", LOG_INFO, "Loading kernel manager");
+		load_object(KERNELD);
+		KERNELD->set_global_access("System", 1);
 
-			LOGD->post_message("boot", LOG_INFO, "Configuring kernel library");
+		LOGD->post_message("boot", LOG_INFO, "Configuring kernel library");
 
-			configure_klib();
-			configure_rsrc();
+		configure_klib();
+		configure_rsrc();
 
-			boot_subsystem("Channel");
-			boot_subsystem("String");
-			boot_subsystem("Algorithm");
-			boot_subsystem("Bigstruct");
+		boot_subsystem("Channel");
+		boot_subsystem("String");
+		boot_subsystem("Algorithm");
+		boot_subsystem("Bigstruct");
 
-			LOGD->post_message("boot", LOG_INFO, "Loading program manager");
-			load_object(PROGRAM_INFO);
-			load_object(PROGRAMD);
+		LOGD->post_message("boot", LOG_INFO, "Loading program manager");
+		load_object(PROGRAM_INFO);
+		load_object(PROGRAMD);
 
-			LOGD->post_message("boot", LOG_INFO, "Loading object manager");
-			load_object(OBJECTD);
+		LOGD->post_message("boot", LOG_INFO, "Loading object manager");
+		load_object(OBJECTD);
 
-			LOGD->post_message("boot", LOG_INFO, "Initializing program database");
-			OBJECTD->discover_objects();
+		LOGD->post_message("boot", LOG_INFO, "Initializing program database");
+		OBJECTD->discover_objects();
 
-			LOGD->post_message("boot", LOG_INFO, "Loading clone manager");
-			load_object(CLONE_INFO);
-			load_object(CLONED);
+		LOGD->post_message("boot", LOG_INFO, "Loading clone manager");
+		load_object(CLONE_INFO);
+		load_object(CLONED);
 
-			load();
-			configure_logging();
+		load();
 
-			LOGD->post_message("boot", LOG_INFO, "Configuring user manager");
-			SYSTEM_USERD->enable();
+		configure_logging();
 
-			LOGD->post_message("boot", LOG_INFO, "Starting status manager");
-			STATUSD->enable();
+		LOGD->post_message("boot", LOG_INFO, "Configuring user manager");
+		SYSTEM_USERD->enable();
 
-			LOGD->post_message("boot", LOG_INFO, "Starting watchdog");
-			WATCHDOGD->enable();
+		LOGD->post_message("boot", LOG_INFO, "Starting status manager");
+		STATUSD->enable();
 
-			/* Booted up */
+		LOGD->post_message("boot", LOG_INFO, "Starting watchdog");
+		WATCHDOGD->enable();
 
-			LOGD->post_message("boot", LOG_INFO, "System ready");
+		/* Booted up */
 
-			boot_subsystem("Kotaka");
-			boot_subsystem("Game");
-			boot_subsystem("Test");
-			call_out("audit_filequota", 0);
-		} : {
-			LOGD->flush();
-			shutdown();
-			error("System setup failed");
-		}
+		LOGD->post_message("boot", LOG_INFO, "System ready");
+
+		boot_subsystem("Kotaka");
+		boot_subsystem("Game");
+		boot_subsystem("Test");
+		call_out("audit_filequota", 0);
+	} : {
+		LOGD->flush();
+		shutdown();
+		error("System setup failed");
 	}
 }
 
