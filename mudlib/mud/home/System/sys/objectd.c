88755258c05e7e93559cf156729427b8915f0091
Raymond Jennings <shentino@gmail.com>
2015-05-04 00:24:07 -0700

    Handle multiple callers of objectd->touch, both from the kernel library and the auto touch library

--- mudlib/mud/home/System/sys/objectd.c
+++ mudlib/mud/home/System/sys/objectd.c
@@ -412,12 +412,16 @@ mixed include_file(string compiled, string from, string path)
 	return path;
 }
 
-int touch(object obj, string function)
+int touch(varargs object obj, string function)
 {
-	ACCESS_CHECK(KERNEL());
-
-	if (!sscanf(object_name(obj), "/kernel/%*s")) {
-		obj->_F_touch(function);
+	if (previous_program() == DRIVER) {
+		if (!sscanf(object_name(obj), "/kernel/%*s")) {
+			obj->_F_touch(function);
+		}
+	} else if (sscanf(previous_program(), USR_DIR
+		+ "/System" + INHERITABLE_SUBDIR + "auto/%*s")) {
+	} else {
+		error("Access denied");
 	}
 }
 
