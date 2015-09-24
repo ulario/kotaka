2eda93d79b903f0d85adc929aa4f351dc01ce732
Raymond Jennings <shentino@gmail.com>
2015-09-23 21:59:24 -0700

    Add new verb to check the current version number

---
+++ mudlib/mud/home/Verb/sys/verb/core/ooc/version.c
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2015  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kernel/version.h>
+#include <kotaka/paths/system.h>
+#include <kotaka/paths/verb.h>
+#include <kotaka/version.h>
+#include <status.h>
+
+inherit LIB_VERB;
+
+string *query_parse_methods()
+{
+	return ({ "raw" });
+}
+
+void main(object actor, mapping roles)
+{
+	send_out("DGD version: " + status(ST_VERSION) + "\n");
+	send_out("Kernel library version: " + KERNEL_LIB_VERSION + "\n");
+	send_out("Kotaka version: " + INITD->query_version() + "\n");
+}
