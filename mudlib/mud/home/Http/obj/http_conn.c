7a70665d9e7aab1dcf2f86fe00f9b53de6639c95
Raymond Jennings <shentino@gmail.com>
2015-10-02 23:37:15 -0700

    Add http.h include to http_conn

--- mudlib/mud/home/Http/obj/http_conn.c
+++ mudlib/mud/home/Http/obj/http_conn.c
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <kotaka/paths/http.h>
 #include <kotaka/paths/system.h>
 #include <kernel/user.h>
 
