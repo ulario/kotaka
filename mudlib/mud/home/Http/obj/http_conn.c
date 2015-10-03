d5402abde567641a95a4958b57e2b64f074b332a
Raymond Jennings <shentino@gmail.com>
2015-10-02 21:51:45 -0700

    Move http handling into a catch, and return an error to the browser if anything goes wrong

--- mudlib/mud/home/Http/obj/http_conn.c
+++ mudlib/mud/home/Http/obj/http_conn.c
@@ -75,49 +75,59 @@ int message_done()
 
 private void handle_get()
 {
-	object conn;
-	object header;
+	catch {
+		object conn;
+		object header;
 
-	header = new_object("~/lwo/http_response");
+		header = new_object("~/lwo/http_response");
 
-	header->set_status(503, "No handler");
+		header->set_status(503, "No handler");
 
-	message(header->generate_header());
-	message("<html>\n");
-	message("<head>\n");
-	message("<title>No handler</title>\n");
-	message("</head>\n");
-	message("<body>\n");
-	message("<h1 style=\"color: red\">No handler</h1>\n");
-	message("<table>\n");
-	message("<tr><td>Method</td><td>" + method + "</td></tr>\n");
-	message("<tr><td>Path</td><td>" + path + "</td></tr>\n");
-	message("<tr><td>Version</td><td>" + version + "</td></tr>\n");
-	message("</table>\n");
-	message("<p>There is no handler for that path.</p>\n");
-	message("<p>For the curious, here's a list of all objects involved in this connection:</p>\n");
+		message(header->generate_header());
+		message("<html>\n");
+		message("<head>\n");
+		message("<title>No handler</title>\n");
+		message("</head>\n");
+		message("<body>\n");
+		message("<h1 style=\"color: red\">No handler</h1>\n");
+		message("<table>\n");
+		message("<tr><td>Method</td><td>" + method + "</td></tr>\n");
+		message("<tr><td>Path</td><td>" + path + "</td></tr>\n");
+		message("<tr><td>Version</td><td>" + version + "</td></tr>\n");
+		message("</table>\n");
+		message("<p>There is no handler for that path.</p>\n");
+		message("<p>For the curious, here's a list of all objects involved in this connection:</p>\n");
 
-	conn = this_object();
+		conn = this_object();
 
-	message("<p style=\"color: darkgreen\">\n");
+		message("<p style=\"color: darkgreen\">\n");
 
-	while(conn) {
-		message(object_name(conn) + "<br />\n");
+		while(conn) {
+			message(object_name(conn) + "<br />\n");
 
-		if (conn <- LIB_USER) {
-			conn = conn->query_conn();
-		} else {
-			break;
+			if (conn <- LIB_USER) {
+				conn = conn->query_conn();
+			} else {
+				break;
+			}
 		}
-	}
 
-	message("</p>\n");
-	message("<p>And here is the request your browser sent:</p>");
-	message("<pre>\n");
-	message(request);
-	message("</pre>\n");
-	message("</body>\n");
-	message("</html>\n");
+		message("</p>\n");
+		message("<p>And here is the request your browser sent:</p>");
+		message("<pre>\n");
+		message(request);
+		message("</pre>\n");
+		message("</body>\n");
+		message("</html>\n");
+	} : {
+		object header;
+
+		header = new_object("~/lwo/http_response");
+		header->set_status(503, "Internal server error");
+
+		message(header->generate_header()
+			+ read_file("~/data/error/500.html"));
+	}
 }
 
 private int input(string message)
