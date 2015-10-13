f7b17d3355bd25983794c845d9383a7c5f3f29bb
Raymond Jennings <shentino@gmail.com>
2015-10-12 17:36:47 -0700

    saveload: reorganize functions into sections

--- mudlib/mud/home/Game/sys/saveload.c
+++ mudlib/mud/home/Game/sys/saveload.c
@@ -33,202 +33,9 @@ object objlist;	/* ({ idnum: obj, data }) */
 object dirputqueue;
 object objputqueue;
 
-object *parse_object(string input)
-{
-	int onum;
+/* private helper functions */
 
-	if (sscanf(input, "O:%d", onum)) {
-		return ({ objlist->query_element(onum - 1)[0] });
-	} else {
-		error("Cannot parse: " + input);
-	}
-}
-
-string sprint_object(object obj, varargs mapping seen)
-{
-	string path;
-	int oindex;
-
-	if (sscanf(object_name(obj), "%s#%d", path, oindex)) {
-		switch(path) {
-		case USR_DIR + "/Game/obj/thing":
-			return "<O:" + oindex2onum->query_element(oindex) + ">";
-		case USR_DIR + "/Text/obj/user":
-			return "nil";
-		default:
-			error("Clone is not a thing (" + path + ")");
-		}
-	} else {
-		return STRINGD->sprint_object(obj, seen);
-	}
-}
-
-private void put_object(object obj)
-{
-	int oindex;
-	int sz;
-	object *inv;
-
-	ASSERT(sscanf(object_name(obj), "%*s#%d", oindex));
-
-	if (oindex2onum->query_element(oindex) != nil) {
-		return;
-	}
-
-	sz = objlist->query_size();
-	oindex2onum->set_element(oindex, sz + 1);
-	objlist->push_back( ({ obj, obj->save() }) );
-
-	inv = obj->query_inventory();
-
-	for (sz = sizeof(inv); --sz >= 0; ) {
-		objputqueue->push_back(inv[sz]);
-	}
-}
-
-private void purge_savedir()
-{
-	string *names;
-
-	do {
-		int sz;
-
-		names = CONFIGD->get_dir("save/*")[0];
-
-		if (names) {
-			for (sz = sizeof(names); --sz >= 0; ) {
-				CONFIGD->remove_file("save/" + names[sz]);
-			}
-		}
-	} while (names && sizeof(names));
-}
-
-private void put_directory(string dir)
-{
-	mapping list;
-	int *keys;
-	string *names;
-	int i, sz;
-
-	list = CATALOGD->list_directory(dir);
-
-	names = map_indices(list);
-	keys = map_values(list);
-
-	sz = sizeof(keys);
-
-	for (i = 0; i < sz; i++) {
-		if (keys[i] == 2) {
-			if (dir) {
-				dirputqueue->push_front(dir + ":" + names[i]);
-			} else {
-				dirputqueue->push_front(names[i]);
-			}
-		} else {
-			if (dir) {
-				objputqueue->push_front(CATALOGD->lookup_object(dir + ":" + names[i]));
-			} else {
-				objputqueue->push_front(CATALOGD->lookup_object(names[i]));
-			}
-		}
-	}
-}
-
-void save_world_put()
-{
-	ACCESS_CHECK(previous_program() == SUSPENDD);
-
-	catch {
-		int ticks;
-		int done;
-
-		ticks = status(ST_TICKS);
-
-		while (!done && ticks - status(ST_TICKS) < 10000) {
-			if (!objputqueue->empty()) {
-				object obj;
-
-				obj = objputqueue->query_front();
-				objputqueue->pop_front();
-
-				put_object(obj);
-			} else if (!dirputqueue->empty()) {
-				string dir;
-
-				dir = dirputqueue->query_front();
-				dirputqueue->pop_front();
-
-				put_directory(dir);
-			} else {
-				done = 1;
-
-				break;
-			}
-		}
-
-		if (done) {
-			CONFIGD->make_dir(".");
-			CONFIGD->make_dir("save");
-			SUSPENDD->queue_work("save_world_write", objlist->query_size());
-		} else {
-			SUSPENDD->queue_work("save_world_put");
-		}
-	} : {
-		LOGD->post_message("system", LOG_INFO, "World save aborted");
-	}
-}
-
-void save_world_write(int i)
-{
-	ACCESS_CHECK(previous_program() == SUSPENDD);
-
-	catch {
-		int ticks;
-
-		ticks = status(ST_TICKS);
-
-		while (i > 0 && ticks - status(ST_TICKS) < 10000) {
-			i--;
-
-			CONFIGD->write_file("save/" + (i + 1) + ".obj",
-				STRINGD->hybrid_sprint(
-					objlist->query_element(i)[1]
-				) + "\n"
-			);
-		}
-
-		if (i > 0) {
-			SUSPENDD->queue_work("save_world_write", i);
-		} else {
-			LOGD->post_message("system", LOG_INFO, "World saved");
-		}
-	} : {
-		LOGD->post_message("system", LOG_INFO, "World save aborted");
-	}
-}
-
-void save_world()
-{
-	int sz;
-
-	oindex2onum = new_object(BIGSTRUCT_MAP_LWO);
-	oindex2onum->claim();
-	oindex2onum->set_type(T_INT);
-
-	objlist = new_object(BIGSTRUCT_ARRAY_LWO);
-	objlist->claim();
-	dirputqueue = new_object(BIGSTRUCT_DEQUE_LWO);
-	dirputqueue->claim();
-	objputqueue = new_object(BIGSTRUCT_DEQUE_LWO);
-	objputqueue->claim();
-
-	purge_savedir();
-
-	dirputqueue->push_back(nil);
-
-	SUSPENDD->suspend_system();
-	SUSPENDD->queue_work("save_world_put");
-}
+/* - loading */
 
 void load_world_purge(int i)
 {
@@ -364,6 +171,186 @@ void load_world_set(int i)
 	}
 }
 
+/* - saving */
+
+private void purge_savedir()
+{
+	string *names;
+
+	do {
+		int sz;
+
+		names = CONFIGD->get_dir("save/*")[0];
+
+		if (names) {
+			for (sz = sizeof(names); --sz >= 0; ) {
+				CONFIGD->remove_file("save/" + names[sz]);
+			}
+		}
+	} while (names && sizeof(names));
+}
+
+private void put_object(object obj)
+{
+	int oindex;
+	int sz;
+	object *inv;
+
+	ASSERT(sscanf(object_name(obj), "%*s#%d", oindex));
+
+	if (oindex2onum->query_element(oindex) != nil) {
+		return;
+	}
+
+	sz = objlist->query_size();
+	oindex2onum->set_element(oindex, sz + 1);
+	objlist->push_back( ({ obj, obj->save() }) );
+
+	inv = obj->query_inventory();
+
+	for (sz = sizeof(inv); --sz >= 0; ) {
+		objputqueue->push_back(inv[sz]);
+	}
+}
+
+private void put_directory(string dir)
+{
+	mapping list;
+	int *keys;
+	string *names;
+	int i, sz;
+
+	list = CATALOGD->list_directory(dir);
+
+	names = map_indices(list);
+	keys = map_values(list);
+
+	sz = sizeof(keys);
+
+	for (i = 0; i < sz; i++) {
+		if (keys[i] == 2) {
+			if (dir) {
+				dirputqueue->push_front(dir + ":" + names[i]);
+			} else {
+				dirputqueue->push_front(names[i]);
+			}
+		} else {
+			if (dir) {
+				objputqueue->push_front(CATALOGD->lookup_object(dir + ":" + names[i]));
+			} else {
+				objputqueue->push_front(CATALOGD->lookup_object(names[i]));
+			}
+		}
+	}
+}
+
+void save_world_put()
+{
+	ACCESS_CHECK(previous_program() == SUSPENDD);
+
+	catch {
+		int ticks;
+		int done;
+
+		ticks = status(ST_TICKS);
+
+		while (!done && ticks - status(ST_TICKS) < 10000) {
+			if (!objputqueue->empty()) {
+				object obj;
+
+				obj = objputqueue->query_front();
+				objputqueue->pop_front();
+
+				put_object(obj);
+			} else if (!dirputqueue->empty()) {
+				string dir;
+
+				dir = dirputqueue->query_front();
+				dirputqueue->pop_front();
+
+				put_directory(dir);
+			} else {
+				done = 1;
+
+				break;
+			}
+		}
+
+		if (done) {
+			CONFIGD->make_dir(".");
+			CONFIGD->make_dir("save");
+			SUSPENDD->queue_work("save_world_write", objlist->query_size());
+		} else {
+			SUSPENDD->queue_work("save_world_put");
+		}
+	} : {
+		LOGD->post_message("system", LOG_INFO, "World save aborted");
+	}
+}
+
+void save_world_write(int i)
+{
+	ACCESS_CHECK(previous_program() == SUSPENDD);
+
+	catch {
+		int ticks;
+
+		ticks = status(ST_TICKS);
+
+		while (i > 0 && ticks - status(ST_TICKS) < 10000) {
+			i--;
+
+			CONFIGD->write_file("save/" + (i + 1) + ".obj",
+				STRINGD->hybrid_sprint(
+					objlist->query_element(i)[1]
+				) + "\n"
+			);
+		}
+
+		if (i > 0) {
+			SUSPENDD->queue_work("save_world_write", i);
+		} else {
+			LOGD->post_message("system", LOG_INFO, "World saved");
+		}
+	} : {
+		LOGD->post_message("system", LOG_INFO, "World save aborted");
+	}
+}
+
+/* public helper functions */
+
+object *parse_object(string input)
+{
+	int onum;
+
+	if (sscanf(input, "O:%d", onum)) {
+		return ({ objlist->query_element(onum - 1)[0] });
+	} else {
+		error("Cannot parse: " + input);
+	}
+}
+
+string sprint_object(object obj, varargs mapping seen)
+{
+	string path;
+	int oindex;
+
+	if (sscanf(object_name(obj), "%s#%d", path, oindex)) {
+		switch(path) {
+		case USR_DIR + "/Game/obj/thing":
+			return "<O:" + oindex2onum->query_element(oindex) + ">";
+		case USR_DIR + "/Text/obj/user":
+			return "nil";
+		default:
+			error("Clone is not a thing (" + path + ")");
+		}
+	} else {
+		return STRINGD->sprint_object(obj, seen);
+	}
+}
+
+/* public functions */
+
 void load_world()
 {
 	int sz;
@@ -380,8 +367,25 @@ void load_world()
 	SUSPENDD->queue_work("load_world_purge", status(ST_OTABSIZE) - 1);
 }
 
-/* private helper functions */
+void save_world()
+{
+	int sz;
 
-/* public helper functions */
+	oindex2onum = new_object(BIGSTRUCT_MAP_LWO);
+	oindex2onum->claim();
+	oindex2onum->set_type(T_INT);
 
-/* public functions */
+	objlist = new_object(BIGSTRUCT_ARRAY_LWO);
+	objlist->claim();
+	dirputqueue = new_object(BIGSTRUCT_DEQUE_LWO);
+	dirputqueue->claim();
+	objputqueue = new_object(BIGSTRUCT_DEQUE_LWO);
+	objputqueue->claim();
+
+	purge_savedir();
+
+	dirputqueue->push_back(nil);
+
+	SUSPENDD->suspend_system();
+	SUSPENDD->queue_work("save_world_put");
+}
