25541c8b9f4cd264b5150e0e1b90e1a9122605a7
Raymond Jennings <shentino@gmail.com>
2015-11-03 04:24:59 -0800

    periodic snapshots every hour

---
+++ mud/home/System/sys/dumpd.c
@@ -0,0 +1,75 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2010, 2011, 2012, 2013, 2014, 2015  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <kernel/kernel.h>
+#include <kotaka/paths/system.h>
+#include <kotaka/privilege.h>
+#include <kotaka/log.h>
+#include <status.h>
+
+inherit SECOND_AUTO;
+
+#define INTERVAL 3600
+#define OFFSET 0
+
+private int delay();
+
+static void create()
+{
+	call_out("dump", delay());
+}
+
+void upgrade()
+{
+	int sz;
+	mixed **callouts;
+
+	callouts = status(this_object(), O_CALLOUTS);
+
+	for (sz = sizeof(callouts) - 1; sz >= 0; --sz) {
+		remove_call_out(callouts[sz][CO_HANDLE]);
+	}
+
+	call_out("dump", delay());
+}
+
+private int delay()
+{
+	int time, goal;
+
+	time = time();
+
+	goal = time;
+	goal -= goal % INTERVAL;
+	goal += OFFSET;
+
+	if (goal < time) {
+		goal += INTERVAL;
+	}
+
+	return goal - time;
+}
+
+static void dump()
+{
+	call_out("dump", delay());
+
+	dump_state();
+}
