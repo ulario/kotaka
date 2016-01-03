4c0726d3c73b5d93f449ca12ec676575f7b6a752
Raymond Jennings <shentino@gmail.com>
2016-01-02 17:38:00 -0800

    Starting cleanup of ObjectD, beginning with relocation of kernel recompile function

---
+++ mud/home/System/sys/subd.c
@@ -0,0 +1,92 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2015  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kernel/access.h>
+#include <kotaka/privilege.h>
+#include <kotaka/assert.h>
+#include <kotaka/paths/utility.h>
+#include <kotaka/paths/bigstruct.h>
+#include <kotaka/paths/system.h>
+
+inherit SECOND_AUTO;
+
+/* private functions */
+
+/* public functions */
+
+void recompile_kernel_library()
+{
+	string *names;
+	mixed **dir;
+	int i;
+
+	ACCESS_CHECK(PRIVILEGED() || INTERFACE());
+
+	dir = get_dir("/kernel/lib/*");
+	names = dir[0];
+
+	for (i = 0; i < sizeof(names); i++) {
+		string name;
+
+		name = names[i];
+		name = name[0 .. strlen(name) - 3];
+
+		destruct_object("/kernel/lib/" + name);
+	}
+
+	dir = get_dir("/kernel/lib/api/*");
+	names = dir[0];
+
+	for (i = 0; i < sizeof(names); i++) {
+		string name;
+
+		name = names[i];
+		name = name[0..strlen(name) - 3];
+
+		destruct_object("/kernel/lib/api/" + name);
+	}
+
+	dir = get_dir("/kernel/obj/*");
+	names = dir[0];
+
+	for (i = 0; i < sizeof(names); i++) {
+		string name;
+
+		name = names[i];
+		name = name[0 .. strlen(name) - 3];
+
+		if (find_object("/kernel/obj/" + name)) {
+			compile_object("/kernel/obj/" + name);
+		}
+	}
+
+	dir = get_dir("/kernel/sys/*");
+	names = dir[0];
+
+	for (i = 0; i < sizeof(names); i++) {
+		string name;
+
+		name = names[i];
+		name = name[0..strlen(name) - 3];
+
+		if (find_object("/kernel/sys/" + name)) {
+			compile_object("/kernel/sys/" + name);
+		}
+	}
+}
