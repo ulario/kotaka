504be0d09876e94c978611c1941235dbb4449c7f
Raymond Jennings <shentino@gmail.com>
2017-04-18 10:12:51 -0700

    Make the fake score command a bit more verbose

--- mud/home/Verb/sys/verb/core/ooc/score.c
+++ mud/home/Verb/sys/verb/core/ooc/score.c
@@ -67,13 +67,47 @@ string make_xp_bar(int cur, int max, int len)
 	return buf;
 }
 
+string from_number(int num)
+{
+	if (num > 99) {
+		return "OV";
+	} else if (num <= -10) {
+		return "UN";
+	} else if (num < 0) {
+		return "" + num;
+	} else if (num < 10) {
+		return " " + num;
+	} else {
+		return "" + num;
+	}
+}
+
+string make_abi_number(int cur, int max)
+{
+	string buf;
+
+	buf = "\033[1m";
+
+	if (cur < max) {
+		buf += "\033[31m";
+	} else if (cur > max) {
+		buf += "\033[36m";
+	}
+
+	buf += from_number(cur);
+	buf += "\033[37m/";
+	buf += from_number(max);
+	buf += "\033[0m";
+
+	return buf;
+}
+
 void main(object actor, mapping roles)
 {
 	object user;
 	object body;
 	object painter;
 	object gc;
-	string hps, mps, eps, xps;
 	int hp, mp, ep, xp;
 	int i, j;
 
@@ -92,9 +126,22 @@ void main(object actor, mapping roles)
 
 	painter = new_object(LWO_PAINTER);
 
-	send_out("Str ---/--- Cha ---/---\n");
-	send_out("Dex ---/--- Int ---/---\n");
-	send_out("Con ---/--- Wis ---/---\n\n");
+	send_out("     Phy   Men   Soc   Spi\n");
+	send_out("Pow");
+	for (i = 0; i < 4; i++) {
+		send_out(" " + make_abi_number(random(11) + 5, random(11) + 5));
+	}
+	send_out("\n");
+	send_out("Fin");
+	for (i = 0; i < 4; i++) {
+		send_out(" " + make_abi_number(random(11) + 5, random(11) + 5));
+	}
+	send_out("\n");
+	send_out("Res");
+	for (i = 0; i < 4; i++) {
+		send_out(" " + make_abi_number(random(11) + 5, random(11) + 5));
+	}
+	send_out("\n\n");
 
 	send_out("HP [" + make_bar(hp, 20, 20) + "]\n");
 	send_out("MP [" + make_bar(mp, 20, 20) + "]\n");
