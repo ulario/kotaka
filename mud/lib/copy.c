09d84316e2439066ed161c65fd8fcc7fb94e53eb
Raymond Jennings <shentino@gmail.com>
2017-04-26 11:11:35 -0700

    Oops, forgot type header

--- mud/lib/copy.c
+++ mud/lib/copy.c
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <type.h>
 
 mixed deep_copy(mixed in, varargs mapping dupes)
 {
