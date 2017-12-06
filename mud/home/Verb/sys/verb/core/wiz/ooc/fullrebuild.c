f0ebdc56f57341ac73af9de612e99ff35563baeb
Raymond Jennings <shentino@gmail.com>
2017-12-06 11:16:27 -0800

    Add fullrebuild verb

---
+++ mud/home/Verb/sys/verb/core/wiz/ooc/fullrebuild.c
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2017  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths/system.h>
+#include <kotaka/paths/verb.h>
+
+inherit LIB_VERB;
+
+string *query_parse_methods()
+{
+	return ({ "raw" });
+}
+
+void main(object actor, mapping roles)
+{
+	if (query_user()->query_class() < 3) {
+		send_out("Permission denied.\n");
+		return;
+	}
+
+	SYSTEM_SUBD->full_rebuild();
+}
