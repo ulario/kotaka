f8287e39d80f024dc5d89fa3481fd87d0de8fba3
Raymond Jennings <shentino@gmail.com>
2018-07-11 08:10:05 -0700

    Add multimap compaction, as well as listing of indices and values

--- mud/home/System/lib/struct/multimap.c
+++ mud/home/System/lib/struct/multimap.c
@@ -18,8 +18,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <kotaka/paths/system.h>
+#include <status.h>
+#include <kotaka/paths/string.h>
+#include <kotaka/log.h>
 
 inherit SECOND_AUTO;
+inherit "~/lib/struct/list";
 
 static void set_multimap(mapping multimap, int index, mixed value)
 {
@@ -87,4 +91,145 @@ static mixed query_multimap(mapping multimap, int index)
 	}
 
 	return submap[index];
+}
+
+private int compact_submap(mapping map, int level)
+{
+	if (level) {
+		int *indices;
+		mapping *maps;
+		int sz;
+
+		indices = map_indices(map);
+		maps = map_values(map);
+
+		for (sz = sizeof(indices); --sz >= 0; ) {
+			if (compact_submap(maps[sz], level - 1) == 0) {
+				map[indices[sz]] = nil;
+			}
+		}
+	}
+
+	return map_sizeof(map);
+}
+
+void compact_multimap(mapping multimap)
+{
+	int *levels;
+	mapping *maps;
+	int sz;
+
+	levels = map_indices(multimap);
+	maps = map_values(multimap);
+
+	for (sz = sizeof(levels); --sz >= 0; ) {
+		if (compact_submap(maps[sz], levels[sz]) == 0) {
+			multimap[levels[sz]] = nil;
+		}
+	}
+}
+
+private void build_list(mixed **list, int level, mapping map, int values)
+{
+	LOGD->post_message("system", LOG_NOTICE, "Checking level " + level + " for " + STRINGD->hybrid_sprint(map));
+
+	if (level) {
+		int *levels;
+		mapping *maps;
+
+		maps = map_values(map);
+
+		while (sizeof(maps)) {
+			build_list(list, level - 1, maps[0], values);
+
+			maps = maps[1 ..];
+		}
+	} else {
+		mixed *data;
+
+		if (values) {
+			data = map_values(map);
+		} else {
+			data = map_indices(map);
+		}
+
+		while (sizeof(data)) {
+			if (list_empty(list)) {
+				object *left;
+				int max;
+
+				max = status(ST_ARRAYSIZE);
+
+				if (sizeof(data) > max) {
+					LOGD->post_message("system", LOG_NOTICE, "Empty list, overflow, tacking on " + max + " elements");
+
+					list_push_back(list, data[0 .. max - 1]);
+					data = data[max ..];
+				} else {
+					LOGD->post_message("system", LOG_NOTICE, "Empty list, tacking on " + sizeof(data) + " elements");
+
+					list_push_back(list, data);
+					data = ({ });
+				}
+			} else {
+				mixed *last;
+				int max;
+
+				last = list_back_node(list);
+
+				max = status(ST_ARRAYSIZE) - sizeof(last[1]);
+
+				if (sizeof(data) > max) {
+					LOGD->post_message("system", LOG_NOTICE, "Building list, last element has " + sizeof(last[1]) + ", overflow, tacking on " + max + " of " + sizeof(data));
+					last[1] = last[1] + data[0 .. max - 1];
+					data = data[max ..];
+
+					/* obviously we have more to do */
+					list_push_back(list, ({ }) );
+				} else {
+					LOGD->post_message("system", LOG_NOTICE, "Building list, last element has " + sizeof(last[1]) + ", tacking on " + sizeof(data));
+					last[1] = last[1] + data;
+					data = ({ });
+				}
+			}
+		}
+	}
+}
+
+private mixed **create_list(mapping map, int values)
+{
+	int sz;
+	mixed **list;
+
+	int *levels;
+	mapping *maps;
+	/* return a linked list with all the clones */
+
+	if (!map) {
+		return nil;
+	}
+
+	list = ({ nil, nil });
+
+	levels = map_indices(map);
+	maps = map_values(map);
+
+	while (sizeof(levels)) {
+		build_list(list, levels[0], maps[0], values);
+
+		levels = levels[1 ..];
+		maps = maps[1 ..];
+	}
+
+	return list;
+}
+
+static mixed **query_multimap_indices(mapping multimap)
+{
+	return create_list(multimap, 0);
+}
+
+static mixed **query_multimap_values(mapping multimap)
+{
+	return create_list(multimap, 1);
 }
