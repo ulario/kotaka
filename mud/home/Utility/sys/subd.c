9848099a59bc9e3559c310774ce5c17d1195fa0f
Raymond Jennings <shentino@gmail.com>
2018-09-14 23:31:12 -0700

    Move time formatting routines to /lib/time.

--- mud/home/Utility/sys/subd.c
+++ mud/home/Utility/sys/subd.c
@@ -26,44 +26,6 @@ inherit "/lib/time";
 inherit "/lib/tier";
 inherit "/lib/copy";
 
-string ptime(int time)
-{
-	string c;
-	string hms;
-	string w;
-	string m;
-	string d;
-	string y;
-
-	string dt;
-
-	c = ctime(time);
-
-	hms = c[11 .. 18];
-
-	w = c[0 .. 2];
-	m = c[4 .. 6];
-	d = c[8 .. 9];
-	y = c[20 .. 23];
-
-	if (d[0] == ' ')
-		d[0] = '0';
-
-	dt = m + " " + d + ", " + y;
-	return dt + " " + hms;
-}
-
-string pmtime(mixed *mtime)
-{
-	int msec;
-	string ms;
-
-	msec = (int) (mtime[1] * 1000.0);
-	ms = "000" + (string) msec;
-	ms = ms[strlen(ms) - 3..];
-	return ptime(mtime[0]) + "." + ms;
-}
-
 void gather_inheriters(int oindex, object hits)
 {
 	object pinfo;
