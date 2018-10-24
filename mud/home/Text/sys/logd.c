9b799459b0401c64f61fe076300f09de4f30c9a9
Raymond Jennings <shentino@gmail.com>
2018-10-24 04:19:20 -0700

    Don't try to flush an empty buffer, also cut log files off at 32M.

--- mud/home/Text/sys/logd.c
+++ mud/home/Text/sys/logd.c
@@ -62,12 +62,32 @@ static void flush()
 {
 	string file;
 	string msg;
+	mixed *info;
+
+	if (list_empty(buf)) {
+		return;
+	}
 
 	({ file, msg }) = list_front(buf);
 	list_pop_front(buf);
 
 	SECRETD->make_dir(".");
 	SECRETD->make_dir("log");
+
+	if (info = SECRETD->file_info(file)) {
+		/* ({ file size, file modification time, object }) */
+		int size;
+		int time;
+		mixed obj;
+
+		({ size, time, obj }) = info;
+
+		if (size + strlen(msg) > (32 << 20)) {
+			SECRETD->remove_file(file + ".old");
+			SECRETD->rename_file(file, file + ".old");
+		}
+	}
+
 	SECRETD->write_file(file, msg);
 
 	if (!list_empty(buf)) {
