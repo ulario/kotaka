45d1b29ff7c15e195ce20e9f667bc172f8e53592
Raymond Jennings <shentino@gmail.com>
2018-11-15 22:30:27 -0800

    Rebuild completion should be sent to the system log

--- mud/home/System/sys/subd.c
+++ mud/home/System/sys/subd.c
@@ -373,7 +373,7 @@ static void full_rebuild_tick(mixed **list, mapping initds)
 	string path;
 
 	if (list_empty(list)) {
-		LOGD->post_message("debug", LOG_NOTICE, "Full rebuild completed");
+		LOGD->post_message("system", LOG_NOTICE, "Full rebuild completed");
 		return;
 	}
 
