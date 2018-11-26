f99b11de3d74f9e3197d0e539cba013afd79e689
Raymond Jennings <shentino@gmail.com>
2018-11-26 11:39:48 -0800

    Remove some obsolete defines

--- mud/include/game/paths.h
+++ mud/include/game/paths.h
@@ -21,10 +21,5 @@
 
 #define LIB_HANDLER		(USR_DIR + "/Game/lib/handler")
 
-#define SNOOPD			(USR_DIR + "/Game/sys/snoopd")
 #define GAME_INITD		(USR_DIR + "/Game/initd")
 #define GAME_HELPD		(USR_DIR + "/Game/sys/helpd")
-#define GAME_DRIVER		(USR_DIR + "/Game/sys/driver")
-#define GAME_ROOT		(USR_DIR + "/Game/sys/root")
-#define GAME_SUBD		(USR_DIR + "/Game/sys/subd")
-#define GAME_TESTD		(USR_DIR + "/Game/sys/testd")
