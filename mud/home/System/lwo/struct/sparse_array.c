d7d550ce4a2dccbee7ebf92022f09f47db304fc4
Raymond Jennings <shentino@gmail.com>
2018-11-28 09:07:23 -0800

    Move sparsearray logic into a library

--- mud/home/System/lwo/struct/sparse_array.c
+++ mud/home/System/lwo/struct/sparse_array.c
@@ -21,121 +21,33 @@
 #include <kotaka/paths/system.h>
 
 inherit SECOND_AUTO;
-inherit "~/lib/struct/list";
+inherit "~/lib/struct/sparsearray";
 
 mapping data;
 
 static void create(int clone)
 {
-	data = ([ ]);
+	if (clone) {
+		data = ([ ]);
+	}
 }
 
 void set_element(int index, mixed value)
 {
-	int shift;
-	int level;
-
-	mapping submap;
-
-	shift = index;
-
-	while (shift & ~255) {
-		shift >>= 8;
-		level++;
-	}
-
-	submap = data;
-
-	while (level) {
-		shift = index & (-1 << (level << 3));
-
-		if (!submap[shift]) {
-			submap[shift] = ([ ]);
-		}
-
-		submap = submap[shift];
-		level--;
-	}
-
-	submap[index] = value;
+	sparsearray_set_element(data, index, value);
 }
 
 mixed query_element(int index)
 {
-	int shift;
-	int level;
-
-	mapping submap;
-
-	shift = index;
-
-	while (shift & ~255) {
-		shift >>= 8;
-		level++;
-	}
-
-	submap = data;
-
-	while (level) {
-		shift = index & (-1 << (level << 3));
-
-		if (!submap[shift]) {
-			return nil;
-		}
-
-		submap = submap[shift];
-		level--;
-	}
-
-	return submap[index];
-}
-
-private void build_list(mapping submap, int level, mixed **list, int values)
-{
-	int *indices;
-	int i, sz;
-
-	indices = map_indices(submap);
-	sz = sizeof(indices);
-
-	if (level) {
-		for (i = 0; i < sz; i++) {
-			build_list(submap[indices[i]], level - 1, list, values);
-		}
-	} else {
-		for (i = 0; i < sz; i++) {
-			list_push_back(list, values ? submap[indices[i]] : indices[i]);
-		}
-	}
-}
-
-private mixed **fetch_list(int values)
-{
-	mixed **list;
-	int level;
-	int bit;
-	mapping submap;
-
-	list = ({ nil, nil });
-
-	build_list(data[0 .. 0xff], 0, list, values);
-	build_list(data[0x100 .. 0xffff], 1, list, values);
-	build_list(data[0x10000 .. 0xffffff], 2, list, values);
-	build_list(data[0x1000000 .. 0xffffffff], 3, list, values);
-
-	return list;
+	return sparsearray_query_element(data, index);
 }
 
 mixed **query_indices()
 {
-	return fetch_list(0);
+	return sparsearray_query_indices(data);
 }
 
 mixed **query_values()
 {
-	return fetch_list(1);
-}
-
-void compact()
-{
+	return sparsearray_query_values(data);
 }
