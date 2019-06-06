f60fc9a5569a8ed85826809a3d6efc49ccd80e1a
Raymond Jennings <shentino@gmail.com>
2019-06-05 18:25:36 -0700

    Combine stale and obsolete verbs into stale.  Also, it might take a lot of ticks, so do it in the background.

--- mud/home/Verb/sys/verb/ooc/wiz/stale.c
+++ mud/home/Verb/sys/verb/ooc/wiz/stale.c
@@ -17,11 +17,14 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <status.h>
 #include <kotaka/paths/system.h>
 #include <kotaka/paths/verb.h>
 #include <kotaka/assert.h>
+#include <kotaka/log.h>
 
 inherit LIB_VERB;
+inherit "/lib/sort";
 inherit "~System/lib/struct/list";
 
 string *query_parse_methods()
@@ -29,46 +32,163 @@ string *query_parse_methods()
 	return ({ "raw" });
 }
 
+/*
+
+an object is stale if:
+
+* it inherits a destructed library
+* any of its sources either don't exist or were modified after the object was compiled
+
+*/
+
+static void report_tick(mixed **list, object user, object proxy)
+{
+	int pindex;
+	object pinfo;
+	mixed *libs;
+	string *libnames;
+	string *incs;
+	int sz;
+	int ctime;
+	string path;
+	mapping srcstat;
+	mixed *info;
+
+	pindex = list_front(list);
+	list_pop_front(list);
+
+	if (!list_empty(list)) {
+		call_out("report_tick", 0, list, user, proxy);
+	}
+
+	pinfo = OBJECTD->query_program_info(pindex);
+	path = pinfo->query_path();
+
+	if (pinfo->query_destructed()) {
+		return;
+	}
+
+	libs = pinfo->query_inherits();
+	libnames = ({ });
+	ASSERT(libs);
+
+	for (sz = sizeof(libs); --sz >= 0; ) {
+		object pinfo2;
+
+		pinfo2 = OBJECTD->query_program_info(libs[sz]);
+
+		if (pinfo2->query_destructed()) {
+			libnames += ({ pinfo2->query_path() });
+		}
+	}
+
+	ctime = status(path, O_COMPILETIME);
+
+	srcstat = ([ ]);
+	/* 1 = modified */
+	/* -1 = missing */
+
+	info = proxy->file_info(path + ".c");
+
+	if (!info) {
+		srcstat[path + ".c"] = -1;
+	} else if (info[1] > ctime) {
+		srcstat[path + ".c"] = 1;
+	}
+
+	incs = pinfo->query_includes();
+
+	for (sz = sizeof(incs); --sz >= 0; ) {
+		string inc;
+
+		inc = incs[sz];
+
+		info = proxy->file_info(inc);
+
+		if (!info) {
+			srcstat[inc] = -1;
+		} else if (info[1] > ctime) {
+			srcstat[inc] = 1;
+		}
+	}
+
+	if (map_sizeof(srcstat) || sizeof(libnames)) {
+		int sz;
+		int i;
+
+		user->message("\n" + path + " is stale\n");
+
+		qsort(libnames, 0, sizeof(libnames));
+
+		if (sz = sizeof(libnames)) {
+			user->message("  Destructed inherits\n");
+
+			for (; i < sz; i++) {
+				user->message("    " + libnames[i] + "\n");
+			}
+
+			if (map_sizeof(srcstat)) {
+				user->message("\n");
+			}
+		}
+
+		if (map_sizeof(srcstat)) {
+			user->message("  Missing or modified sources\n");
+		}
+
+		switch (srcstat[path + ".c"]) {
+		case -1:
+			user->message("    " + path + ".c (missing)\n");
+			break;
+		case 1:
+			user->message("    " + path + ".c (modified)\n");
+		}
+
+		srcstat[path + ".c"] = nil;
+
+		if (sz = map_sizeof(srcstat)) {
+			string *paths;
+			int *stats;
+			int i;
+
+			paths = map_indices(srcstat);
+			stats = map_values(srcstat);
+
+			for (i = 0; i < sz; i++) {
+				user->message("  " + paths[i]);
+
+				switch(stats[i]) {
+				case -1:
+					user->message(" is missing\n");
+					break;
+				case 1:
+					user->message(" was modified\n");
+					break;
+				}
+			}
+
+			user->message("\n");
+		}
+	}
+}
+
 void main(object actor, mapping roles)
 {
-	/* Report all objects inheriting a destructed inheritable */
 	mixed **list;
+	object proxy;
+	object user;
 
-	if (query_user()->query_class() < 2) {
+	user = query_user();
+
+	if (user->query_class() < 2) {
 		send_out("Only a wizard can do that.\n");
 		return;
 	}
 
 	list = OBJECTD->query_program_indices();
+	proxy = PROXYD->get_proxy(query_user()->query_name());
 
-	while (!list_empty(list)) {
-		int pindex;
-		object pinfo;
-		int *libs;
-		int ssz, j;
-
-		pindex = list_front(list);
-		list_pop_front(list);
-
-		pinfo = OBJECTD->query_program_info(pindex);
-
-		if (pinfo->query_destructed()) {
-			continue;
-		}
-
-		libs = pinfo->query_inherits();
-		ASSERT(libs);
-
-		ssz = sizeof(libs);
-
-		for (j = 0; j < ssz; j++) {
-			object pinfo2;
-
-			pinfo2 = OBJECTD->query_program_info(libs[j]);
-
-			if (pinfo2->query_destructed()) {
-				send_out(pinfo->query_path() + " inherits destructed " + pinfo2->query_path() + "\n");
-			}
-		}
+	if (!list_empty(list)) {
+		call_out("report_tick", 0, list, user, proxy);
 	}
 }
