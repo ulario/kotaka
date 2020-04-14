e7331a92749df96745e695e03cbbd3820439552b
Raymond Jennings <shentino@gmail.com>
2020-04-14 16:01:37 -0700

    Simplify DumpD, will rebuild scheduled dumps later

--- mud/home/System/sys/dumpd.c
+++ mud/home/System/sys/dumpd.c
@@ -2,7 +2,7 @@
  * This file is part of Kotaka, a mud library for DGD
  * http://github.com/shentino/kotaka
  *
- * Copyright (C) 2018, 2019  Raymond Jennings
+ * Copyright (C) 2018, 2019, 2020  Raymond Jennings
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -21,132 +21,18 @@
 #include <kotaka/assert.h>
 #include <kotaka/paths/system.h>
 #include <kotaka/privilege.h>
+#include <kotaka/log.h>
 #include <status.h>
 
 inherit SECOND_AUTO;
 
-int interval;	/* how long between dumps */
-int offset;	/* remainder of time % interval */
-int steps;	/* total snapshots in a cycle */
-
-private void stop()
-{
-	mixed **callouts;
-	int sz;
-
-	callouts = status(this_object(), O_CALLOUTS);
-
-	for (sz = sizeof(callouts); --sz >= 0; ) {
-		remove_call_out(callouts[sz][CO_HANDLE]);
-	}
-}
-
-private void start()
-{
-	if (interval && steps) {
-		int now;
-		int goal;
-		int full;
-		int test;
-
-		now = time();
-		goal = now;
-		goal -= goal % interval;
-		goal += offset % interval;
-
-		if (goal <= now) {
-			goal += interval;
-		}
-
-		test = (goal - offset) % (interval * steps);
-
-		if (test == test % interval) {
-			full = 1;
-		}
-
-		call_out("dump", goal - now, full);
-	}
-}
-
-private void configure()
-{
-	interval = 60 * 5; /* a dump every 5 minutes */
-	steps = 3600 / interval; /* one full dump every hour */
-	offset = 0;
-}
-
 static void create()
 {
-	configure();
+	call_out("dump", 3600);
 }
 
-static void dump(varargs int full)
+static void dump()
 {
-	int now;
-
-	now = time();
-
-	dump_state(!full);
-
-	start();
-}
-
-void upgrade()
-{
-	ACCESS_CHECK(SYSTEM());
-
-	configure();
-}
-
-void reboot()
-{
-	ACCESS_CHECK(SYSTEM());
-
-	stop();
-	start();
-}
-
-void set_interval(int new_interval)
-{
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	if (new_interval < 1) {
-		error("Invalid interval");
-	}
-
-	stop();
-
-	interval = new_interval;
-
-	start();
-}
-
-void set_offset(int new_offset)
-{
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	if (new_offset < 0) {
-		error("Invalid offset");
-	}
-
-	stop();
-
-	offset = new_offset;
-
-	start();
-}
-
-void set_steps(int new_steps)
-{
-	ACCESS_CHECK(SYSTEM() || KADMIN());
-
-	if (new_steps < 1) {
-		error("Invalid steps");
-	}
-
-	stop();
-
-	steps = new_steps;
-
-	start();
+	dump_state();
+	call_out("dump", 3600);
 }
