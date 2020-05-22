53a9221e757ebf7026071c5cea176dcfe0cd69af
Raymond Jennings <shentino@gmail.com>
2020-05-22 04:09:16 -0700

    Refactor properties

--- mud/home/Http/lib/thing.c
+++ mud/home/Http/lib/thing.c
@@ -1,13 +1,18 @@
 inherit "support";
 inherit "bulk";
+inherit "/lib/string/sprint";
 
 static string thing_text(object obj)
 {
-	mapping lprops;
 	object arch;
 	object env;
 	object *inv;
+	string *names;
+	mixed *values;
 	int sz;
+
+	mapping props;
+
 	string buffer;
 
 	buffer = "<h2>Thing</h2>";
@@ -40,7 +45,41 @@ static string thing_text(object obj)
 		buffer += "</ul>\n";
 	}
 
+	inv = obj->query_inventory();
+	sz = sizeof(inv);
+
+	if (sz) {
+		int i;
+
+		buffer += "<p>Inventory:</p>\n";
+		buffer +="<ul>\n";
+
+		for (i = 0; i < sz; i++) {
+			buffer += "<li>" + object2link(inv[i]) + "</li>\n";
+		}
+
+		buffer += "</ul>\n";
+	}
+
 	buffer += bulk_text(obj);
+
+	props = obj->query_local_properties();
+	names = map_indices(props);
+	values = map_values(props);
+	sz = sizeof(names);
+
+	if (sz) {
+		int i;
+
+		buffer += "<h3>Local properties:</h3>\n";
+		buffer += "<dl>\n";
+
+		for (i = 0; i < sz; i++) {
+			buffer += "<dt>" + names[i] + "</dt><dd>" + hybrid_sprint(values[i]) + "</dd>\n";
+		}
+
+		buffer += "</dl>\n";
+	}
 
 	return buffer;
 }
