41cdace7b9f4caf0e21119996ca2d8696c6abae0
Raymond Jennings <shentino@gmail.com>
2020-05-23 02:01:19 -0700

    Allow local mass and mass to be set separately by form

--- mud/home/Http/lib/form/thing.c
+++ mud/home/Http/lib/form/thing.c
@@ -8,8 +8,13 @@ static string thing_form(object obj)
 	buffer = "<h1>Object form</h1>\n";
 
 	buffer += "<form action=\"object.lpc?obj=" + object2string(obj) + "\" method=\"post\">\n";
-	buffer += "<p>Mass: <input type=\"text\" name=\"mass\" value=\"" + mixed_sprint(obj->query_mass()) + "\"/></p><br />\n";
+	buffer += "Mass: <input type=\"text\" name=\"mass\" value=\"" + mixed_sprint(obj->query_mass()) + "\"/>\n";
 	buffer += "<input type=\"submit\" value=\"change mass\" />\n";
+	buffer += "</form>\n";
+
+	buffer += "<form action=\"object.lpc?obj=" + object2string(obj) + "\" method=\"post\">\n";
+	buffer += "Local mass: <input type=\"text\" name=\"localmass\" value=\"" + mixed_sprint(obj->query_local_mass()) + "\"/>\n";
+	buffer += "<input type=\"submit\" value=\"change local mass\" />\n";
 	buffer += "</form>\n";
 
 	return buffer;
