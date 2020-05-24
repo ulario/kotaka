b701d2ce0e96280713b84192aed48fb87d609d94
Raymond Jennings <shentino@gmail.com>
2020-05-24 13:12:16 -0700

    Remove http form for mass, only manipulate local mass

--- mud/home/Http/lib/form/thing.c
+++ mud/home/Http/lib/form/thing.c
@@ -8,11 +8,6 @@ static string thing_form(object obj)
 	buffer = "<p>Fun little boxes:</p>\n";
 
 	buffer += "<form action=\"object.lpc?obj=" + object2string(obj) + "\" method=\"post\">\n";
-	buffer += "Mass: <input type=\"text\" name=\"mass\" value=\"" + mixed_sprint(obj->query_mass()) + "\"/>\n";
-	buffer += "<input type=\"submit\" value=\"change mass\" />\n";
-	buffer += "</form>\n";
-
-	buffer += "<form action=\"object.lpc?obj=" + object2string(obj) + "\" method=\"post\">\n";
 	buffer += "Local mass: <input type=\"text\" name=\"localmass\" value=\"" + mixed_sprint(obj->query_local_mass()) + "\"/>\n";
 	buffer += "<input type=\"submit\" value=\"change local mass\" />\n";
 	buffer += "</form>\n";
