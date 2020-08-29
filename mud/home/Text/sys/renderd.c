8c68b8612c0c9ce44b5c259f270cc6115030c525
Raymond Jennings <shentino@gmail.com>
2020-08-28 18:53:16 -0700

    Remove geometry.h from RenderD

--- mud/home/Text/sys/renderd.c
+++ mud/home/Text/sys/renderd.c
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <kotaka/paths/geometry.h>
 #include <kotaka/paths/text.h>
 #include <kotaka/privilege.h>
 
