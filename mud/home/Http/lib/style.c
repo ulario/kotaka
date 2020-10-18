b436187b5addfdeeea153e83c4dbd72a66971ae4
Raymond Jennings <shentino@gmail.com>
2020-10-17 20:16:08 -0700

    Move style information to a css file

--- mud/home/Http/lib/style.c
+++ mud/home/Http/lib/style.c
@@ -1,12 +1,6 @@
 static string style()
 {
 	return "<style>\n"
-		+ "h1, h2, h3 { padding: 1em; text-align: center; background-color: #aaa; border: .25em solid black; border-radius: .5em; }\n"
-		+ "table { margin: 0 auto; }\n"
-		+ "div.column { column-count: 2; column-gap: 2em; column-fill: auto; }\n"
-		+ "div.infobox { margin: 1em; padding: 1em; background-color: #ffa; border: 1em solid #aa0; border-radius: 2em; }\n"
-		+ "td { margin: 0; vertical-align: top; padding: 0 1em; }\n"
-		+ "table { margin: 0 auto; }\n"
-		+ "td.spacer { width: 2em; }\n"
+		+ read_file("http.css")
 		+ "</style>\n";
 }
