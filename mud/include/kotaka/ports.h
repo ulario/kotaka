b0fc9c8ca117daaf1e9d154e5b33dedb8a6def75
Raymond Jennings <shentino@gmail.com>
2021-02-08 17:10:04 -0800

    Add ports.h

---
+++ mud/include/kotaka/ports.h
@@ -0,0 +1,6 @@
+#define PORT_BASE  49152
+
+#define LOGIN_PORT    (PORT_BASE)     /* Text login */
+#define ADMIN_PORT    (PORT_BASE + 1) /* klib backdoor */
+#define STATUS_PORT   (PORT_BASE + 2) /* status */
+#define HTTP_PORT     (PORT_BASE + 3) /* HTTP */
