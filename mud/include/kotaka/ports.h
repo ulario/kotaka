141bbfada9f559295fdb4d5afbcd2f6f301fd08c
Raymond Jennings <shentino@gmail.com>
2021-02-08 19:39:15 -0800

    Put ports in correct order in ports.h according to config file

--- mud/include/kotaka/ports.h
+++ mud/include/kotaka/ports.h
@@ -1,6 +1,6 @@
 #define PORT_BASE  49152
 
-#define LOGIN_PORT    (PORT_BASE)     /* Text login */
-#define ADMIN_PORT    (PORT_BASE + 1) /* klib backdoor */
-#define STATUS_PORT   (PORT_BASE + 2) /* status */
+#define ADMIN_PORT    (PORT_BASE + 0) /* klib backdoor */
+#define STATUS_PORT   (PORT_BASE + 1) /* status */
+#define LOGIN_PORT    (PORT_BASE + 2) /* Text login */
 #define HTTP_PORT     (PORT_BASE + 3) /* HTTP */
