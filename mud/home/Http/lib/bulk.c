c7ad50037cecd5c0d4f0452683babda2cefba8e1
Raymond Jennings <shentino@gmail.com>
2021-03-07 19:16:50 -0800

    Don't show spurious details of virtual thing

--- mud/home/Http/lib/bulk.c
+++ mud/home/Http/lib/bulk.c
@@ -5,50 +5,49 @@ static string bulk_text(object obj)
 {
 	string buffer;
 
-	buffer = "<span style=\"white-space: no wrap;\">\n";
-
-	buffer += "Mass: " + mixed_sprint(obj->query_mass()) + " kg";
-
-	if (obj->query_mass_absolute()) {
-		buffer += " (absolute)<br />\n";
-	} else {
-		buffer += " (derived, local mass " + mixed_sprint(obj->query_local_mass()) + " kg<br />\n";
-	}
-
-
-	buffer += "Density: " + mixed_sprint(obj->query_density()) + " kg/l";
-
-	if (obj->query_density_absolute()) {
-		buffer += " (absolute)<br />\n";
-	} else {
-		buffer += " (derived, local density " + mixed_sprint(obj->query_local_density()) + " kg/l)<br />\n";
-	}
-
-	buffer += "Capacity: " + mixed_sprint(obj->query_capacity()) + " m^3";
-
-	if (obj->query_capacity_absolute()) {
-		buffer += " (absolute)<br />\n";
-	} else {
-		buffer += " (derived, local capacity " + mixed_sprint(obj->query_local_capacity()) + " m^3)<br />\n";
-	}
-
-	buffer += "Max mass: " + mixed_sprint(obj->query_max_mass()) + " kg";
-
-	if (obj->query_max_mass_absolute()) {
-		buffer += " (absolute)<br />\n";
-	} else {
-		buffer += " (derived, local max mass " + mixed_sprint(obj->query_local_max_mass()) + " kg)<br />\n";
-	}
-
 	if (obj->query_virtual()) {
-		buffer += "Virtual<br />\n";
-	}
+		buffer = "Virtual<br />\n";
+	} else {
+		if (obj->query_flexible()) {
+			buffer += "Flexible<br />\n";
+		}
 
-	if (obj->query_flexible()) {
-		buffer += "Flexible<br />\n";
-	}
+		buffer = "<span style=\"white-space: no wrap;\">\n";
 
-	buffer += "</span>\n";
+		buffer += "Mass: " + mixed_sprint(obj->query_mass()) + " kg";
+
+		if (obj->query_mass_absolute()) {
+			buffer += " (absolute)<br />\n";
+		} else {
+			buffer += " (derived, local mass " + mixed_sprint(obj->query_local_mass()) + " kg<br />\n";
+		}
+
+		buffer += "Density: " + mixed_sprint(obj->query_density()) + " kg/l";
+
+		if (obj->query_density_absolute()) {
+			buffer += " (absolute)<br />\n";
+		} else {
+			buffer += " (derived, local density " + mixed_sprint(obj->query_local_density()) + " kg/l)<br />\n";
+		}
+
+		buffer += "Capacity: " + mixed_sprint(obj->query_capacity()) + " m^3";
+
+		if (obj->query_capacity_absolute()) {
+			buffer += " (absolute)<br />\n";
+		} else {
+			buffer += " (derived, local capacity " + mixed_sprint(obj->query_local_capacity()) + " m^3)<br />\n";
+		}
+
+		buffer += "Max mass: " + mixed_sprint(obj->query_max_mass()) + " kg";
+
+		if (obj->query_max_mass_absolute()) {
+			buffer += " (absolute)<br />\n";
+		} else {
+			buffer += " (derived, local max mass " + mixed_sprint(obj->query_local_max_mass()) + " kg)<br />\n";
+		}
+
+		buffer += "</span>\n";
+	}
 
 	return oinfobox("Bulk", 3, buffer);
 }
