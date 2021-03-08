8c2df0d32da866e73f69c7f164802d2413c646a8
Raymond Jennings <shentino@gmail.com>
2021-03-07 18:09:38 -0800

    Rewrite bulk output for html

--- mud/home/Http/lib/bulk.c
+++ mud/home/Http/lib/bulk.c
@@ -5,18 +5,50 @@ static string bulk_text(object obj)
 {
 	string buffer;
 
-	if (obj->query_virtual()) {
-		buffer = "Virtual<br />\n";
+	buffer = "<span style=\"white-space: no wrap;\">\n";
+
+	buffer += "Mass: " + mixed_sprint(obj->query_mass()) + " kg";
+
+	if (obj->query_mass_absolute()) {
+		buffer += " (absolute)<br />\n";
 	} else {
-		buffer = "Mass: " + mixed_sprint(obj->query_mass()) + " kg"
-			+ " (local mass: " + mixed_sprint(obj->query_local_mass()) + " kg)<br />\n";
-		buffer += "Density: " + mixed_sprint(obj->query_density()) + " kg/l"
-			+ " (local density: " + mixed_sprint(obj->query_density()) + " kg/l)<br />\n";
-		buffer += "Capacity: " + mixed_sprint(obj->query_capacity()) + " m^3"
-			+ " (local capacity: " + mixed_sprint(obj->query_local_capacity()) + " m^3)<br />\n";
-		buffer += "Max mass: " + mixed_sprint(obj->query_max_mass()) + " kg\n"
-			+ " (local max mass: " + mixed_sprint(obj->query_max_mass()) + " kg)<br />\n";
+		buffer += " (derived, local mass " + mixed_sprint(obj->query_local_mass()) + " kg<br />\n";
 	}
+
+
+	buffer += "Density: " + mixed_sprint(obj->query_density()) + " kg/l";
+
+	if (obj->query_density_absolute()) {
+		buffer += " (absolute)<br />\n";
+	} else {
+		buffer += " (derived, local density " + mixed_sprint(obj->query_local_density()) + " kg/l)<br />\n";
+	}
+
+	buffer += "Capacity: " + mixed_sprint(obj->query_capacity()) + " m^3";
+
+	if (obj->query_capacity_absolute()) {
+		buffer += " (absolute)<br />\n";
+	} else {
+		buffer += " (derived, local capacity " + mixed_sprint(obj->query_local_capacity()) + " m^3)<br />\n";
+	}
+
+	buffer += "Max mass: " + mixed_sprint(obj->query_max_mass()) + " kg";
+
+	if (obj->query_max_mass_absolute()) {
+		buffer += " (absolute)<br />\n";
+	} else {
+		buffer += " (derived, local max mass " + mixed_sprint(obj->query_local_max_mass()) + " kg)<br />\n";
+	}
+
+	if (obj->query_virtual()) {
+		buffer += "Virtual<br />\n";
+	}
+
+	if (obj->query_flexible()) {
+		buffer += "Flexible<br />\n";
+	}
+
+	buffer += "</span>\n";
 
 	return oinfobox("Bulk", 3, buffer);
 }
