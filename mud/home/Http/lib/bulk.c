111f7e716c3cd7867cf6d4dacd2f762e4337a805
Raymond Jennings <shentino@gmail.com>
2021-03-16 22:10:35 -0700

    Remove support in http for absolute bulk

--- mud/home/Http/lib/bulk.c
+++ mud/home/Http/lib/bulk.c
@@ -17,13 +17,18 @@ static string bulk_text(object obj)
 		buffer += "Mass: " + mixed_sprint(obj->query_mass()) + " kg<br />\n";
 		buffer += "Density: " + mixed_sprint(obj->query_density()) + " kg/l<br />\n";
 		buffer += "Capacity: " + mixed_sprint(obj->query_capacity()) + " m^3<br />\n";
-		buffer += "Max mass: " + mixed_sprint(obj->query_max_mass()) + " kg<br />\n";
+		buffer += "Max mass: " + mixed_sprint(obj->query_max_mass()) + " kg<br />\n</br />\n";
+
+		if (obj->query_archetype()) {
+			buffer += "Local mass: " + mixed_sprint(obj->query_local_mass()) + " kg<br />\n";
+			buffer += "Local density: " + mixed_sprint(obj->query_local_density()) + " kg/l<br />\n";
+			buffer += "Local capacity: " + mixed_sprint(obj->query_local_capacity()) + " m^3<br />\n";
+			buffer += "Local max mass: " + mixed_sprint(obj->query_local_max_mass()) + " kg\n";
+		} else {
+			buffer += "No archetype\n";
+		}
 
 		buffer += "</span>\n";
-
-		if (obj->query_absolute()) {
-			buffer += "Absolute bulk<br />\n";
-		}
 	}
 
 	return oinfobox("Bulk", 3, buffer);
