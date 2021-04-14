e1fd17e1b5a7b8b37c8334f4b6113e7077ee6e9f
Raymond Jennings <shentino@gmail.com>
2021-04-13 19:17:23 -0700

    Don't load sys and lwo in Geometry, they don't exist right now

--- mud/home/Geometry/initd.c
+++ mud/home/Geometry/initd.c
@@ -2,7 +2,7 @@
  * This file is part of Kotaka, a mud library for DGD
  * http://github.com/shentino/kotaka
  *
- * Copyright (C) 2018, 2020  Raymond Jennings
+ * Copyright (C) 2018, 2020, 2021  Raymond Jennings
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -26,9 +26,6 @@ inherit UTILITY_COMPILE;
 private void load()
 {
 	MODULED->boot_module("Thing");
-
-	load_dir("lwo");
-	load_dir("sys");
 }
 
 private void set_limits()
