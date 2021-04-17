66ae1128ef04e108137c6a9f62e11596189027f1
Raymond Jennings <shentino@gmail.com>
2021-04-16 22:24:38 -0700

    Update tutorial

--- mud/home/Text/obj/ustate/tutorial.c
+++ mud/home/Text/obj/ustate/tutorial.c
@@ -23,13 +23,16 @@
 
 inherit TEXT_LIB_USTATE;
 
+int stopped;
 int pending;
 
 private void do_help()
 {
 	send_out("To use the tutorial, please choose one of the following topics:\n");
+	send_out("help - This help page\n");
 	send_out("characters - How characters work on this MUD\n");
-	send_out("communication - How to communicate, both IC and OOC\n");
+	send_out("communication - How to communicate, both IC and OOC\n\n");
+	send_out("Use \"quit\" to go or return to the shell.\n");
 }
 
 private void do_characters()
@@ -52,11 +55,16 @@ private void do_communication()
 	send_out("Communicating with other players, or characters, can be done as follows:\n\n");
 	send_out("OOC:\n");
 	send_out("Players can use channels to talk to other subscribed users.\n");
-	send_out("chlist - List available channels");
-	send_out("chon - Turn a channel on");
-	send_out("choff - Turn a channel off");
+	send_out("chlist - List available channels\n");
+	send_out("chon - Turn a channel on\n");
+	send_out("choff - Turn a channel off\n");
 	send_out("chpost <channel name> <message> - Post to a channel.\n\n");
 	send_out("The \"chat\" channel is for player use.\n\n");
+}
+
+private void prompt()
+{
+	send_out("[tutorial] ");
 }
 
 static void create(int clone)
@@ -74,8 +82,9 @@ void begin()
 	ACCESS_CHECK(previous_object() == query_user());
 
 	send_out("Welcome to the Ulario MUD tutorial.\n\n");
-	send_out("Available tutorial topics:\n\n");
-	send_out("characters, communication, help.\n");
+
+	do_help();
+
 
 	prompt();
 }
@@ -85,7 +94,43 @@ void end()
 	destruct_object(this_object());
 }
 
+void stop()
+{
+	stopped = 1;
+}
+
+void go()
+{
+	stopped = 0;
+
+	if (pending) {
+		prompt();
+	}
+}
+
 void receive_in(string input)
 {
 	ACCESS_CHECK(previous_object() == query_user());
+
+	switch (input) {
+	case "characters":
+		do_characters();
+		break;
+	case "communication":
+		do_communication();
+		break;
+	case "help":
+		do_help();
+		break;
+	case "quit":
+		pop_state();
+		return;
+	default:
+		send_out("No such topic in the tutorial.\n");
+		break;
+	}
+
+	if (!stopped) {
+		prompt();
+	}
 }
