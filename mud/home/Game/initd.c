24d4410765a9a5ec09d7b252f390718dc563458e
Raymond Jennings <shentino@gmail.com>
2021-06-20 07:49:20 -0700

    Don't automatically boot Test module, this is reserved for development and debugging

--- mud/home/Game/initd.c
+++ mud/home/Game/initd.c
@@ -2,7 +2,7 @@
  * This file is part of Kotaka, a mud library for DGD
  * http://github.com/shentino/kotaka
  *
- * Copyright (C) 2018, 2020  Raymond Jennings
+ * Copyright (C) 2018, 2020, 2021  Raymond Jennings
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -42,7 +42,6 @@ private void load()
 	MODULED->boot_module("Http");
 	MODULED->boot_module("Intermud");
 	MODULED->boot_module("Kotaka");
-	MODULED->boot_module("Test");
 	MODULED->boot_module("Text");
 	MODULED->boot_module("Thing");
 
