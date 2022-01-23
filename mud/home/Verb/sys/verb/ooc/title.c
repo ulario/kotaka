14443544cb5109427dbac45c1642c9dc665f2741
Raymond Jennings <shentino@gmail.com>
2022-01-23 02:41:39 -0800

    Implement title verb

---
+++ mud/home/Verb/sys/verb/ooc/title.c
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2022  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths/account.h>
+#include <kotaka/paths/string.h>
+#include <kotaka/paths/verb.h>
+
+inherit LIB_VERB;
+
+string *query_parse_methods()
+{
+	return ({ "raw" });
+}
+
+string query_help_title()
+{
+	return "Title";
+}
+
+string *query_help_contents()
+{
+	return ({ "Sets your title" });
+}
+
+void main(object actor, mapping roles)
+{
+	object user;
+
+	user = query_user();
+
+	if (user->query_class() < 3) {
+		send_out("You do not have sufficient access rights to set your title.\n");
+		return;
+	}
+
+	ACCOUNTD->set_account_property(user->query_username(), "title", roles["raw"]);
+
+	send_out("Done.\n");
+}
