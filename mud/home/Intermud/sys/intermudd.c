33c473fb70d36f487f08d6e79aa1febcb7623db8
Raymond Jennings <shentino@gmail.com>
2022-01-27 06:26:13 -0800

    Be careful of empty lines when checking for newline suffix

--- mud/home/Intermud/sys/intermudd.c
+++ mud/home/Intermud/sys/intermudd.c
@@ -2,7 +2,7 @@
  * This file is part of Kotaka, a mud library for DGD
  * http://github.com/shentino/kotaka
  *
- * Copyright (C) 2018, 2019, 2020, 2021  Raymond Jennings
+ * Copyright (C) 2018, 2019, 2020, 2021, 2022  Raymond Jennings
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -469,7 +469,7 @@ private void i3_handle_tell(mixed *value)
 
 		msg = value[7];
 
-		if (msg[strlen(msg) - 1] != '\n') {
+		if (msg == "" || msg[strlen(msg) - 1] != '\n') {
 			msg += "\n";
 		}
 
