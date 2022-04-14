fe11b5fe7ffc4c36764a5156224540048991cb95
Raymond Jennings <shentino@gmail.com>
2022-04-14 14:33:29 -0700

    Per release process for 0.62, tag CatalogD as deprecated

--- mud/home/System/sys/catalogd.c
+++ mud/home/System/sys/catalogd.c
@@ -2,7 +2,7 @@
  * This file is part of Kotaka, a mud library for DGD
  * http://github.com/shentino/kotaka
  *
- * Copyright (C) 2018  Raymond Jennings
+ * Copyright (C) 2018, 2022  Raymond Jennings
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -17,6 +17,9 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/**************/
+/* DEPRECATED */
+/**************/
 #include <kotaka/paths/system.h>
 #include <kotaka/privilege.h>
 #include <kotaka/log.h>
