7834ea3ad1727b42f458a087b201095d6e0309f9
Raymond Jennings <shentino@gmail.com>
2022-05-16 03:08:47 -0700

    Implement idea verb

---
+++ mud/home/Verb/sys/verb/ooc/idea.c
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Kotaka, a mud library for DGD
+ * http://github.com/shentino/kotaka
+ *
+ * Copyright (C) 2022  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <kotaka/paths/account.h>
+#include <kotaka/paths/text.h>
+#include <kotaka/paths/verb.h>
+
+inherit LIB_VERB;
+inherit "~Text/lib/sub";
+
+string *query_parse_methods()
+{
+	return ({ "raw" });
+}
+
+string query_help_title()
+{
+	return "Idea";
+}
+
+string *query_help_contents()
+{
+	return ({ "Use this command to suggest an idea." });
+}
+
+void main(object actor, mapping roles)
+{
+	if (roles["raw"]) {
+		send_out("Usage: idea\n");
+		return;
+	}
+
+	if (query_user_class(query_user()->query_name()) < 1) {
+		send_out("Only a registered user can suggest ideas.\n");
+		return;
+	}
+
+	query_ustate()->push_state(query_user()->clone_ustate("feedback/idea"));
+}
