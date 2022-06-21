d4720a695c56065d63a62b262d4957114a3d9368
Raymond Jennings <shentino@gmail.com>
2022-06-21 13:35:05 -0700

    Reindent the switch statement

--- mud/home/Text/lib/pinkfish.c
+++ mud/home/Text/lib/pinkfish.c
@@ -24,78 +24,78 @@ inherit "/lib/string/case";
 
 private string ansi_of_fish(string fish)
 {
-			switch(fish) {
-			case "":
-				break;
+	switch(fish) {
+	case "":
+		break;
 
-			case "BOLD":
-			case "BLINK":
-				fish = "\033[1m";
-				break;
+	case "BOLD":
+	case "BLINK":
+		fish = "\033[1m";
+		break;
 
-			case "RESET":
-				fish = "\033[0m";
-				break;
+	case "RESET":
+		fish = "\033[0m";
+		break;
 
-			case "RED":
-				fish = "\033[31m";
-				break;
+	case "RED":
+		fish = "\033[31m";
+		break;
 
-			case "GREEN":
-				fish = "\033[32m";
-				break;
+	case "GREEN":
+		fish = "\033[32m";
+		break;
 
-			case "ORANGE":
-			case "YELLOW":
-				fish = "\033[33m";
-				break;
+	case "ORANGE":
+	case "YELLOW":
+		fish = "\033[33m";
+		break;
 
-			case "BLUE":
-				fish = "\033[34m";
-				break;
+	case "BLUE":
+		fish = "\033[34m";
+		break;
 
-			case "CYAN":
-				fish = "\033[35m";
-				break;
+	case "CYAN":
+		fish = "\033[35m";
+		break;
 
-			case "MAGENTA":
-				fish = "\033[36m";
-				break;
+	case "MAGENTA":
+		fish = "\033[36m";
+		break;
 
-			case "B_RED":
-				fish = "\033[1;31m";
-				break;
+	case "B_RED":
+		fish = "\033[1;31m";
+		break;
 
-			case "B_GREEN":
-				fish = "\033[1;32m";
-				break;
+	case "B_GREEN":
+		fish = "\033[1;32m";
+		break;
 
-			case "B_YELLOW":
-				fish = "\033[1;33m";
-				break;
+	case "B_YELLOW":
+		fish = "\033[1;33m";
+		break;
 
-			case "B_BLUE":
-				fish = "\033[1;34m";
-				break;
+	case "B_BLUE":
+		fish = "\033[1;34m";
+		break;
 
-			case "B_CYAN":
-				fish = "\033[1;35m";
-				break;
+	case "B_CYAN":
+		fish = "\033[1;35m";
+		break;
 
-			case "B_MAGENTA":
-				fish = "\033[1;36m";
-				break;
+	case "B_MAGENTA":
+		fish = "\033[1;36m";
+		break;
 
-			case "WHITE":
-				fish = "\033[37m";
-				break;
+	case "WHITE":
+		fish = "\033[37m";
+		break;
 
-			default:
-				/* if all caps, it's probably a pinkfish code */
-				if (fish == to_upper(fish)) {
-					LOGD->post_message("system", LOG_NOTICE, "Potential untranslated pinkfish code " + fish);
-				}
-			}
+	default:
+		/* if all caps, it's probably a pinkfish code */
+		if (fish == to_upper(fish)) {
+			LOGD->post_message("system", LOG_NOTICE, "Potential untranslated pinkfish code " + fish);
+		}
+	}
 
 	return fish;
 }
