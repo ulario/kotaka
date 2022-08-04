097ff42c7521210595968e8e72b4e42c1b415dd8
Raymond Jennings <shentino@gmail.com>
2022-08-03 22:51:25 -0700

    I forgot a semicolon

--- mud/home/Game/lib/thing/detail.c
+++ mud/home/Game/lib/thing/detail.c
@@ -638,7 +638,7 @@ atomic void patch_detail_downcase()
 	new_vetoed_details = ({ });
 
 	for (sz = sizeof(vetoed_details); --sz >= 0; ) {
-		new_vetoed_details |= ({ vetoed_details[sz] })
+		new_vetoed_details |= ({ vetoed_details[sz] });
 	}
 
 	qsort(new_vetoed_details, 0, sizeof(new_vetoed_details));
